d82c51177f8865b2911dc8aa05ddcff5cdf344ae
lassulus <lass@aidsballs.de>
2015-04-12 20:32:07 +0200

    xserver-lass: use mainUser

--- modules/lass/xserver-lass.nix
+++ modules/lass/xserver-lass.nix
@@ -1,8 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  mainUser = config.users.extraUsers.mainUser;
+
+in {
   imports = [
     ../tv/xserver.nix
   ];
-  services.xserver.displayManager.auto.user = "lass";
+  services.xserver.displayManager.auto.user = mainUser.name;
 }
