a487c74e3d47a949324edba95ce52ff7f05108b9
lassulus <lass@aidsballs.de>
2015-05-15 23:19:35 +0200

    desktop-base: use sshkeys.nix

--- modules/lass/desktop-base.nix
+++ modules/lass/desktop-base.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./sshkeys.nix
+  ];
   boot.tmpOnTmpfs = true;
   # see tmpfiles.d(5)
   systemd.tmpfiles.rules = [
@@ -65,9 +68,7 @@
   services.gitolite = {
     enable = true;
     dataDir = "/home/gitolite";
-    adminPubkey = ''
-      ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAp83zynhIueJJsWlSEykVSBrrgBFKq38+vT8bRfa+csqyjZBl2SQFuCPo+Qbh49mwchpZRshBa9jQEIGqmXxv/PYdfBFQuOFgyUq9ZcTZUXqeynicg/SyOYFW86iiqYralIAkuGPfQ4howLPVyjTZtWeEeeEttom6p6LMY5Aumjz2em0FG0n9rRFY2fBzrdYAgk9C0N6ojCs/Gzknk9SGntA96MDqHJ1HXWFMfmwOLCnxtE5TY30MqSmkrJb7Fsejwjoqoe9Y/mCaR0LpG2cStC1+37GbHJNH0caCMaQCX8qdfgMVbWTVeFWtV6aWOaRgwLrPDYn4cHWQJqTfhtPrNQ== lass@mors
-    '';
+    adminPubkey = config.sshKeys.lass.pub;
   };
 
   services.journald.extraConfig = ''
