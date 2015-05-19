3be7c69f5cba0d8e8c9955b8252a6f613fce6ef5
lassulus <lass@aidsballs.de>
2015-05-19 22:31:33 +0200

    disbale binary-caches (they dont work with ssh)

--- modules/lass/binary-caches.nix
+++ modules/lass/binary-caches.nix
@@ -7,7 +7,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCZSq5oLrokkh3F+MOdK5/nzVIEDvqyvfzLMNWmzsYD root@uriel"
   ];
   nix.binaryCaches = [
-    "scp://nix-ssh@mors"
-    "scp://nix-ssh@uriel"
+    #"scp://nix-ssh@mors"
+    #"scp://nix-ssh@uriel"
   ];
 }
