56f65724e368533342c8df146e412130afa4491e
tv <tv@shackspace.de>
2015-05-20 19:15:12 +0200

    lib cac: add cac_listtasks

--- lib/cac.sh
+++ lib/cac.sh
@@ -7,3 +7,13 @@ cac_listservers() {
       "https://panel.cloudatcost.com/api/v1/listservers.php?key=$cac_key\\&login=$cac_login"
   fi
 }
+
+cac_listtasks() {
+  if test -z "${cac_via-}"; then
+    curl -fsS \
+      "https://panel.cloudatcost.com/api/v1/listtasks.php?key=$cac_key&login=$cac_login"
+  else
+    ssh -q $cac_via -t curl -fsS \
+      "https://panel.cloudatcost.com/api/v1/listtasks.php?key=$cac_key\\&login=$cac_login"
+  fi
+}
