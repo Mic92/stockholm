cc62429cf3cbe638b932e9226ffe59c74b7dd873
tv <tv@shackspace.de>
2015-05-20 19:14:35 +0200

    lib cac: remove \ from URL

--- lib/cac.sh
+++ lib/cac.sh
@@ -1,7 +1,7 @@
 cac_listservers() {
   if test -z "${cac_via-}"; then
     curl -fsS \
-      "https://panel.cloudatcost.com/api/v1/listservers.php?key=$cac_key\&login=$cac_login"
+      "https://panel.cloudatcost.com/api/v1/listservers.php?key=$cac_key&login=$cac_login"
   else
     ssh -q $cac_via -t curl -fsS \
       "https://panel.cloudatcost.com/api/v1/listservers.php?key=$cac_key\\&login=$cac_login"
