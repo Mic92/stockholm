68590be2f202f77e488625fa332772043e1cedfc
tv <tv@shackspace.de>
2015-05-20 19:22:49 +0200

    lib cac: wrap cac_via'ble calls with _cac_exec

--- lib/cac.sh
+++ lib/cac.sh
@@ -1,31 +1,25 @@
-cac_listservers() {
+_cac_exec() {
   if test -z "${cac_via-}"; then
-    curl -fsS \
-      -G \
-      --data-urlencode key="$cac_key" \
-      --data-urlencode login="$cac_login" \
-      'https://panel.cloudatcost.com/api/v1/listservers.php'
+    (exec "$@")
   else
-    ssh -q $cac_via -t curl -fsS \
-      -G \
-      --data-urlencode key="$cac_key" \
-      --data-urlencode login="$cac_login" \
-      'https://panel.cloudatcost.com/api/v1/listservers.php'
+    ssh -q "$cac_via" -t "$@"
   fi
 }
 
+cac_listservers() {
+  _cac_exec \
+    curl -fsS \
+      -G \
+      --data-urlencode key="$cac_key" \
+      --data-urlencode login="$cac_login" \
+      'https://panel.cloudatcost.com/api/v1/listservers.php'
+}
+
 cac_listtasks() {
-  if test -z "${cac_via-}"; then
+  _cac_exec \
     curl -fsS \
       -G \
       --data-urlencode key="$cac_key" \
       --data-urlencode login="$cac_login" \
       'https://panel.cloudatcost.com/api/v1/listtasks.php'
-  else
-    ssh -q $cac_via -t curl -fsS \
-      -G \
-      --data-urlencode key="$cac_key" \
-      --data-urlencode login="$cac_login" \
-      'https://panel.cloudatcost.com/api/v1/listtasks.php'
-  fi
 }
