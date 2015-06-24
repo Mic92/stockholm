b8dab2b12b5235e85da41e48a08ad6aff3249afb
tv <tv@shackspace.de>
2015-06-24 20:18:26 +0200

    rmdir networking: bump

--- modules/rmdir/networking.nix
+++ modules/rmdir/networking.nix
@@ -1,13 +1,14 @@
-{...}:
+_:
+
 {
   networking.hostName = "rmdir";
   networking.interfaces.enp2s1.ip4 = [
     {
-      address = "162.219.6.2";
+      address = "167.88.44.94";
       prefixLength = 24;
     }
   ];
-  networking.defaultGateway = "162.219.6.1";
+  networking.defaultGateway = "167.88.44.1";
   networking.nameservers = [
     "8.8.8.8"
   ];
