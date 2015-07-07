f4b8b7a1df963ac6d28a77f84cf2687324f41aa5
tv <tv@shackspace.de>
2015-07-07 06:35:51 +0200

    nomic: init play {deepmix,groovesalad,ntslive}

--- modules/nomic/default.nix
+++ modules/nomic/default.nix
@@ -55,6 +55,24 @@ in
     "net.ipv6.conf.default.use_tempaddr" = 2;
   };
 
+  environment.systemPackages = with pkgs; [
+    (writeScriptBin "play" ''
+      #! /bin/sh
+      set -euf
+      mpv() { ${mpv}/bin/mpv "$@"; }
+      case $1 in
+        deepmix)      mpv http://deepmix.ru/deepmix128.pls;;
+        groovesalad)  mpv http://somafm.com/play/groovesalad;;
+        ntslive)      mpv http://listen2.ntslive.co.uk/listen.pls;;
+        *)
+          echo "$0: bad argument: $*" >&2
+          exit 23
+      esac
+    '')
+    rxvt_unicode.terminfo
+    tmux
+  ];
+
   networking = {
     hostName = "nomic";
     wireless.enable = true;
