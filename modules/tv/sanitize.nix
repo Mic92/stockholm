310c857e3bb71ac950bea6c9b94f76f1cd5dcf3d
tv <tv@shackspace.de>
2015-07-10 20:49:19 +0200

    tv sanitize: not rtkit

--- modules/tv/sanitize.nix
+++ modules/tv/sanitize.nix
@@ -6,7 +6,6 @@
       nano = pkgs.runCommand "empty" {} "mkdir -p $out";
     };
 
-  security.rtkit.enable = false;
   services.cron.enable = false;
   services.nscd.enable = false;
   services.ntp.enable = false;
