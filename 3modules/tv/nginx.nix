bb4c2ad9d93a3eb6bf0f26bccb65a7d364c1441f
tv <tv@shackspace.de>
2015-07-16 18:29:57 +0200

    3 tv.nginx: rm cruft

--- 3modules/tv/nginx.nix
+++ 3modules/tv/nginx.nix
@@ -69,14 +69,3 @@ let
 
 in
 out
-
-
-#let
-#  cfg = config.tv.nginx;
-#  arg' = arg // { inherit cfg; };
-#in
-#
-#{
-#  options.tv.nginx = import ./options.nix arg';
-#  config = lib.mkIf cfg.enable (import ./config.nix arg');
-#}
