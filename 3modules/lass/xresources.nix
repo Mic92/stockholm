95ee0e40b54225c9bfea049bfe366c3c27a912bf
lassulus <lass@aidsballs.de>
2015-07-16 15:49:57 +0200

    3 lass.xresources: init

---
+++ 3modules/lass/xresources.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, ... }:
+
+#TODO:
+#prefix with Attribute Name
+#ex: urxvt
+
+#
+#
+with builtins;
+with lib;
+
+
+let
+
+  inherit (import ../../4lib/tv { inherit pkgs lib; }) shell-escape;
+  inherit (pkgs) writeScript;
+
+in
+
+{
+
+  options = {
+    services.xresources.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable the automatic loading of Xresources definitions at display-manager start;
+      '';
+    };
+
+    services.xresources.resources = mkOption {
+      default = {};
+      type = types.attrsOf types.str;
+      example = {
+        urxvt = ''
+          URxvt*scrollBar: false
+          URxvt*urgentOnBell: true
+        '';
+      };
+      description = ''
+        Xresources definitions.
+      '';
+    };
+  };
+
+  config =
+    let
+      cfg = config.services.xresources;
+      xres = concatStringsSep "\n" (attrValues cfg.resources);
+
+    in mkIf cfg.enable {
+        services.xserver.displayManager.sessionCommands = ''
+          echo ${shell-escape xres} | xrdb -merge
+        '';
+      };
+
+}
