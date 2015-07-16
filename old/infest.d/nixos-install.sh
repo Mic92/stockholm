57c520b722f25f384301118046bf9cf182d4edd7
tv <tv@shackspace.de>
2015-07-16 23:22:30 +0200

    Goodbye old world, and thanks for all the fish!

--- old/infest.d/nixos-install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# usage: cat infest-nixos-install.sh | ./cac ssh ...
-set -euf
-nixos-install \
-    -I secrets=/etc/nixos/secrets \
-    -I retiolum-hosts=/etc/nixos/hosts \
-    -I pubkeys=/etc/nixos/pubkeys \
-    -I nixpkgs=/etc/nixos/nixpkgs
