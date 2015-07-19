d0a1d841189481c91ccd38cf79c5db834daa4f9f
tv <tv@shackspace.de>
2015-07-19 16:20:16 +0200

    Zpkgs tv genid: init

--- Zpkgs/tv/default.nix
+++ Zpkgs/tv/default.nix
@@ -3,6 +3,7 @@
 pkgs //
 {
   dic = pkgs.callPackage ./dic.nix {};
+  genid = pkgs.callPackage ./genid.nix {};
   github-hosts-sync = pkgs.callPackage ./github-hosts-sync.nix {};
   github-known_hosts = pkgs.callPackage ./github-known_hosts.nix {};
   much = pkgs.callPackage ./much.nix {};
---
+++ Zpkgs/tv/genid.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, ... }:
+
+pkgs.writeScriptBin "genid" ''
+  #! /bin/sh
+  # usage: genid NAME
+  set -euf
+
+  export PATH=${lib.makeSearchPath "bin" (with pkgs; [
+    bc
+    coreutils
+  ])}
+
+  name=$1
+  hash=$(printf %s "$name" | sha1sum | cut -d\  -f1 | tr a-f A-F)
+  echo "
+    min=2^16  # bigger than nobody and nogroup, see <nixos/modules/misc/ids.nix>
+    max=2^32  # see 2^(8*sizeof(uid_t))
+    ibase=16
+    ($hash + min) % max
+  " | bc
+''
