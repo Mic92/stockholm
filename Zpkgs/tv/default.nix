9736af46c0acf12113537bcaa2eb5d736b9d7c25
tv <tv@shackspace.de>
2015-07-19 16:01:23 +0200

    Zpkgs tv github-known_hosts: init

--- Zpkgs/tv/default.nix
+++ Zpkgs/tv/default.nix
@@ -3,5 +3,6 @@
 pkgs //
 {
   dic = pkgs.callPackage ./dic.nix {};
+  github-known_hosts = pkgs.callPackage ./github-known_hosts.nix {};
   much = pkgs.callPackage ./much.nix {};
 }
---
+++ Zpkgs/tv/github-known_hosts.nix
@@ -0,0 +1,13 @@
+{ lib, ... }:
+
+with builtins;
+with lib;
+
+let
+  github-pubkey = removeSuffix "\n" (readFile ../../Zpubkeys/github.ssh.pub);
+in
+
+toFile "github-known_hosts"
+  (concatMapStrings
+    (i: "github.com,192.30.252.${toString i} ${github-pubkey}\n")
+    (range 0 255))
