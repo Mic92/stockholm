979ac086eb0d54e8c3e0f84f082d1e63b887d2b8
tv <tv@shackspace.de>
2015-07-20 08:37:46 +0200

    Zpkgs tv: inherit (pkgs) callPackage

--- Zpkgs/tv/default.nix
+++ Zpkgs/tv/default.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 
+let
+  inherit (pkgs) callPackage;
+in
+
 pkgs //
 {
-  charybdis = pkgs.callPackage ./charybdis {};
-  dic = pkgs.callPackage ./dic.nix {};
-  genid = pkgs.callPackage ./genid.nix {};
-  github-hosts-sync = pkgs.callPackage ./github-hosts-sync.nix {};
-  github-known_hosts = pkgs.callPackage ./github-known_hosts.nix {};
-  much = pkgs.callPackage ./much.nix {};
-  viljetic-pages = pkgs.callPackage ./viljetic-pages {};
+  charybdis = callPackage ./charybdis {};
+  dic = callPackage ./dic.nix {};
+  genid = callPackage ./genid.nix {};
+  github-hosts-sync = callPackage ./github-hosts-sync.nix {};
+  github-known_hosts = callPackage ./github-known_hosts.nix {};
+  much = callPackage ./much.nix {};
+  viljetic-pages = callPackage ./viljetic-pages {};
 }
