cf53d56a92a67d0210d126acb60270ee5a4b4a21
tv <tv@shackspace.de>
2015-07-22 19:09:27 +0200

    Zpkgs tv genid: bump min

--- Zpkgs/tv/genid.nix
+++ Zpkgs/tv/genid.nix
@@ -13,7 +13,8 @@ pkgs.writeScriptBin "genid" ''
   name=$1
   hash=$(printf %s "$name" | sha1sum | cut -d\  -f1 | tr a-f A-F)
   echo "
-    min=2^16  # bigger than nobody and nogroup, see <nixos/modules/misc/ids.nix>
+    min=2^24  # bigger than nobody and nogroup, see <nixos/modules/misc/ids.nix>
+              # and some spare for stuff like lxd.
     max=2^32  # see 2^(8*sizeof(uid_t))
     ibase=16
     ($hash + min) % max
