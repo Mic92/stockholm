6476abd6ac7e000d0759569a1e2754acb2f518ca
lassulus <lass@aidsballs.de>
2015-07-24 14:00:28 +0200

    2 lass.fastpoke-pages: use functions for static

--- 2configs/lass/fastpoke-pages.nix
+++ 2configs/lass/fastpoke-pages.nix
@@ -1,10 +1,37 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-{
+
+let
+  createStaticPage = domain:
+    {
+      tv.nginx.servers."${domain}" = {
+        server-names = [
+          "${domain}"
+          "www.${domain}"
+        ];
+        locations = [
+          (nameValuePair "/" ''
+            root /var/lib/http/${domain};
+          '')
+        ];
+      };
+      #networking.extraHosts = ''
+      #  10.243.206.102 ${domain}
+      #'';
+    };
+
+in {
   imports = [
     ../../3modules/tv/nginx.nix
     ../../3modules/lass/iptables.nix
+  ] ++ map createStaticPage [
+    "habsys.de"
+    "pixelpocket.de"
+    "karlaskop.de"
+    "ubikmedia.de"
+    "apanowicz.de"
+    "aidsballs.de"
   ];
 
   lass.iptables = {
@@ -15,72 +42,46 @@ with lib;
     };
   };
 
-  #createStaticPage = domain:
-  #  {
-  #    irc.nginx.servers."${domain}" = {
-  #      server-names = [
-  #        "${domain}"
-  #        "www.${domain}"
-  #      ];
-  #      locations = [
-  #        (nameValuePair "/" ''
-  #          root /var/lib/http/${domain};
-  #        '')
-  #      ];
-  #    };
-  #    networking.extraHosts = ''
-  #      10.243.206.102 ${domain}
-  #    '';
-  #  };
-
-  #map createStaticPage [
-  #  "habsys.de"
-  #  "pixelpocket.de"
-  #  "karlaskop.de"
-  #  "ubikmedia.de"
-  #  "apanowicz.de"
-  #  "aidsballs.de"
-  #];
 
   tv.nginx = {
     enable = true;
     servers = {
 
-      "habsys.de" = {
-        server-names = [
-          "habsys.de"
-          "www.habsys.de"
-        ];
-        locations = [
-          (nameValuePair "/" ''
-            root /var/lib/http/habsys.de;
-          '')
-        ];
-      };
+      #"habsys.de" = {
+      #  server-names = [
+      #    "habsys.de"
+      #    "www.habsys.de"
+      #  ];
+      #  locations = [
+      #    (nameValuePair "/" ''
+      #      root /var/lib/http/habsys.de;
+      #    '')
+      #  ];
+      #};
 
-      "karlaskop.de" = {
-        server-names = [
-          "karlaskop.de"
-          "www.karlaskop.de"
-        ];
-        locations = [
-          (nameValuePair "/" ''
-            root /var/lib/http/karlaskop.de;
-          '')
-        ];
-      };
+      #"karlaskop.de" = {
+      #  server-names = [
+      #    "karlaskop.de"
+      #    "www.karlaskop.de"
+      #  ];
+      #  locations = [
+      #    (nameValuePair "/" ''
+      #      root /var/lib/http/karlaskop.de;
+      #    '')
+      #  ];
+      #};
 
-      "pixelpocket.de" = {
-        server-names = [
-          "pixelpocket.de"
-          "www.karlaskop.de"
-        ];
-        locations = [
-          (nameValuePair "/" ''
-            root /var/lib/http/karlaskop.de;
-          '')
-        ];
-      };
+      #"pixelpocket.de" = {
+      #  server-names = [
+      #    "pixelpocket.de"
+      #    "www.karlaskop.de"
+      #  ];
+      #  locations = [
+      #    (nameValuePair "/" ''
+      #      root /var/lib/http/karlaskop.de;
+      #    '')
+      #  ];
+      #};
 
     };
   };
