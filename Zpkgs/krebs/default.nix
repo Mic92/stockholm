78c12a8adca9f55e8faa5c86f102050c8a557b64
tv <tv@shackspace.de>
2015-07-24 23:19:49 +0200

    Zpkgs {tv -> krebs} genid

--- Zpkgs/krebs/default.nix
+++ Zpkgs/krebs/default.nix
@@ -6,6 +6,7 @@ in
 
 pkgs //
 {
+  genid = callPackage ./genid.nix {};
   github-hosts-sync = callPackage ./github-hosts-sync.nix {};
   github-known_hosts = callPackage ./github-known_hosts.nix {};
   hashPassword = callPackage ./hashPassword.nix {};
--- Zpkgs/tv/default.nix
+++ Zpkgs/tv/default.nix
@@ -8,7 +8,6 @@ in
 krebs // {
   charybdis = callPackage ./charybdis {};
   dic = callPackage ./dic.nix {};
-  genid = callPackage ./genid.nix {};
   lentil = callPackage ./lentil {};
   much = callPackage ./much.nix {};
   viljetic-pages = callPackage ./viljetic-pages {};
