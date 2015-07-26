f811bc0144b7268031a960d85e0dfee35d5e8fed
tv <tv@shackspace.de>
2015-07-26 14:03:39 +0200

    1 tv wu: systemPackages += ff

--- 1systems/tv/wu.nix
+++ 1systems/tv/wu.nix
@@ -29,6 +29,12 @@ in
         Zpkgs.genid
         Zpkgs.hashPassword
         Zpkgs.lentil
+        (pkgs.writeScriptBin "ff" ''
+          #! ${pkgs.bash}/bin/bash
+          exec sudo -u ff -i <<EOF
+          exec ${pkgs.firefoxWrapper}/bin/firefox $(printf " %q" "$@")
+          EOF
+        '')
 
         # root
         cryptsetup
@@ -363,7 +369,6 @@ in
     };
   };
 
-  nixpkgs.config.firefox.enableAdobeFlash = true;
   nixpkgs.config.chromium.enablePepperFlash = true;
 
   nixpkgs.config.allowUnfree = true;
