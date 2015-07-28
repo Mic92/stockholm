2de7a7f6b97d9976291bd8f8255a627695fb79fc
makefu <github@syntax-fehler.de>
2015-07-28 19:59:02 +0200

    identity: makefu add email

--- 3modules/krebs/default.nix
+++ 3modules/krebs/default.nix
@@ -104,7 +104,7 @@ let
     };
   };
 
-  makefu-imp = { 
+  makefu-imp = {
     hosts = addNames {
       pnp = {
         cores = 1;
@@ -133,6 +133,7 @@ let
     };
     users = addNames {
       makefu = {
+        mail = "root@euer.krebsco.de";
         pubkey = readFile ../../Zpubkeys/makefu_arch.ssh.pub;
       };
     };
