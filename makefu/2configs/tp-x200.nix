8b302ea8866fb6f0703f34540f31cf5871440e53
makefu <makefu@tsp>
2015-08-09 13:17:37 +0000

    makefu: x200 - add middle mouse scroll

--- makefu/2configs/tp-x200.nix
+++ makefu/2configs/tp-x200.nix
@@ -20,4 +20,9 @@ with lib;
   hardware.trackpoint.enable = true;
   hardware.trackpoint.sensitivity = 255;
   hardware.trackpoint.speed = 255;
+  services.xserver.displayManager.sessionCommands = ''
+    xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 1
+    xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 2
+    xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 200
+  '';
 }
