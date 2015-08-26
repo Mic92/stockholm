8c5ca07413e983208ced2fc5b67dd84fb70482a4
makefu <github@syntax-fehler.de>
2015-08-26 17:11:05 +0200

    add vaapi for tp-x220
    this resolves the issue of vlc being slow after suspend

--- makefu/2configs/tp-x220.nix
+++ makefu/2configs/tp-x220.nix
@@ -8,13 +8,21 @@ with lib;
   boot.kernelModules = [ "kvm-intel" ];
 
   #services.xserver.vaapiDrivers = [pkgs.vaapiIntel pkgs.vaapiVdpau ];
-  services.xserver.vaapiDrivers = [];
+
+  services.xserver = {
+    videoDriver = "intel";
+    vaapiDrivers = [ pkgs.vaapiIntel ];
+    deviceSection = ''
+      Option "AccelMethod" "sna"
+      BusID "PCI:0:2:0"
+    '';
+  };
 
   services.xserver.displayManager.sessionCommands =''
   xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
   xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
-  xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
   xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
+  # xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
   '';
 
 }
