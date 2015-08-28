8827469a35852a0623d8353c22b481a15328254a
tv <tv@shackspace.de>
2015-08-28 21:43:16 +0200

    krebs pkgs += execve

--- krebs/5pkgs/default.nix
+++ krebs/5pkgs/default.nix
@@ -1,11 +1,13 @@
 { lib, pkgs, ... }:
 
+with import ../4lib { inherit lib; };
+
 let
   inherit (pkgs) callPackage;
 in
 
 pkgs //
-{
+rec {
   cac = callPackage ./cac.nix {};
   dic = callPackage ./dic.nix {};
   genid = callPackage ./genid.nix {};
@@ -14,6 +16,20 @@ pkgs //
   hashPassword = callPackage ./hashPassword.nix {};
   nq = callPackage ./nq.nix {};
   posix-array = callPackage ./posix-array.nix {};
+
+  execve = name: { filename, argv, envp }:
+    writeC name {} ''
+      #include <unistd.h>
+      int main () {
+        const char *filename = ${toC filename};
+        char *const argv[] = ${toC (argv ++ [null])};
+        char *const envp[] = ${toC (
+          mapAttrsToList (k: v: "${k}=${v}") envp ++ [null]
+        )};
+        execve(filename, argv, envp);
+        return -1;
+      }
+    '';
 
   writeC = name: {}: src: pkgs.runCommand name {} ''
     PATH=${lib.makeSearchPath "bin" (with pkgs; [
