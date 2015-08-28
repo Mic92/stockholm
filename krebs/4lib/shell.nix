f6997378c91be84432703183397f8675143030e9
tv <tv@shackspace.de>
2015-08-28 20:11:03 +0200

    krebs shell: add cat

--- krebs/4lib/shell.nix
+++ krebs/4lib/shell.nix
@@ -12,4 +12,11 @@ rec {
       if isSafeChar c then c
       else if c == "\n" then "'\n'"
       else "\\${c}");
+
+  #
+  # shell script generators
+  #
+
+  # example: "${cat (toJSON { foo = "bar"; })} | jq -r .foo"
+  cat = s: "printf '%s' ${escape s}";
 }
