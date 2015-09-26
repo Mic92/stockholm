0d806f61d2709f0ff8bf03ee431839035215e689
lassulus <lass@aidsballs.de>
2015-09-26 12:12:50 +0200

    lass: add firefoxPatched + firefoxPlugins

--- lass/5pkgs/default.nix
+++ lass/5pkgs/default.nix
@@ -8,4 +8,9 @@ rec {
   bitlbee-dev = callPackage ./bitlbee-dev.nix {};
   bitlbee-steam = callPackage ./bitlbee-steam.nix { inherit bitlbee-dev; };
   bitlbee = callPackage ./bitlbee.nix { inherit bitlbee-steam; };
+  firefoxPlugins = {
+    noscript = callPackage ./firefoxPlugins/noscript.nix {};
+    ublock = callPackage ./firefoxPlugins/ublock.nix {};
+    vimperator = callPackage ./firefoxPlugins/vimperator.nix {};
+  };
 }
