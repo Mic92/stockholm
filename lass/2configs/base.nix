a6812b4440e095287b6f2228a78b65c7629866de
lassulus <lass@aidsballs.de>
2015-09-30 11:55:39 +0200

    lass: make compatible with new krebs.build

--- lass/2configs/base.nix
+++ lass/2configs/base.nix
@@ -42,9 +42,11 @@ with lib;
     enable = true;
     search-domain = "retiolum";
     exim-retiolum.enable = true;
-    build.deps.nixpkgs = {
-      url = https://github.com/Lassulus/nixpkgs;
-      rev = "68bd8e4a9dc247726ae89cc8739574261718e328";
+    build.source = {
+      git.nixpkgs = {
+        url = https://github.com/Lassulus/nixpkgs;
+        rev = "68bd8e4a9dc247726ae89cc8739574261718e328";
+      };
     };
   };
 
