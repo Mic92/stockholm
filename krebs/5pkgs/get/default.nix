655f510425848b2718773372ce7d6f297276ccce
tv <tv@shackspace.de>
2015-10-02 15:13:03 +0200

    get: 1.3.0 -> 1.3.1

--- krebs/5pkgs/get/default.nix
+++ krebs/5pkgs/get/default.nix
@@ -1,12 +1,12 @@
 { coreutils, gnugrep, gnused, fetchgit, jq, nix, stdenv, ... }:
 
 stdenv.mkDerivation {
-  name = "get-1.3.0";
+  name = "get-1.3.1";
 
   src = fetchgit {
     url = http://cgit.cd.retiolum/get;
-    rev = "fbe8f8d12ede9762fceb15b9944b69a4ee6331eb";
-    sha256 = "bcdf036f8b5d1467285d0998aeac7e48280adfb9e1278f9f424c9c8b5e6ed8fa";
+    rev = "64c97edd3f9952cd5e703208c46748a035a515bf";
+    sha256 = "32ca83f4fd86fd3285bef9dcfd0917308086d239189858daceca175de49ff97c";
   };
 
   phases = [
