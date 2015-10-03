041ef784486b554374fd5ad9e172889ad7e631d2
makefu <github@syntax-fehler.de>
2015-10-03 19:57:38 +0200

    add alsa-tools, working tinc_graphs

---
+++ makefu/5pkgs/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+let
+  inherit (pkgs) callPackage;
+in
+{
+   alsa-hdspmixer = callPackage ./alsa-tools { alsaToolTarget="hdspmixer";};
+  alsa-hdspconf = callPackage ./alsa-tools { alsaToolTarget="hdspconf";};
+  alsa-hdsploader = callPackage ./alsa-tools { alsaToolTarget="hdsploader";};
+  tinc_graphs = callPackage ./tinc_graphs {};
+}
---
+++ makefu/5pkgs/tinc_graphs/default.nix
@@ -0,0 +1,23 @@
+{stdenv,fetchurl,pkgs,python3Packages, ... }:
+
+python3Packages.buildPythonPackage rec {
+  name = "tinc_graphs-${version}";
+  version = "0.2.8";
+  propagatedBuildInputs = with pkgs;[
+    python3Packages.pygeoip
+    ## ${geolite-legacy}/share/GeoIP/GeoIPCity.dat
+  ];
+  src = fetchurl {
+    url = "https://pypi.python.org/packages/source/t/tinc_graphs/tinc_graphs-${version}.tar.gz";
+    sha256 = "1rvy07ybjpqlsf9qizdp0zyq4ibd6w74k4glbbkw0x7j3j2skzdk";
+  };
+  preFixup = with pkgs;''
+    wrapProgram $out/bin/build-graphs --prefix PATH : "${imagemagick}/bin:${graphviz}/bin"
+  '';
+  meta = {
+    homepage = http://krebsco.de/;
+    description = "Create Graphs from Tinc Stats";
+    license = stdenv.lib.licenses.wtfpl;
+  };
+}
+
