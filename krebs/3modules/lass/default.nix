829b8303ce302454b02e3bf1e859ac786684449f
lassulus <lass@aidsballs.de>
2015-10-04 15:16:14 +0200

    krebs 3 lass: bump echelon ip (again)

--- krebs/3modules/lass/default.nix
+++ krebs/3modules/lass/default.nix
@@ -9,7 +9,7 @@ with import ../../4lib { inherit lib; };
       dc = "lass"; #dc = "cac";
       nets = rec {
         internet = {
-          addrs4 = ["162.244.30.241"];
+          addrs4 = ["167.88.34.158"];
           aliases = [
             "echelon.internet"
           ];
