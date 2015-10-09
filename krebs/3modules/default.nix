9ae6d1611ef8cd8479235ac91272e694080b9d89
tv <tv@shackspace.de>
2015-10-09 12:06:00 +0200

    add github.com and its addrs4 to known_hosts

--- krebs/3modules/default.nix
+++ krebs/3modules/default.nix
@@ -137,6 +137,13 @@ let
         mkIf (privkey != null) (mkForce [privkey]);
 
       services.openssh.knownHosts =
+        {
+          github = {
+            hostNames = ["github.com"] ++
+              map (i: "192.30.252.${toString i}") (range 0 255);
+            publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
+          };
+        } //
         mapAttrs
           (name: host: {
             hostNames =
