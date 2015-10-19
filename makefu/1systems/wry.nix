7656868d1b3232d5a17ed422c25c38e37c81e588
makefu <github@syntax-fehler.de>
2015-10-19 19:48:56 +0200

    m 1 wry: start Reaktor

--- makefu/1systems/wry.nix
+++ makefu/1systems/wry.nix
@@ -5,23 +5,29 @@ let
   ip = (lib.head config.krebs.build.host.nets.internet.addrs4);
 in {
   imports = [
-    ../../tv/2configs/CAC-CentOS-7-64bit.nix
-    ../2configs/base.nix
-    ../2configs/base-sources.nix
-    ../2configs/tinc-basic-retiolum.nix
+      # TODO: copy this config or move to krebs
+      ../../tv/2configs/CAC-CentOS-7-64bit.nix
+      ../2configs/base.nix
+      ../2configs/base-sources.nix
+      ../2configs/tinc-basic-retiolum.nix
+
+      # Reaktor
+      ../2configs/Reaktor/simpleExtend.nix
   ];
+
+  krebs.Reaktor.enable = true;
 
   networking.firewall.allowPing = true;
   networking.interfaces.enp2s1.ip4 = [
-      {
-        address = ip;
-        prefixLength = 24;
-      }
-    ];
-    networking.defaultGateway = "104.233.87.1";
-    networking.nameservers = [
-      "8.8.8.8"
-    ];
+  {
+    address = ip;
+    prefixLength = 24;
+  }
+  ];
+  networking.defaultGateway = "104.233.87.1";
+  networking.nameservers = [
+    "8.8.8.8"
+  ];
 
   # based on ../../tv/2configs/CAC-Developer-2.nix
   sound.enable = false;
