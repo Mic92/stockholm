952c03b3742cc1a979e4a1c148d083f1334cfe65
makefu <github@syntax-fehler.de>
2015-10-19 20:02:29 +0200

    krebs 5 retiolum-bootstrap: init new_install.sh package

---
+++ krebs/5pkgs/retiolum-bootstrap/default.nix
@@ -0,0 +1,6 @@
+{ fetchurl }:
+
+fetchurl {
+  url = https://raw.githubusercontent.com/krebscode/painload/master/retiolum/scripts/tinc_setup/new_install.sh; 
+  sha256 = "03kmil8q2xm3rdm2jxyah7vww84pw6w01d0c3siid9zpn2j7la9s";
+}
