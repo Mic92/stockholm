bbd28caa40c06574653fc8364157721c562eabbb
makefu <github@syntax-fehler.de>
2015-10-28 00:42:36 +0100

    krebs 5 collectd-connect-time: bump version (again)

--- krebs/5pkgs/collectd-connect-time/default.nix
+++ krebs/5pkgs/collectd-connect-time/default.nix
@@ -2,10 +2,10 @@
 
 pythonPackages.buildPythonPackage rec {
   name = "collectd-connect-time-${version}";
-  version = "0.2.0";
+  version = "0.2.1";
   src = fetchurl {
     url = "https://pypi.python.org/packages/source/c/collectd-connect-time/collectd-connect-time-${version}.tar.gz";
-    sha256 = "1l63pi79z6vg4xf744a71g5rxgsngpkc96a7y4ha2x798r55g7v6";
+    sha256 = "0611h53ww9lk1qm27njsffckkibirmq7p8cxlq02dgl1zbh7583d";
   };
   meta = {
     homepage = https://pypi.python.org/pypi/collectd-connect-time/;
