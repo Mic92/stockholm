23424a3a2ae7fd489547c453374d6ffc551c5483
makefu <github@syntax-fehler.de>
2015-10-28 21:27:39 +0100

    m 5 tw-upload-plugin: use new version

--- makefu/5pkgs/tw-upload-plugin/default.nix
+++ makefu/5pkgs/tw-upload-plugin/default.nix
@@ -3,6 +3,6 @@
 pkgs.fetchFromGitHub {
   owner = "makefu";
   repo = "tw-upload-plugin";
-  rev = "4a99de";
-  sha256 = "0y7bqzi4fmm3lxb7pbgicph5r31xlvkkz4a1pklsrb7vjwsyb91m";
+  rev = "a00aac";
+  sha256 = "0kazqs24kzjxqzr33kg1jbfx8xyvmrnrdxh6g27kgkgbl1d2qknh";
 }
