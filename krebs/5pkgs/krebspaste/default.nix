12597b1febb0bc47cf98529a12e5fc6af1d8f5a4
makefu <github@syntax-fehler.de>
2015-11-05 16:21:09 +0100

    k 5 krebspaste: resolve TODO, retiolum is always secure

--- krebs/5pkgs/krebspaste/default.nix
+++ krebs/5pkgs/krebspaste/default.nix
@@ -1,9 +1,7 @@
 { writeScriptBin, pkgs }:
 
-# TODO: add krebs CA to toolchain, remove --insecure
 # TODO: use `wrapProgram --add-flags` instead?
-
 writeScriptBin "krebspaste" ''
   #! /bin/sh
-  exec ${pkgs.bepasty-client-cli}/bin/bepasty-cli --insecure --url http://paste.retiolum "$@"
+  exec ${pkgs.bepasty-client-cli}/bin/bepasty-cli --url http://paste.retiolum "$@"
 ''
