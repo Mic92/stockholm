e57ce8bc98ba3d90b0044d90f4bff10475354fd1
tv <tv@krebsco.de>
2015-11-07 19:21:01 +0100

    xu nixpkgs: c44a593 -> 7ae05ed

--- tv/1systems/xu.nix
+++ tv/1systems/xu.nix
@@ -5,6 +5,9 @@ with lib;
 {
   krebs.build.host = config.krebs.hosts.xu;
 
+  krebs.build.source.git.nixpkgs.rev =
+    "7ae05edcdd14f6ace83ead9bf0d114e97c89a83a";
+
   imports = [
     ../2configs/hw/x220.nix
     #../2configs/consul-client.nix
