fa48986f3f1358fea0af9bbfe908032ae50c0034
tv <tv@krebsco.de>
2015-11-08 13:42:14 +0100

    cd: use default krebs.build

--- tv/1systems/cd.nix
+++ tv/1systems/cd.nix
@@ -4,22 +4,7 @@ with lib;
 
 {
   krebs.build.host = config.krebs.hosts.cd;
-  krebs.build.user = config.krebs.users.tv;
-
   krebs.build.target = "root@cd.internet";
-
-  krebs.build.source = {
-    git.nixpkgs = {
-      url = https://github.com/NixOS/nixpkgs;
-      rev = "c44a593aa43bba6a0708f6f36065a514a5110613";
-    };
-    dir.secrets = {
-      path = "/home/tv/secrets/cd";
-    };
-    dir.stockholm = {
-      path = "/home/tv/stockholm";
-    };
-  };
 
   imports = [
     ../2configs/hw/CAC-Developer-2.nix
