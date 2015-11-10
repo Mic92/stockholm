7ce26169db88cf68ab802a68ed55a3ed7b9be33a
tv <tv@krebsco.de>
2015-11-10 12:44:05 +0100

    tv: setup XDG_RUNTIME_DIR for users

--- tv/2configs/default.nix
+++ tv/2configs/default.nix
@@ -167,5 +167,13 @@ with lib;
         "sendmail"  # for sudo
       ];
     }
+    {
+      systemd.tmpfiles.rules = let
+        forUsers = flip map users;
+        isUser = { group, ... }: hasSuffix "users" group;
+        users = filter isUser (mapAttrsToList (_: id) config.users.users);
+      in forUsers (u: "d /run/xdg/${u.name} 0700 ${u.name} ${u.group} -");
+      environment.variables.XDG_RUNTIME_DIR = "/run/xdg/$LOGNAME";
+    }
   ];
 }
