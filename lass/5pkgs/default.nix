2e2e5196d149379643244f92239f88f5d2eb2237
lassulus <lass@aidsballs.de>
2015-11-13 14:56:37 +0100

    l: use bitlbee plugin architecture from nixpkgs

--- lass/5pkgs/default.nix
+++ lass/5pkgs/default.nix
@@ -5,9 +5,6 @@ let
 in
 
 rec {
-  #bitlbee-dev = callPackage ./bitlbee-dev.nix {};
-  #bitlbee-steam = callPackage ./bitlbee-steam.nix { inherit bitlbee-dev; };
-  #bitlbee = callPackage ./bitlbee.nix { inherit bitlbee-steam; };
   firefoxPlugins = {
     noscript = callPackage ./firefoxPlugins/noscript.nix {};
     ublock = callPackage ./firefoxPlugins/ublock.nix {};
