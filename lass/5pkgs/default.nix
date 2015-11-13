07dca519636f07ee4887e14e0e9a9739ec9f8034
lassulus <lass@aidsballs.de>
2015-11-13 14:53:26 +0100

    l 5 go -> k 5 go

--- lass/5pkgs/default.nix
+++ lass/5pkgs/default.nix
@@ -5,15 +5,14 @@ let
 in
 
 rec {
-  bitlbee-dev = callPackage ./bitlbee-dev.nix {};
-  bitlbee-steam = callPackage ./bitlbee-steam.nix { inherit bitlbee-dev; };
-  bitlbee = callPackage ./bitlbee.nix { inherit bitlbee-steam; };
+  #bitlbee-dev = callPackage ./bitlbee-dev.nix {};
+  #bitlbee-steam = callPackage ./bitlbee-steam.nix { inherit bitlbee-dev; };
+  #bitlbee = callPackage ./bitlbee.nix { inherit bitlbee-steam; };
   firefoxPlugins = {
     noscript = callPackage ./firefoxPlugins/noscript.nix {};
     ublock = callPackage ./firefoxPlugins/ublock.nix {};
     vimperator = callPackage ./firefoxPlugins/vimperator.nix {};
   };
-  go = callPackage ./go/default.nix {};
   newsbot-js = callPackage ./newsbot-js/default.nix {};
   xmonad-lass =
     let src = pkgs.writeNixFromCabal "xmonad-lass.nix" ./xmonad-lass; in
