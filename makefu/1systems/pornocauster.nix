cf5a1ba6bcf657396bc6b8c2fbc32143d27849d0
makefu <github@syntax-fehler.de>
2015-12-15 21:18:29 +0100

    m 1 pornocauster: use latest buildbot for stable build

--- makefu/1systems/pornocauster.nix
+++ makefu/1systems/pornocauster.nix
@@ -36,11 +36,17 @@
       # ../2configs/mediawiki.nix
       #../2configs/wordpress.nix
     ];
-  nixpkgs.config.packageOverrides = pkgs: { tinc = pkgs.tinc_pre; };
+  nixpkgs.config.packageOverrides = pkgs: {
+    tinc = pkgs.tinc_pre;
+    buildbot = let
+      pkgs1509 = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {};
+      in pkgs1509.buildbot;
+  };
+  makefu.buildbot.master.enable = true;
+
   #krebs.Reaktor.enable = true;
   #krebs.Reaktor.nickname = "makefu|r";
-
-  krebs.build.host = config.krebs.hosts.pornocauster;
+  # nix.binaryCaches = [ "http://acng.shack/nixos" "https://cache.nixos.org" ];
 
   environment.systemPackages = with pkgs;[
     get
@@ -58,4 +64,5 @@
     25
   ];
 
+  krebs.build.host = config.krebs.hosts.pornocauster;
 }
