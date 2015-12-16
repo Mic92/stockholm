cf033f051a14e90e7526fee0d63eef86ba3c75fc
makefu <github@syntax-fehler.de>
2015-12-16 11:40:48 +0100

    m 2 zsh-user: load gpg-agent, obsoletes oh-my-zsh ssh plugin

--- makefu/2configs/zsh-user.nix
+++ makefu/2configs/zsh-user.nix
@@ -22,6 +22,16 @@ in
 
       autoload -U compinit && compinit
       zstyle ':completion:*' menu select
+
+      # load gpg-agent
+      envfile="$HOME/.gnupg/gpg-agent.env"
+      if [ -e "$envfile" ] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+        eval "$(cat "$envfile")"
+      else
+        eval "$(${pkgs.gnupg}/bin/gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
+      fi
+      export GPG_AGENT_INFO
+      export SSH_AUTH_SOCK
       '';
 
     promptInit = ''
