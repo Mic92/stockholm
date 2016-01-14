7a0480e5fe2b0b0762e56caa1878f7ada8cb76e1
makefu <github@syntax-fehler.de>
2016-01-14 17:30:02 +0100

    ma 1 minimal: cosmetics

--- shared/1systems/test-minimal-deploy.nix
+++ shared/1systems/test-minimal-deploy.nix
@@ -5,7 +5,7 @@
     build.user = config.krebs.users.shared;
     build.host = config.krebs.hosts.test-all-krebs-modules;
   };
-  # just get the system running
+  # just get the system to eval in nixos without errors
   boot.loader.grub.devices = ["/dev/sda"];
   fileSystems."/" = {
     device = "/dev/lol";
