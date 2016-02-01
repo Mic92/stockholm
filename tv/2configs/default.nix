8217472e99ad5c9392a4da443dd2fe66343c8668
tv <tv@krebsco.de>
2016-02-01 03:31:24 +0100

    tv config: isUser root == true

--- tv/2configs/default.nix
+++ tv/2configs/default.nix
@@ -180,7 +180,8 @@ with lib;
     {
       systemd.tmpfiles.rules = let
         forUsers = flip map users;
-        isUser = { group, ... }: hasSuffix "users" group;
+        isUser = { name, group, ... }:
+          name == "root" || hasSuffix "users" group;
         users = filter isUser (mapAttrsToList (_: id) config.users.users);
       in forUsers (u: "d /run/xdg/${u.name} 0700 ${u.name} ${u.group} -");
       environment.variables.XDG_RUNTIME_DIR = "/run/xdg/$LOGNAME";
