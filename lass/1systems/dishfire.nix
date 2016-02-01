138bdc6bf6a18a59cf47d2d2db7c4e7640f50641
lassulus <lass@aidsballs.de>
2016-02-01 16:18:36 +0100

    l 1 dishfire: import git.nix

--- lass/1systems/dishfire.nix
+++ lass/1systems/dishfire.nix
@@ -4,6 +4,7 @@
   imports = [
     <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
     ../2configs/base.nix
+    ../2configs/git.nix
     ../2configs/websites/fritz.nix
     {
       boot.loader.grub = {
