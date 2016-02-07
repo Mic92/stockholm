5a10c4beb56d052b816682058becedf7b8eacaaa
tv <tv@krebsco.de>
2016-02-08 00:25:35 +0100

    tv backup: xu:/home → wu:/bku/xu-home @ 06:00

--- tv/2configs/backup.nix
+++ tv/2configs/backup.nix
@@ -14,5 +14,17 @@ with lib;
         yearly   = { format = "%Y";                       };
       };
     };
+    xu-home-wu = {
+      method = "push";
+      src = { host = config.krebs.hosts.xu; path = "/home"; };
+      dst = { host = config.krebs.hosts.wu; path = "/bku/xu-home"; };
+      startAt = "06:00";
+      snapshots = {
+        daily    = { format = "%Y-%m-%d";    retain =  7; };
+        weekly   = { format = "%YW%W";       retain =  4; };
+        monthly  = { format = "%Y-%m";       retain = 12; };
+        yearly   = { format = "%Y";                       };
+      };
+    };
   };
 }
