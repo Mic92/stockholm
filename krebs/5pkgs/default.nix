7f7256a76f5698a9f8599ce71780f47ab13590f4
tv <tv@krebsco.de>
2016-02-08 01:23:06 +0100

    krebs pkgs: move builders to dedicated file

--- krebs/5pkgs/default.nix
+++ krebs/5pkgs/default.nix
@@ -1,14 +1,6 @@
-{ lib, pkgs, ... }:
-
+{ lib, pkgs, ... }@args:
 with lib;
-let
-  subdirs = mapAttrs (_: flip pkgs.callPackage {})
-                     (filterAttrs (_: dir.has-default-nix)
-                                  (subdirsOf ./.));
-in
-
-subdirs // rec {
-
+{
   haskellPackages = pkgs.haskellPackages.override {
     overrides = self: super:
       mapAttrs (name: path: self.callPackage path {})
@@ -29,55 +21,11 @@ subdirs // rec {
 
   ReaktorPlugins = pkgs.callPackage ./Reaktor/plugins.nix {};
 
-  execve = name: { filename, argv, envp ? {}, destination ? "" }:
-    writeC name { inherit destination; } ''
-      #include <unistd.h>
-      int main () {
-        const char *filename = ${toC filename};
-        char *const argv[] = ${toC (argv ++ [null])};
-        char *const envp[] = ${toC (
-          mapAttrsToList (k: v: "${k}=${v}") envp ++ [null]
-        )};
-        execve(filename, argv, envp);
-        return -1;
-      }
-    '';
-
   test = {
     infest-cac-centos7 = pkgs.callPackage ./test/infest-cac-centos7 {};
   };
-
-  execveBin = name: cfg: execve name (cfg // { destination = "/bin/${name}"; });
-
-  writeC = name: { destination ? "" }: src: pkgs.runCommand name {} ''
-    PATH=${makeSearchPath "bin" (with pkgs; [
-      binutils
-      coreutils
-      gcc
-    ])}
-    src=${pkgs.writeText "${name}.c" src}
-    exe=$out${destination}
-    mkdir -p "$(dirname "$exe")"
-    gcc -O -Wall -o "$exe" $src
-    strip --strip-unneeded "$exe"
-  '';
-
-  writeDash = name: text: pkgs.writeScript name ''
-    #! ${pkgs.dash}/bin/dash
-    ${text}
-  '';
-
-  writeDashBin = name: text: pkgs.writeTextFile {
-    executable = true;
-    destination = "/bin/${name}";
-    name = name;
-    text = ''
-      #! ${pkgs.dash}/bin/dash
-      ${text}
-    '';
-  };
-
-  writeNixFromCabal = name: path: pkgs.runCommand name {} ''
-    ${pkgs.cabal2nix}/bin/cabal2nix ${path} > $out
-  '';
 }
+// import ./builders.nix args
+// mapAttrs (_: flip pkgs.callPackage {})
+            (filterAttrs (_: dir.has-default-nix)
+                         (subdirsOf ./.))
