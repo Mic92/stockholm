08b03ef2e7dc6eff237c213ee341d0da6b9a0d96
makefu <github@syntax-fehler.de>
2016-02-12 02:41:38 +0100

    k 5 repo-sync: 0.2.0 -> 0.2.5

--- krebs/5pkgs/repo-sync/default.nix
+++ krebs/5pkgs/repo-sync/default.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, python3Packages, fetchurl, ... }:
 with python3Packages; buildPythonPackage rec {
   name = "repo-sync-${version}";
-  version = "0.2.0";
+  version = "0.2.5";
   disabled = isPy26 || isPy27;
   propagatedBuildInputs = [
     docopt
@@ -9,7 +9,7 @@ with python3Packages; buildPythonPackage rec {
   ];
   src = fetchurl {
     url = "https://pypi.python.org/packages/source/r/repo-sync/repo-sync-${version}.tar.gz";
-    sha256 = "161ssq4138c327p5d68vy91psldal7vp61vk3xdqkhpzd2nz5ag5";
+    sha256 = "1a59bj0vc5ajq8indkvkdk022yzvvv5mjb57hk3xf1j3wpr85p84";
   };
   meta = {
     homepage = http://github.com/makefu/repo-sync;
