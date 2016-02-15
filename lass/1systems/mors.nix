0e03417f2e214795320c0a0f75b10d1bfbdf8648
lassulus <lass@aidsballs.de>
2016-02-15 16:58:04 +0100

    l 1 mors: activate postgresql

--- lass/1systems/mors.nix
+++ lass/1systems/mors.nix
@@ -130,6 +130,12 @@
         };
       };
     }
+    {
+      services.postgresql = {
+        enable = true;
+        authentication = "local all all ident";
+      };
+    }
   ];
 
   krebs.build.host = config.krebs.hosts.mors;
