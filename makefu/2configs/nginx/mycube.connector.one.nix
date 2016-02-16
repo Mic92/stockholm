b9eae4bf0986a04fb2b19b9da98e6aa93151eb44
makefu <github@syntax-fehler.de>
2016-02-16 22:44:00 +0100

    ma 2 mycube.connector.one: init

--- makefu/2configs/nginx/mycube.connector.one.nix
+++ makefu/2configs/nginx/mycube.connector.one.nix
@@ -5,20 +5,22 @@ let
   hostname = config.krebs.build.host.name;
   external-ip = head config.krebs.build.host.nets.internet.addrs4;
 in {
+  services.redis.enable = true;
+
   krebs.nginx = {
     enable = mkDefault true;
     servers = {
-      update-connector-one = {
+      mybox-connector-one = {
         listen = [ "${external-ip}:80" ];
         server-names = [
-          "update.connector.one"
-          "firmware.connector.one"
+          "mycube.connector.one"
+          "mybox.connector.one"
         ];
         locations = singleton (nameValuePair "/" ''
-          autoindex on;
-          root /var/www/update.connector.one;
-          sendfile on;
-          gzip on;
+          proxy_set_header   Host $host;
+          proxy_set_header   X-Real-IP          $remote_addr;
+          proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_pass http://localhost:8001/;
         '');
       };
     };
