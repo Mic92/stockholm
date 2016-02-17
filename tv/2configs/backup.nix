d71a8863ac10f34bfe30f950f32bbf57427e2ca7
tv <tv@krebsco.de>
2016-02-17 17:30:39 +0100

    tv krebs.backup.plans.nomic-home-xu: init

--- tv/2configs/backup.nix
+++ tv/2configs/backup.nix
@@ -2,6 +2,18 @@
 with config.krebs.lib;
 {
   krebs.backup.plans = {
+    nomic-home-xu = {
+      method = "push";
+      src = { host = config.krebs.hosts.nomic; path = "/home"; };
+      dst = { host = config.krebs.hosts.xu;    path = "/bku/nomic-home"; };
+      startAt = "05:00";
+      snapshots = {
+        daily    = { format = "%Y-%m-%d";    retain =  7; };
+        weekly   = { format = "%YW%W";       retain =  4; };
+        monthly  = { format = "%Y-%m";       retain = 12; };
+        yearly   = { format = "%Y";                       };
+      };
+    };
     wu-home-xu = {
       method = "push";
       src = { host = config.krebs.hosts.wu; path = "/home"; };
