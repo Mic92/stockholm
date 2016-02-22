937641b6139973339eebc88a475b3b895c9aa701
tv <tv@krebsco.de>
2016-02-22 11:33:51 +0100

    cac-api: 1.1.0 -> 1.1.1

--- krebs/5pkgs/cac-api/default.nix
+++ krebs/5pkgs/cac-api/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchgit, bc, cac-cert, coreutils, curl, dash, gnugrep, gnused, inotifyTools, jq, ncurses, openssh, sshpass, ... }:
 
 stdenv.mkDerivation {
-  name = "cac-api-1.1.0";
+  name = "cac-api-1.1.1";
 
   src = fetchgit {
     url = http://cgit.cd.krebsco.de/cac-api;
-    rev = "0809fae379239687ed1170e04311dc2880ef0aba";
-    sha256 = "357ced27c9ed88028967c934178a1d230bf38617a7494cd4632fabdd2a04fcdd";
+    rev = "46c7af2935ccc096ba0e93cd1adf575026edf44a";
+    sha256 = "0i8aspkmfw74np7hlbip3hk7zbgl6cxrnbg83x4wgqrj5dpx6vy0";
   };
 
   phases = [
@@ -29,7 +29,7 @@ stdenv.mkDerivation {
       ncurses
       openssh
       sshpass
-    ]}
+    ]}:"$PATH"
     EOF
       # [1]: Disable fetching tasks; listtasks is currently broken:
       # Unknown column 'iod.apitask.cid' in 'field list'
