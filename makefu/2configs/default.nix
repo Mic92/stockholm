da20505f5d187f2bbe649543125f1097c6d87d85
makefu <github@syntax-fehler.de>
2016-02-27 12:26:06 +0100

    ma 2 default: fix path to glibc patch

--- makefu/2configs/default.nix
+++ makefu/2configs/default.nix
@@ -6,7 +6,7 @@ with config.krebs.lib;
 
   system.replaceRuntimeDependencies = with pkgs.lib;
         [{original = pkgs.glibc; replacement = pkgs.stdenv.lib.overrideDerivation pkgs.glibc (oldAttr: { patches = oldAttr.patches ++
-          [(pkgs.fetchurl { url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/libraries/glibc/cve-2015-7547.patch";
+          [(pkgs.fetchurl { url = "https://raw.githubusercontent.com/NixOS/nixpkgs/fc48bf5a2ceb908b73dc035374e2ec5a31086aa2/pkgs/development/libraries/glibc/cve-2015-7547.patch";
                             sha256 = "0awpc4rp2x27rjpj83ps0rclmn73hsgfv2xxk18k82w4hdxqpp5r";})];
         });}
         ];
