5a3c98aec7ff1755daa4b9cc647c2bbe0b8ba036
lassulus <lass@aidsballs.de>
2016-03-15 14:26:51 +0100

    l 5 newsbot-js -> k 5 newsbot-js

--- lass/5pkgs/default.nix
+++ lass/5pkgs/default.nix
@@ -8,7 +8,6 @@
       ublock = pkgs.callPackage ./firefoxPlugins/ublock.nix {};
       vimperator = pkgs.callPackage ./firefoxPlugins/vimperator.nix {};
     };
-    newsbot-js = pkgs.callPackage ./newsbot-js/default.nix {};
     xmonad-lass =
       let src = pkgs.writeNixFromCabal "xmonad-lass.nix" ./xmonad-lass; in
       pkgs.haskellPackages.callPackage src {};
