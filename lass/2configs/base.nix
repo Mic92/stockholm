76be13147a300e9449ab826e009f4c61b9330b60
lassulus <lass@aidsballs.de>
2016-04-09 14:14:43 +0200

    l 2 base: nixpkgs rev 40c586b -> e781a82

--- lass/2configs/base.nix
+++ lass/2configs/base.nix
@@ -55,7 +55,7 @@ with config.krebs.lib;
         stockholm = "/home/lass/stockholm";
         nixpkgs = {
           url = https://github.com/NixOS/nixpkgs;
-          rev = "40c586b7ce2c559374df435f46d673baf711c543";
+          rev = "e781a8257b4312f6b138c7d0511c77d8c06ed819";
           dev = "/home/lass/src/nixpkgs";
         };
       } // optionalAttrs config.krebs.build.host.secure {
