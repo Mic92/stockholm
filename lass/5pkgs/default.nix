f0863085c7f865069e0d49a99488734abe044c6a
lassulus <lass@aidsballs.de>
2016-04-13 16:37:26 +0200

    l 5: add mk_sql_pair

--- lass/5pkgs/default.nix
+++ lass/5pkgs/default.nix
@@ -8,6 +8,7 @@
       ublock = pkgs.callPackage ./firefoxPlugins/ublock.nix {};
       vimperator = pkgs.callPackage ./firefoxPlugins/vimperator.nix {};
     };
+    mk_sql_pair = pkgs.callPackage ./mk_sql_pair/default.nix {};
     mpv-poll = pkgs.callPackage ./mpv-poll/default.nix {};
     xmonad-lass =
       let src = pkgs.writeNixFromCabal "xmonad-lass.nix" ./xmonad-lass; in
