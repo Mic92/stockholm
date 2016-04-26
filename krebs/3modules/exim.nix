96301634b906182be0d8a5d669481da309f4a17a
tv <tv@krebsco.de>
2016-04-27 01:10:25 +0200

    services.exim -> krebs.exim

--- krebs/3modules/exim.nix
+++ krebs/3modules/exim.nix
@@ -1,111 +1,80 @@
-{ config, lib, pkgs, ... }:
-
-let
-  inherit (lib) mkIf mkOption singleton types;
-  inherit (pkgs) coreutils exim;
-  cfg = config.services.exim;
-in
-
-{
-
-  ###### interface
-
-  options = {
-
-    services.exim = {
-
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Whether to enable the Exim mail transfer agent.";
-      };
-
-      config = mkOption {
-        type = types.string;
-        default = "";
-        description = ''
-          Verbatim Exim configuration.  This should not contain exim_user,
-          exim_group, exim_path, or spool_directory.
-        '';
-      };
-
-      user = mkOption {
-        type = types.string;
-        default = "exim";
-        description = ''
-          User to use when no root privileges are required.
-          In particular, this applies when receiving messages and when doing
-          remote deliveries.  (Local deliveries run as various non-root users,
-          typically as the owner of a local mailbox.) Specifying this value
-          as root is not supported.
-        '';
-      };
-
-      group = mkOption {
-        type = types.string;
-        default = "exim";
-        description = ''
-          Group to use when no root privileges are required.
-        '';
-      };
-
-      spoolDir = mkOption {
-        type = types.string;
-        default = "/var/spool/exim";
-        description = ''
-          Location of the spool directory of exim.
-        '';
-      };
-
+{ config, lib, pkgs, ... }: with config.krebs.lib; let
+  cfg = config.krebs.exim;
+in {
+  options.krebs.exim = {
+    enable = mkEnableOption "krebs.exim";
+    config = mkOption {
+      type = types.str;
+      default = "";
+      description = ''
+        Verbatim Exim configuration.  This should not contain exim_user,
+        exim_group, exim_path, or spool_directory.
+      '';
     };
-
+    user = mkOption {
+      type = types.user;
+      default = {
+        name = "exim";
+        home = "/var/spool/exim";
+      };
+      description = ''
+        User to use when no root privileges are required.
+        In particular, this applies when receiving messages and when doing
+        remote deliveries.  (Local deliveries run as various non-root users,
+        typically as the owner of a local mailbox.) Specifying this value
+        as root is not supported.
+      '';
+    };
+    group = mkOption {
+      type = types.group;
+      default = {
+        name = "exim";
+      };
+      description = ''
+        Group to use when no root privileges are required.
+      '';
+    };
   };
-
-
-  ###### implementation
-
-  config = mkIf cfg.enable {
-
+  config = lib.mkIf cfg.enable {
     environment = {
       etc."exim.conf".text = ''
-        exim_user = ${cfg.user}
-        exim_group = ${cfg.group}
+        exim_user = ${cfg.user.name}
+        exim_group = ${cfg.group.name}
         exim_path = /var/setuid-wrappers/exim
-        spool_directory = ${cfg.spoolDir}
+        spool_directory = ${cfg.user.home}
         ${cfg.config}
       '';
-      systemPackages = [ exim ];
+      systemPackages = [ pkgs.exim ];
     };
-
-    users.extraUsers = singleton {
-      name = cfg.user;
-      description = "Exim mail transfer agent user";
-      uid = config.ids.uids.exim;
-      group = cfg.group;
-    };
-
-    users.extraGroups = singleton {
-      name = cfg.group;
-      gid = config.ids.gids.exim;
-    };
-
-    security.setuidPrograms = [ "exim" ];
-
-    systemd.services.exim = {
-      description = "Exim Mail Daemon";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart   = "${exim}/bin/exim -bdf -q30m";
-        ExecReload  = "${coreutils}/bin/kill -HUP $MAINPID";
+    krebs.setuid = {
+      exim = {
+        filename = "${pkgs.exim}/bin/exim";
+        mode = "4111";
       };
-      preStart = ''
-        if ! test -d ${cfg.spoolDir}; then
-          ${coreutils}/bin/mkdir -p ${cfg.spoolDir}
-          ${coreutils}/bin/chown ${cfg.user}:${cfg.group} ${cfg.spoolDir}
-        fi
-      '';
+      sendmail = {
+        filename = "${pkgs.exim}/bin/exim";
+        mode = "4111";
+      };
     };
-
+    systemd.services.exim = {
+      restartTriggers = [
+        config.environment.etc."exim.conf".source
+      ];
+      serviceConfig = {
+        ExecStart = "${pkgs.exim}/bin/exim -bdf -q30m";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+    users = {
+      groups.${cfg.group.name} = {
+        inherit (cfg.group) name gid;
+      };
+      users.${cfg.user.name} = {
+        inherit (cfg.user) home name uid;
+        createHome = true;
+        group = cfg.group.name;
+      };
+    };
   };
-
 }
