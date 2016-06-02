3cce6ddda4d143dea1badace98db0d79d4a14199
makefu <github@syntax-fehler.de>
2016-06-02 14:44:31 +0200

    ma 2 exim-retiolum: add firewall exception for exim-retiolum

--- makefu/2configs/exim-retiolum.nix
+++ makefu/2configs/exim-retiolum.nix
@@ -2,9 +2,10 @@
 
 with config.krebs.lib;
 {
+  networking.firewall.allowedTCPPorts = [ 25 ];
+
   krebs.exim-retiolum.enable = true;
   environment.systemPackages = with pkgs; [
     msmtp
   ];
-
 }
