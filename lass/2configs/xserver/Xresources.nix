821fec2bcdf811dcca30e0fae40fa7812800ff6c
lassulus <lass@aidsballs.de>
2016-06-06 22:17:41 +0200

    l 2 xserver Xresources: steal tv's colors

--- lass/2configs/xserver/Xresources.nix
+++ lass/2configs/xserver/Xresources.nix
@@ -19,9 +19,48 @@ pkgs.writeText "Xresources" ''
 
   URxvt.intensityStyles: false
 
-  URxvt*background:                     #000000
-  URxvt*foreground:                     #ffffff
+  URxvt*background:	#050505
+  ! URxvt*background:	#041204
 
-  !change unreadable blue
-  URxvt*color4:                         #268bd2
+  !URxvt.depth: 32
+  !URxvt*background: rgba:0500/0500/0500/cccc
+
+  ! URxvt*background:	#080810
+  URxvt*foreground:	#d0d7d0
+  ! URxvt*background:	black
+  ! URxvt*foreground:	white
+  ! URxvt*background:	rgb:00/00/40
+  ! URxvt*foreground:	rgb:a0/a0/d0
+  ! XTerm*cursorColor:	rgb:00/00/60
+  URxvt*cursorColor:	#f042b0
+  URxvt*cursorColor2:	#f0b000
+  URxvt*cursorBlink:	off
+  ! URxvt*cursorUnderline: true
+  ! URxvt*highlightColor: #232323
+  ! URxvt*highlightTextColor: #b0ffb0
+
+  URxvt*.pointerBlank: true
+  URxvt*.pointerBlankDelay: 987654321
+  URxvt*.pointerColor: #f042b0
+  URxvt*.pointerColor2: #050505
+
+  ! URxvt*color0:	#000000
+  ! URxvt*color1:	#c00000
+  ! URxvt*color2:	#80c070
+  URxvt*color3:	#c07000
+  ! URxvt*color4:	#0000c0
+  URxvt*color4:	#4040c0
+  ! URxvt*color5:	#c000c0
+  ! URxvt*color6:	#008080
+  URxvt*color7:	#c0c0c0
+
+  URxvt*color8:	#707070
+  URxvt*color9:	#ff6060
+  URxvt*color10:	#70ff70
+  URxvt*color11:	#ffff70
+  URxvt*color12:	#7070ff
+  URxvt*color13:	#ff50ff
+  URxvt*color14:	#70ffff
+  URxvt*color15:	#ffffff
+
 ''
