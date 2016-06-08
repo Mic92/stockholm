72335ce00ec759e5b9d7a1468fe517e66948d8ed
lassulus <lass@aidsballs.de>
2016-06-08 18:22:15 +0200

    l 1 prism: serve nix-cache

--- lass/1systems/prism.nix
+++ lass/1systems/prism.nix
@@ -210,6 +210,30 @@ in {
         '')
       ];
     }
+    {
+      services.nix-serve = {
+        enable = true;
+        secretKeyFile = config.krebs.secret.files.nix-serve-key.path;
+      };
+      systemd.services.nix-serve = {
+        requires = ["secret.service"];
+        after = ["secret.service"];
+      };
+      krebs.secret.files.nix-serve-key = {
+        path = "/run/secret/nix-serve.key";
+        owner.name = "nix-serve";
+        source-path = toString <secrets> + "/nix-serve.key";
+      };
+      krebs.nginx = {
+        enable = true;
+        servers.nix-serve = {
+          server-names = [ "cache.prism.r" ];
+          locations = lib.singleton (lib.nameValuePair "/" ''
+            proxy_pass http://localhost:${toString config.services.nix-serve.port};
+          '');
+        };
+      };
+    }
   ];
 
   krebs.build.host = config.krebs.hosts.prism;
