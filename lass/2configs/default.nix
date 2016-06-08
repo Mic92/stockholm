82f811474c036daff9b29a4ee1e89784dc781cc9
lassulus <lass@aidsballs.de>
2016-06-08 18:23:02 +0200

    l 2: activate prism as binary-cache

--- lass/2configs/default.nix
+++ lass/2configs/default.nix
@@ -40,6 +40,12 @@ with config.krebs.lib;
         };
       };
     }
+    {
+      nix = {
+        binaryCaches = ["http://cache.prism.r"];
+        binaryCachePublicKeys = ["cache.prism-1:+S+6Lo/n27XEtvdlQKuJIcb1yO5NUqUCE2lolmTgNJU="];
+      };
+    }
   ];
 
   networking.hostName = config.krebs.build.host.name;
