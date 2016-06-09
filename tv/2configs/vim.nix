0b6103498b4555df63e29a31905627e66e25f610
tv <tv@krebsco.de>
2016-06-09 13:22:47 +0200

    tv vim-syntax-vim: init at 1.0.0

--- tv/2configs/vim.nix
+++ tv/2configs/vim.nix
@@ -44,6 +44,19 @@ let
         hi TrailingSpace  ctermbg=88
       '';
     })))
+    ((rtp: rtp // { inherit rtp; }) (pkgs.writeTextFile (let
+      name = "vim";
+    in {
+      name = "vim-syntax-${name}-1.0.0";
+      destination = "/syntax/${name}.vim";
+      text = /* vim */ ''
+        ${concatMapStringsSep "\n" (s: /* vim */ ''
+          syn keyword vimColor${s} ${s}
+            \ containedin=ALLBUT,vimComment,vimLineComment
+          hi vimColor${s} ctermfg=${s}
+        '') (map (i: lpad 3 "0" (toString i)) (range 0 255))}
+      '';
+    })))
   ];
 
   dirs = {
