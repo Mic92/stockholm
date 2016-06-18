e37ac8ecd8dc7282953e83994dc6c4e3c78b7a85
lassulus <lass@aidsballs.de>
2016-06-19 00:27:45 +0200

    l 2 power-action: add low battery warning

--- lass/2configs/power-action.nix
+++ lass/2configs/power-action.nix
@@ -5,9 +5,22 @@ let
     ${pkgs.systemd}/bin/systemctl suspend
   '';
 
+  speak = text:
+    pkgs.writeDash "speak" ''
+      ${pkgs.espeak}/bin/espeak -v +whisper -s 110 "${text}"
+    '';
+
 in {
   lass.power-action = {
     enable = true;
+    plans.low-battery = {
+      upperLimit = 30;
+      lowerLimit = 25;
+      charging = false;
+      action = pkgs.writeDash "warn-low-battery" ''
+        ${speak "power level low"}
+      '';
+    };
     plans.suspend = {
       upperLimit = 10;
       lowerLimit = 0;
@@ -17,6 +30,11 @@ in {
       '';
     };
   };
+
+  users.users.power-action.extraGroups = [
+    "audio"
+  ];
+
   security.sudo.extraConfig = ''
     ${config.lass.power-action.user.name} ALL= (root) NOPASSWD: ${suspend}
   '';
