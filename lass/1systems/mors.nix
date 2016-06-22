401d80610243f1eac1fcb123f51ce3ea6e2280da
lassulus <lass@aidsballs.de>
2016-06-22 19:21:11 +0200

    l 2: rename cbase to c-base

--- lass/1systems/mors.nix
+++ lass/1systems/mors.nix
@@ -26,11 +26,10 @@
     ../2configs/teamviewer.nix
     ../2configs/libvirt.nix
     ../2configs/fetchWallpaper.nix
-    ../2configs/cbase.nix
+    ../2configs/c-base.nix
     ../2configs/mail.nix
     ../2configs/krebs-pass.nix
     ../2configs/umts.nix
-    #../2configs/buildbot-standalone.nix
     {
       #risk of rain port
       krebs.iptables.tables.filter.INPUT.rules = [
