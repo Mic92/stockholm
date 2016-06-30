1542f9bbee823025f703e6abf3836905cee416fd
tv <tv@krebsco.de>
2016-06-30 16:24:41 +0200

    miefda: stash

--- krebs/3modules/default.nix
+++ krebs/3modules/default.nix
@@ -91,7 +91,6 @@ let
   imp = lib.mkMerge [
     { krebs = import ./lass   { inherit config lib; }; }
     { krebs = import ./makefu { inherit config lib; }; }
-    { krebs = import ./miefda { inherit config lib; }; }
     { krebs = import ./shared { inherit config lib; }; }
     { krebs = import ./tv     { inherit config lib; }; }
     {
