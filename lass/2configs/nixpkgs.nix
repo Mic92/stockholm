12223227b9bb87bd963913d829f0e80b5785b5d4
lassulus <lass@aidsballs.de>
2016-07-19 20:25:42 +0200

    l 2 nixpkgs: adapt to new populate

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -1,8 +1,8 @@
 { ... }:
 
 {
-  krebs.build.source.nixpkgs = {
+  krebs.build.source.nixpkgs.git = {
       url = https://github.com/lassulus/nixpkgs;
-      rev = "11a7899222929b6eb0951f7a1c0182f65b3b4637";
+      ref = "11a7899222929b6eb0951f7a1c0182f65b3b4637";
     };
 }
