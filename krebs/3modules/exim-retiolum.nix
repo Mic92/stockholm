24db6439c4bc64d39b991a677a8dac4a6581ed74
makefu <github@syntax-fehler.de>
2016-07-20 10:06:04 +0200

    retiolum: config which is working but not functioning (see TODO in retiolum.nix)

--- krebs/3modules/exim-retiolum.nix
+++ krebs/3modules/exim-retiolum.nix
@@ -37,7 +37,7 @@ let
       config =
         # This configuration makes only sense for retiolum-enabled hosts.
         # TODO modular configuration
-        assert config.krebs.retiolum.enable;
+        assert (lib.hasAttr "retiolum" config.krebs.tinc);
         ''
           keep_environment =
 
