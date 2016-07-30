d3e9830f9850591e5a0e0820b785fd324087ac40
makefu <github@syntax-fehler.de>
2016-07-30 18:31:17 +0200

    m 5 wol: init

--- makefu/5pkgs/default.nix
+++ makefu/5pkgs/default.nix
@@ -5,20 +5,21 @@ let
 in
 {
   nixpkgs.config.packageOverrides = rec {
-    alsa-hdspmixer = callPackage ./alsa-tools { alsaToolTarget="hdspmixer";};
     alsa-hdspconf = callPackage ./alsa-tools { alsaToolTarget="hdspconf";};
+    alsa-hdspmixer = callPackage ./alsa-tools { alsaToolTarget="hdspmixer";};
     alsa-hdsploader = callPackage ./alsa-tools { alsaToolTarget="hdsploader";};
     awesomecfg = callPackage ./awesomecfg {};
     bintray-upload = callPackage ./bintray-upload {};
+    inherit (callPackage ./devpi {}) devpi-web devpi-server;
+    farpd = callPackage ./farpd {};
     git-xlsx-textconv = callPackage ./git-xlsx-textconv {};
     mergerfs = callPackage ./mergerfs {};
     mycube-flask = callPackage ./mycube-flask {};
     nodemcu-uploader = callPackage ./nodemcu-uploader {};
+    ps3netsrv = callPackage ./ps3netsrv {};
     tw-upload-plugin = callPackage ./tw-upload-plugin {};
-    inherit (callPackage ./devpi {}) devpi-web devpi-server;
     skytraq-logger = callPackage ./skytraq-logger {};
     taskserver = callPackage ./taskserver {};
-    ps3netsrv = callPackage ./ps3netsrv {};
-    farpd = callPackage ./farpd {};
+    wol = callPackage ./wol {};
   };
 }
