8f0f19c5539fde7c93761bb4939ab0a5349e8d2e
makefu <github@syntax-fehler.de>
2016-08-24 22:05:02 +0200

    k 5 pwqgen: allow user-provided wordset

--- krebs/5pkgs/passwdqc-utils/default.nix
+++ krebs/5pkgs/passwdqc-utils/default.nix
@@ -1,13 +1,18 @@
-{stdenv,pam,fetchurl,...}:
+{ stdenv, pam,
+  fetchurl, lib,
+  wordset-file ? null, # set your own wordset-file
+  ... }:
 
 stdenv.mkDerivation rec {
   name = "passwdqc-utils-${version}";
   version = "1.3.0";
   buildInputs = [ pam ];
+
   src = fetchurl {
     url = "http://www.openwall.com/passwdqc/passwdqc-${version}.tar.gz";
     sha256 = "0l3zbrp4pvah0dz33m48aqlz9nx663cc1fqhnlwr0p853b10la93";
   };
+
   buildTargets = "utils";
   installFlags= [ "BINDIR=$(out)/bin"
                   "CONFDIR=$(out)/etc"
@@ -15,7 +20,12 @@ stdenv.mkDerivation rec {
                   "DEVEL_LIBDIR=$(out)/lib"
                   "SECUREDIR=$(out)/lib/security"
                   "INCLUDEDIR=$(out)/include"
-                  "MANDIR=$(out)/man"];
+                  "MANDIR=$(out)/man" ];
+
+  patchPhase = lib.optionalString (wordset-file != null) ''
+    cp -f ${wordset-file} wordset_4k.c
+  '';
+
   installTargets = "install_lib install_utils";
 
   meta = {
--- makefu/5pkgs/default.nix
+++ makefu/5pkgs/default.nix
@@ -18,6 +18,12 @@ in
     mycube-flask = callPackage ./mycube-flask {};
     nodemcu-uploader = callPackage ./nodemcu-uploader {};
     ps3netsrv = callPackage ./ps3netsrv {};
+    pwqgen-ger = callPackage ../../krebs/5pkgs/passwdqc-utils {
+      wordset-file = pkgs.fetchurl {
+        url = https://gist.githubusercontent.com/makefu/b56f5554c9ef03fe6e09878962e6fd8d/raw/1f147efec51325bc9f80c823bad8381d5b7252f6/wordset_4k.c ;
+        sha256 = "18ddzyh11bywrhzdkzvrl7nvgp5gdb4k1s0zxbz2bkhd14vi72bb";
+      };
+    };
     tw-upload-plugin = callPackage ./tw-upload-plugin {};
     skytraq-logger = callPackage ./skytraq-logger {};
     taskserver = callPackage ./taskserver {};
