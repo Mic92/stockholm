5ba1efdca4ca95bfd6001de5eef179b86b8fa0b6
lassulus <lass@aidsballs.de>
2016-08-24 23:53:27 +0200

    l 2 nixpkgs: c5cbda2 -> 9ad6040

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -2,7 +2,7 @@
 
 {
   krebs.build.source.nixpkgs.git = {
-    url = https://github.com/nixos/nixpkgs;
-    ref = "c5cbda24147de441906871238138eb7fc90282db";
+    url = https://github.com/lassulus/nixpkgs;
+    ref = "9ad60400c2a4b964c0947bb52a84ec3f601261ef";
   };
 }
