836f1d3277e97945d0029c7458ab548eb41722dd
lassulus <lass@aidsballs.de>
2016-10-01 02:51:26 +0200

    l 2 nixpkgs: 3fb009d -> 8a89481

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -3,6 +3,6 @@
 {
   krebs.build.source.nixpkgs.git = {
     url = https://github.com/lassulus/nixpkgs;
-    ref = "3fb009d94e70f5d1151f4ec239a90d2de1979a74";
+    ref = "8a8948167324f67d26a1c7ddc8e387128332b622";
   };
 }
