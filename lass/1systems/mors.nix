7e16427bb5afec6cfefb89e442bc3b529a583e39
lassulus <lass@aidsballs.de>
2016-10-12 12:15:57 +0200

    l 1 mors: disable c-base vpn

--- lass/1systems/mors.nix
+++ lass/1systems/mors.nix
@@ -23,7 +23,7 @@ with config.krebs.lib;
     ../2configs/teamviewer.nix
     ../2configs/libvirt.nix
     ../2configs/fetchWallpaper.nix
-    ../2configs/c-base.nix
+    #../2configs/c-base.nix
     ../2configs/mail.nix
     ../2configs/krebs-pass.nix
     ../2configs/repo-sync.nix
