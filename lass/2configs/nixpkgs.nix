49ad272f65e310344e7a209050c54c1acedbedb8
lassulus <lass@aidsballs.de>
2016-10-12 09:41:12 +0200

    l 2 nixpkgs: change url to github/nixos

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -2,7 +2,7 @@
 
 {
   krebs.build.source.nixpkgs.git = {
-    url = https://github.com/lassulus/nixpkgs;
+    url = https://github.com/nixos/nixpkgs;
     ref = "0c022c45df220159233d591eadcd85118dd70345";
   };
 }
