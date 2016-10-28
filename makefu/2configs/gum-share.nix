c26a3ce4b465db07f3d94e263588b96d167e53fa
makefu <github@syntax-fehler.de>
2016-10-28 14:11:31 +0200

    m 1 gum: add gum-share

---
+++ makefu/2configs/gum-share.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+with config.krebs.lib;
+let
+  hostname = config.krebs.build.host.name;
+in {
+  # users.users.smbguest = {
+  #   name = "smbguest";
+  #   uid = config.ids.uids.smbguest;
+  #   description = "smb guest user";
+  #   home = "/var/empty";
+  # };
+
+  users.users.download = { };
+  services.samba = {
+    enable = true;
+    shares = {
+      download = {
+        path = "/var/download";
+        "read only" = "no";
+        browseable = "yes";
+        "guest ok" = "no";
+        "valid users" = "download";
+      };
+    };
+    extraConfig = ''
+      # guest account = smbguest
+      # map to guest = bad user
+      # disable printing
+      load printers = no
+      printing = bsd
+      printcap name = /dev/null
+      disable spoolss = yes
+    '';
+  };
+  networking.firewall.extraCommands = ''
+      iptables -A INPUT -i retiolum -p tcp --dport 445 -j ACCEPT
+  '';
+}
