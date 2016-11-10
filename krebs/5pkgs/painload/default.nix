b5ddef3996ad4a3e19d26e8bc748b6d0c954113b
tv <tv@krebsco.de>
2016-11-10 21:49:48 +0100

    painload: init at 8df031f

---
+++ krebs/5pkgs/painload/default.nix
@@ -0,0 +1,7 @@
+{ fetchgit, ... }:
+
+fetchgit {
+  url = https://github.com/krebscode/painload;
+  rev = "8df031f810a2776d8c43b03a9793cb49398bd33b";
+  sha256 = "03md5k6fmz0j1ny22iw96dzq7cvijbz24ii85i0h2dhcychdp650";
+}
