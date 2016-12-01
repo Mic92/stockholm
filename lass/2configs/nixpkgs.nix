f4ce5ea248c6dcb965f9367a4569a39f4be747af
lassulus <lass@aidsballs.de>
2016-12-01 21:50:23 +0100

    l 2 nixpkgs: ece0cea -> e360c72

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -3,6 +3,6 @@
 {
   krebs.build.source.nixpkgs.git = {
     url = https://github.com/nixos/nixpkgs;
-    ref = "ece0cea127f0a8799a6bd3b12c368193491f9058";
+    ref = "e360c7205f84be180d80b97f3980fb809a951486";
   };
 }
