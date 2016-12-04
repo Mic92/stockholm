41c7c82343b77700372ee601961d214609d3c124
lassulus <lass@aidsballs.de>
2016-12-04 15:38:26 +0100

    l 2 nixpkgs: e360c72 -> 6a9f087

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -3,6 +3,6 @@
 {
   krebs.build.source.nixpkgs.git = {
     url = https://github.com/nixos/nixpkgs;
-    ref = "e360c7205f84be180d80b97f3980fb809a951486";
+    ref = "6a9f087e37e392b9c3819196af33c529c12c4e48";
   };
 }
