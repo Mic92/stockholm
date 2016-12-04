3b80a5518c8f5b30de8f08d69c3dc01c2ad52d3d
lassulus <lass@aidsballs.de>
2016-12-04 20:40:26 +0100

    l 2 baseX: use pulse.nix

--- lass/2configs/baseX.nix
+++ lass/2configs/baseX.nix
@@ -7,11 +7,8 @@ in {
     ./xserver
     ./mpv.nix
     ./power-action.nix
+    ./pulse.nix
   ];
-  hardware.pulseaudio = {
-    enable = true;
-    systemWide = true;
-  };
 
   users.extraUsers.mainUser.extraGroups = [ "audio" "video" ];
 
