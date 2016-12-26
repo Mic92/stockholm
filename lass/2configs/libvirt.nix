512f90d10096e81180730374d5d9664384894807
lassulus <lass@lassul.us>
2016-12-26 13:29:06 +0100

    l 2 libvirt: add lass to libvirtd

--- lass/2configs/libvirt.nix
+++ lass/2configs/libvirt.nix
@@ -1,23 +1,7 @@
 { config, lib, pkgs, ... }:
 
-let
-  mainUser = config.users.extraUsers.mainUser;
-  inherit (import <stockholm/lib>) genid;
-
-in {
+{
+  users.users.mainUser.extraGroups = [ "libvirtd" ];
   virtualisation.libvirtd.enable = true;
 
-  users.extraUsers = {
-    libvirt = {
-      uid = genid "libvirt";
-      description = "user for running libvirt stuff";
-      home = "/home/libvirt";
-      useDefaultShell = true;
-      extraGroups = [ "libvirtd" "audio" ];
-      createHome = true;
-    };
-  };
-  security.sudo.extraConfig = ''
-    ${mainUser.name} ALL=(libvirt) NOPASSWD: ALL
-  '';
 }
