0d61093e18929e48ebfd984c1e0f2b6b1f0c6c58
lassulus <lass@lassul.us>
2016-12-27 23:01:44 +0100

    l 2 nixpkgs: 435b5f8 -> 819c1ab
    we need this change to make graphite_beacon work with cli support

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -2,7 +2,7 @@
 
 {
   krebs.build.source.nixpkgs.git = {
-    url = https://github.com/nixos/nixpkgs;
-    ref = "435b5f8da08665600c1c93723a48527239882209";
+    url = https://github.com/lassulus/nixpkgs;
+    ref = "819c1ab486a9c81d6a6b76c759aedece2df39037";
   };
 }
