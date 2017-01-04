2b8f8363392a7b337b3f67ffae93719f15952754
tv <tv@krebsco.de>
2017-01-04 08:36:29 +0100

    infest/prepare: fix style inconsistencies

--- krebs/4lib/infest/prepare.sh
+++ krebs/4lib/infest/prepare.sh
@@ -143,10 +143,10 @@ prepare_common() {(
     mkdir -p /mnt/boot
 
     if mount | grep -Fq ' on /boot type '; then
-      bootdev=$(mount | grep " on /boot type " | sed 's/ .*//')
-      mount $bootdev /mnt/boot
+      bootpart=$(mount | grep ' on /boot type ' | sed 's/ .*//')
+      mount $bootpart /mnt/boot
     else
-      mount --bind /boot/ /mnt/boot
+      mount --bind /boot /mnt/boot
     fi
 
   fi
@@ -155,7 +155,7 @@ prepare_common() {(
   # prepare install directory
   #
 
-  rootpart=$(mount | grep " on / type" | sed 's/ .*//')
+  rootpart=$(mount | grep ' on / type ' | sed 's/ .*//')
 
   mkdir -p /mnt/etc/nixos
   mkdir -m 0555 -p /mnt/var/empty
