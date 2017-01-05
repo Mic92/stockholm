ef757f6e37ac02e18e04dd2fdf15d6d723a679ec
tv <tv@krebsco.de>
2017-01-05 21:03:23 +0100

    tv: use body instead of out

--- tv/3modules/iptables.nix
+++ tv/3modules/iptables.nix
@@ -1,10 +1,10 @@
 { config, lib, pkgs, ... }:
 
 with import <stockholm/lib>;
-let
+let {
   cfg = config.tv.iptables;
 
-  out = {
+  body = {
     options.tv.iptables = api;
     config = lib.mkIf cfg.enable imp;
   };
@@ -146,14 +146,4 @@ let
       )}
       COMMIT
     '';
-in out
-
-#let
-#  cfg = config.tv.iptables;
-#  arg' = arg // { inherit cfg; };
-#in
-#
-#{
-#  options.tv.iptables = import ./options.nix arg';
-#  config = lib.mkIf cfg.enable (import ./config.nix arg');
-#}
+}
