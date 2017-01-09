eb208085f011aa48bae73b98caa1f4fccae74ab6
makefu <github@syntax-fehler.de>
2017-01-09 16:02:20 +0100

    m 2 nginx/share-download: expose dl-dir via nginx

---
+++ makefu/2configs/nginx/share-download.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with import <stockholm/lib>;
+{
+  services.nginx = {
+    enable = mkDefault true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    virtualHosts."dl.euer.krebsco.de" = {
+        root = config.makefu.dl-dir;
+        extraConfig = "autoindex on;";
+        forceSSL = true;
+        enableSSL = true;
+        enableACME = true;
+        basicAuth = import <secrets/dl.euer.krebsco.de-auth.nix>;
+    };
+  };
+}
