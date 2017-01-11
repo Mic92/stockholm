8884c4274ddd54364e7215f2ce051a53ac542b99
lassulus <lass@lassul.us>
2017-01-11 23:07:15 +0100

    k 5 bepasty-client-cli: use recent git master

--- krebs/5pkgs/bepasty-client-cli/default.nix
+++ krebs/5pkgs/bepasty-client-cli/default.nix
@@ -1,17 +1,18 @@
-{ lib, pkgs, pythonPackages, fetchurl, ... }:
+{ lib, pkgs, pythonPackages, fetchFromGitHub, ... }:
 
 with pythonPackages; buildPythonPackage rec {
-  name = "bepasty-client-cli-${version}";
-  version = "0.3.0";
+  name = "bepasty-client-cli";
   propagatedBuildInputs = [
     python_magic
     click
     requests2
   ];
 
-  src = fetchurl {
-    url = "https://pypi.python.org/packages/source/b/bepasty-client-cli/bepasty-client-cli-${version}.tar.gz";
-    sha256 = "002kcplyfnmr5pn2ywdfilss0rmbm8wcdzz8hzp03ksy2zr4sdbw";
+  src = fetchFromGitHub {
+    owner = "bepasty";
+    repo = "bepasty-client-cli";
+    rev = "4b7135ba8ba1e17501de08ad7b6aca73c0d949d2";
+    sha256 = "1svchyk9zai1vip9ppm12jm7wfjbdr9ijhgcd2n10xh73jrn9cnc";
   };
 
   meta = {
