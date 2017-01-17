4a21d12981e8edcd1f6ec0fd5214a62b72a2957c
lassulus <lass@lassul.us>
2017-01-17 15:57:24 +0100

    l 2: disable zsh

--- lass/2configs/default.nix
+++ lass/2configs/default.nix
@@ -9,7 +9,6 @@ with import <stockholm/lib>;
     ../2configs/mc.nix
     ../2configs/nixpkgs.nix
     ../2configs/vim.nix
-    ../2configs/zsh.nix
     ./backups.nix
     {
       users.extraUsers =
