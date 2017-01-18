82149ebb75892267af3b9e0a290f975d15965894
lassulus <lass@lassul.us>
2017-01-18 19:54:47 +0100

    l 2 nixpkgs: use 3909827 from 16.09

--- lass/2configs/nixpkgs.nix
+++ lass/2configs/nixpkgs.nix
@@ -2,7 +2,7 @@
 
 {
   krebs.build.source.nixpkgs.git = {
-    url = https://github.com/lassulus/nixpkgs;
-    ref = "d98b556864f2b3a634e39ed1ae29f47c0e3fae35";
+    url = https://github.com/nixos/nixpkgs;
+    ref = "39098270855c171f0824c09d071b606ae991ff87";
   };
 }
