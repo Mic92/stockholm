8daef993dcb6149a02c72a4895d6e808a6c6a8d4
lassulus <lass@lassul.us>
2017-02-03 00:24:39 +0100

    l 2 monitoring client: add collectd config

--- lass/2configs/monitoring/client.nix
+++ lass/2configs/monitoring/client.nix
@@ -26,10 +26,69 @@ with import <stockholm/lib>;
         [[inputs.ping]]
         urls = ["8.8.8.8"]
       ''
+      ''
+        [[inputs.net]]
+      ''
+      ''
+        [[inputs.dns_query]]
+          servers = ["8.8.8.8"]
+      ''
     ];
   };
   systemd.services.telegraf.path = with pkgs; [
     iputils
     lm_sensors
   ];
+
+  services.collectd = {
+    enable = true;
+    autoLoadPlugin = true;
+    extraConfig = ''
+      Hostname ${config.krebs.build.host.name}
+      LoadPlugin load
+      LoadPlugin disk
+      LoadPlugin memory
+      Interval 30.0
+
+      LoadPlugin interface
+      <Plugin "interface">
+        Interface "*Link"
+        Interface "lo"
+        Interface "vboxnet*"
+        Interface "virbr*"
+        IgnoreSelected true
+      </Plugin>
+
+      LoadPlugin df
+      <Plugin "df">
+        MountPoint "/nix/store"
+        FSType "tmpfs"
+        FSType "binfmt_misc"
+        FSType "debugfs"
+        FSType "mqueue"
+        FSType "hugetlbfs"
+        FSType "systemd-1"
+        FSType "cgroup"
+        FSType "securityfs"
+        FSType "ramfs"
+        FSType "proc"
+        FSType "devpts"
+        FSType "devtmpfs"
+        MountPoint "/var/lib/docker/devicemapper"
+        IgnoreSelected true
+      </Plugin>
+
+      LoadPlugin cpu
+      <Plugin cpu>
+        ReportByCpu true
+        ReportByState true
+        ValuesPercentage true
+      </Plugin>
+
+      LoadPlugin network
+      <Plugin "network">
+          Server "prism" "25826"
+      </Plugin>
+    '';
+  };
 }
