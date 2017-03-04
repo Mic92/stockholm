e1bb6d8c27d3be94e3fa18eb8958f2c885322126
lassulus <lass@lassul.us>
2017-03-04 22:01:03 +0100

    l 2 livestream: use python27

--- lass/2configs/livestream.nix
+++ lass/2configs/livestream.nix
@@ -4,7 +4,7 @@ with import <stockholm/lib>;
 let
 
   stream = pkgs.writeDashBin "stream" ''
-    ${pkgs.python35Packages.livestreamer}/bin/livestreamer --http-header Client-ID=jzkbprff40iqj646a697cyrvl0zt2m6 -p mpv "$@"
+    ${pkgs.python27Packages.livestreamer}/bin/livestreamer --http-header Client-ID=jzkbprff40iqj646a697cyrvl0zt2m6 -p mpv "$@"
   '';
 
 in {
