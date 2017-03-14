7c6bf8f43739950a6888ebfe78e17ea3b87f9182
lassulus <lass@lassul.us>
2017-03-14 20:53:16 +0100

    l 2 git: don't announce newest

--- lass/2configs/git.nix
+++ lass/2configs/git.nix
@@ -58,7 +58,7 @@ let
         server = "ni.r";
         verbose = config.krebs.build.host.name == "prism";
         # TODO define branches in some kind of option per repo
-        branches = [ "master" "newest" ];
+        branches = [ "master" ];
       };
     };
   };
