22daf39ce8a48d960019ca11bf1616883a104373
tv <tv@krebsco.de>
2017-03-14 23:15:23 +0100

    tv: add krebspaste

--- tv/2configs/default.nix
+++ tv/2configs/default.nix
@@ -154,6 +154,7 @@ with import <stockholm/lib>;
     {
       environment.systemPackages = [
         pkgs.get
+        pkgs.krebspaste
         pkgs.krebszones
         pkgs.nix-prefetch-scripts
         pkgs.push
