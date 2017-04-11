2260ebedf7fcc699ba9ca3288e355a8f8c65d930
lassulus <lass@lassul.us>
2017-04-11 21:39:57 +0200

    l 2: add paste.nix

---
+++ lass/2configs/paste.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+with import <stockholm/lib>;
+
+{
+  services.nginx.virtualHosts.paste = {
+    serverAliases = [ "p.r" ];
+    locations."/".extraConfig = ''
+      proxy_set_header Host $host;
+      proxy_pass http://localhost:9081;
+    '';
+  };
+  krebs.htgen.paste = {
+    port = 9081;
+    script = toString [
+      "PATH=${makeBinPath [
+        pkgs.nix
+      ]}:$PATH"
+      "STATEDIR=$HOME"
+      ". ${pkgs.htgen}/examples/paste"
+    ];
+  };
+  krebs.iptables.tables.filter.INPUT.rules = [
+    { predicate = "-i retiolum -p tcp --dport 80"; target = "ACCEPT";}
+    { predicate = "-i retiolum -p tcp --dport 9081"; target = "ACCEPT";}
+  ];
+}
