5378630fce7fe7b41d3a18ac822b5853e8cde76e
makefu <github@syntax-fehler.de>
2017-04-12 11:15:02 +0200

    k 3 m: mv pgp keys
    auto-complete correctly ;)

--- krebs/3modules/makefu/default.nix
+++ krebs/3modules/makefu/default.nix
@@ -896,8 +896,8 @@ with import <stockholm/lib>;
     makefu = {
       mail = "makefu@x.retiolum";
       pubkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCl3RTOHd5DLiVeUbUr/GSiKoRWknXQnbkIf+uNiFO+XxiqZVojPlumQUVhasY8UzDzj9tSDruUKXpjut50FhIO5UFAgsBeMJyoZbgY/+R+QKU00Q19+IiUtxeFol/9dCO+F4o937MC0OpAC10LbOXN/9SYIXueYk3pJxIycXwUqhYmyEqtDdVh9Rx32LBVqlBoXRHpNGPLiswV2qNe0b5p919IGcslzf1XoUzfE3a3yjk/XbWh/59xnl4V7Oe7+iQheFxOT6rFA30WYwEygs5As//ZYtxvnn0gA02gOnXJsNjOW9irlxOUeP7IOU6Ye3WRKFRR0+7PS+w8IJLag2xb makefu@x";
-      pgp.pubkeys.default = builtins.readFile ./default.pgp;
-      pgp.pubkeys.brain = builtins.readFile ./brain.pgp;
+      pgp.pubkeys.default = builtins.readFile ./pgp/default.asc;
+      pgp.pubkeys.brain = builtins.readFile ./pgp/brain.asc;
     };
     makefu-omo = {
       inherit (makefu) mail pgp;
