e1350904390ca40e9ab8186c0ff27d088431a430
lassulus <lass@lassul.us>
2017-05-16 23:18:28 +0200

    l 2 retiolum: don't open smtp

--- lass/2configs/retiolum.nix
+++ lass/2configs/retiolum.nix
@@ -5,7 +5,6 @@
   krebs.iptables = {
     tables = {
       filter.INPUT.rules = [
-        { predicate = "-p tcp --dport smtp"; target = "ACCEPT"; }
         { predicate = "-p tcp --dport tinc"; target = "ACCEPT"; }
         { predicate = "-p udp --dport tinc"; target = "ACCEPT"; }
       ];
