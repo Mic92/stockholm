11e13658d19f3e4a43400153f1f13d82a2139389
tv <tv@krebsco.de>
2017-06-18 19:53:58 +0200

    urlwatch: 2.5 -> 2.6

--- krebs/5pkgs/simple/urlwatch/default.nix
+++ krebs/5pkgs/simple/urlwatch/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl, python3Packages }:
 
 python3Packages.buildPythonPackage rec {
-  name = "urlwatch-2.5";
+  name = "urlwatch-${meta.version}";
 
   src = fetchurl {
-    url = "https://thp.io/2008/urlwatch/${name}.tar.gz";
-    sha256 = "0qirpymdmpsx0klmhbx3icmiwpm6fx4wjma646gl9m90pifs8430";
+    url = "https://github.com/thp/urlwatch/archive/${meta.version}.tar.gz";
+    sha256 = "09bn31gn03swi7yr3s1ql8x07hx96gap1ka77kk44kk0lvfxn55b";
   };
 
   propagatedBuildInputs = with python3Packages; [
@@ -20,5 +20,6 @@ python3Packages.buildPythonPackage rec {
     homepage = https://thp.io/2008/urlwatch/;
     license = stdenv.lib.licenses.bsd3;
     maintainers = [ stdenv.lib.maintainers.tv ];
+    version = "2.6";
   };
 }
