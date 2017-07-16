0d3953ade9df4c51834378382753b344cb92e6c5
makefu <github@syntax-fehler.de>
2017-07-16 15:44:53 +0200

    ma sources: add musnix to studio,vbob,wbob

--- makefu/1systems/studio/source.nix
+++ makefu/1systems/studio/source.nix
@@ -2,6 +2,6 @@ import <stockholm/makefu/source.nix> {
   name="studio";
   override.musnix.git = {
     url = https://github.com/musnix/musnix.git;
-    ref = "f0ec1f3";
+    ref = "d8b989f";
   };
 }
