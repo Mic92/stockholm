7aa31b7babafb2e10bbf8418800ef561bccbd67b
lassulus <lassulus@lassul.us>
2017-07-25 23:10:51 +0200

    krebs buildbot: reintroduce packages with minimal overrides

---
+++ krebs/5pkgs/simple/buildbot-classic-slave/default.nix
@@ -0,0 +1,21 @@
+{ coreutils, fetchgit, fetchFromGitHub, buildbot-classic, python2Packages, ... }:
+
+python2Packages.buildPythonApplication {
+  name = "buildbot-slave-0.8.12";
+  namePrefix = "";
+
+  src = buildbot-classic.src;
+  postUnpack = "sourceRoot=\${sourceRoot}/slave";
+
+  patchPhase = ''
+    substituteInPlace buildslave/scripts/logwatcher.py --replace /usr/bin/tail ${coreutils}/bin/tail
+  '';
+
+  propagatedBuildInputs = [ python2Packages.twisted ];
+  doCheck = false;
+
+  postInstall = ''
+    mkdir -p "$out/share/man/man1"
+    cp docs/buildslave.1 "$out/share/man/man1"
+  '';
+}
---
+++ krebs/5pkgs/simple/buildbot-classic/default.nix
@@ -0,0 +1,47 @@
+{ fetchgit, fetchFromGitHub, python2Packages, ... }:
+let
+  # https://github.com/NixOS/nixpkgs/issues/14026
+  nixpkgs-fix = import (fetchgit {
+    url = https://github.com/nixos/nixpkgs;
+    rev = "e026b5c243ea39810826e68362718f5d703fb5d0";
+    sha256 = "11lqd480bi6xbi7xbh4krrxmbp6a6iafv1d0q3sj461al0x0has8";
+  }) {};
+
+in nixpkgs-fix.buildPythonApplication {
+  name = "buildbot-0.8.12";
+  namePrefix = "";
+  patches = [];
+
+  src = fetchFromGitHub {
+    owner = "krebscode";
+    repo = "buildbot-classic";
+    rev = "5b4f5f6f1";
+    sha256 = "1j3xn1gjzvsf90jvfmyln71fzlhjx642ivrqf47zfxpkacljja93";
+  };
+  postUnpack = "sourceRoot=\${sourceRoot}/master";
+
+  patchPhase =
+    # The code insists on /usr/bin/tail, /usr/bin/make, etc.
+    '' echo "patching erroneous absolute path references..."
+       for i in $(find -name \*.py)
+       do
+         sed -i "$i" \
+             -e "s|/usr/bin/python|$(type -P python)|g ; s|/usr/bin/||g"
+       done
+
+      sed -i 's/==/>=/' setup.py
+    '';
+
+  propagatedBuildInputs = [
+    python2Packages.jinja2
+    python2Packages.twisted
+    nixpkgs-fix.pythonPackages.dateutil_1_5
+    nixpkgs-fix.pythonPackages.sqlalchemy_migrate_0_7
+  ];
+  doCheck = false;
+  postInstall = ''
+    mkdir -p "$out/share/man/man1"
+    cp docs/buildbot.1 "$out/share/man/man1"
+  '';
+}
+
