32d8c202b0ec037dcf78ebf6ecad730cfb9c5272
lassulus <lassulus@lassul.us>
2017-07-28 20:20:40 +0200

    icarus: set correct ipv6

--- krebs/3modules/lass/default.nix
+++ krebs/3modules/lass/default.nix
@@ -254,7 +254,7 @@ with import <stockholm/lib>;
       nets = rec {
         retiolum = {
           ip4.addr = "10.243.133.114";
-          ip6.addr = "42:0:0:0:0:0:1ca0:1205";
+          ip6.addr = "42:0:0:0:0:0:01ca:1205";
           aliases = [
             "icarus.r"
             "cgit.icarus.r"
