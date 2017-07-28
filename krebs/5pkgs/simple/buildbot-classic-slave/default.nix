ca33cce6101091075fdffbb866eda58b40b0334e
makefu <github@syntax-fehler.de>
2017-07-29 00:28:54 +0200

    buildbot-classic-slave: version detection with git

--- krebs/5pkgs/simple/buildbot-classic-slave/default.nix
+++ krebs/5pkgs/simple/buildbot-classic-slave/default.nix
@@ -1,16 +1,15 @@
-{ coreutils, fetchgit, fetchFromGitHub, buildbot-classic, python2Packages, ... }:
+{ coreutils, fetchgit, git, buildbot-classic, python2Packages, ... }:
 
 python2Packages.buildPythonApplication {
-  name = "buildbot-classic-slave-0.8.12";
+  name = "buildbot-classic-slave-0.8.13";
   namePrefix = "";
 
   src = buildbot-classic.src;
   postUnpack = "sourceRoot=\${sourceRoot}/slave";
 
-  patchPhase = ''
-    substituteInPlace buildslave/scripts/logwatcher.py --replace /usr/bin/tail ${coreutils}/bin/tail
-  '';
+  patchPhase = ":";
 
+  nativeBuildInputs = [ git ];
   propagatedBuildInputs = [ python2Packages.twisted ];
   doCheck = false;
 
