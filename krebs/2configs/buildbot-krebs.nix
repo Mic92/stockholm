751780e400ba357262963a111d4b4799102cdb2e
lassulus <lassulus@lassul.us>
2017-07-29 02:19:28 +0200

    krebs buildbot-krebs: just build own host

--- krebs/2configs/buildbot-krebs.nix
+++ krebs/2configs/buildbot-krebs.nix
@@ -6,10 +6,7 @@
 
   networking.firewall.allowedTCPPorts = [ 80 8010 9989 ];
   krebs.ci.enable = true;
-  krebs.ci.users.krebs ={
-    hosts = [
-      "test-all-krebs-modules"
-      config.networking.hostName
-    ];
-  };
+  krebs.ci.users.krebs.hosts = [
+    config.networking.hostName
+  ];
 }
