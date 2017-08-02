2653cbd257a9e4255bad6d01e6aed4639821b59c
makefu <github@syntax-fehler.de>
2017-08-02 22:38:26 +0200

    ma stk1160: revert to hacky override

--- makefu/2configs/hw/stk1160.nix
+++ makefu/2configs/hw/stk1160.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
   # TODO: un-pin linuxPackages somehow
+  boot.kernelPackages = builtins.trace "Warning: overriding kernel Packages with 4.9" pkgs.linuxPackages;
   nixpkgs.config.packageOverrides = pkgs: {
-    linux_latest = pkgs.linux_latest.override {
+    linux_4_9 = pkgs.linux_4_9.override {
         extraConfig = ''
           MEDIA_ANALOG_TV_SUPPORT y
           VIDEO_STK1160_COMMON m
