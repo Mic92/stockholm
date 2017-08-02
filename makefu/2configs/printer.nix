a6b5b8dffd6023835e0fbb34c9d13364d9a4f1b3
makefu <github@syntax-fehler.de>
2017-08-02 11:49:27 +0200

    ma printer: add support for magicolor

--- makefu/2configs/printer.nix
+++ makefu/2configs/printer.nix
@@ -1,15 +1,30 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
-{
+let
+  mainUser = config.krebs.build.user.name;
+in {
   services.printing = {
     enable = true;
     drivers = [
       pkgs.samsungUnifiedLinuxDriver
-      pkgs.cups-dymo
+      pkgs.cups-dymo # dymo labelwriter
+      pkgs.foo2zjs # magicolor 1690mf
     ];
   };
 
   # scanners are printers just in reverse anyway
-  hardware.sane.enable = true;
-  hardware.sane.extraBackends = [ pkgs.samsungUnifiedLinuxDriver ];
+  services.saned.enable = true;
+  users.users."${mainUser}".extraGroups = [ "scanner" ];
+
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.samsungUnifiedLinuxDriver ];
+
+    # $ scanimage -p --format=jpg --mode=Gray --source="Automatic Document Feeder" -v --batch="lol%d.jpg" --resolution=150
+
+    # requires 'sane-extra', scan via:
+    extraConfig."magicolor" = ''
+      net 10.42.20.30 0x2098
+    ''; # 10.42.20.30: uhrenkind.shack magicolor 1690mf
+  };
 }
