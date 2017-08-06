35a43cd74b7f0f4f9b53c6af1062d07c47bc80a5
lassulus <lassulus@lassul.us>
2017-08-06 16:10:01 +0200

    l bitcoin: add stuff for all the coins

--- lass/2configs/bitcoin.nix
+++ lass/2configs/bitcoin.nix
@@ -3,11 +3,24 @@
 let
   mainUser = config.users.extraUsers.mainUser;
 in {
-  environment.systemPackages = with pkgs; [
-    electrum
-  ];
 
+  krebs.per-user.bch.packages = [
+    pkgs.electron-cash
+  ];
+  krebs.per-user.bitcoin.packages = [
+    pkgs.electrum
+  ];
+  krebs.per-user.ethereum.packages = [
+    pkgs.go-ethereum
+  ];
   users.extraUsers = {
+    bch = {
+      name = "bch";
+      description = "user for bch stuff";
+      home = "/home/bch";
+      useDefaultShell = true;
+      createHome = true;
+    };
     bitcoin = {
       name = "bitcoin";
       description = "user for bitcoin stuff";
@@ -15,8 +28,16 @@ in {
       useDefaultShell = true;
       createHome = true;
     };
+    ethereum = {
+      name = "ethereum";
+      description = "user for ethereum stuff";
+      home = "/home/ethereum";
+      useDefaultShell = true;
+      createHome = true;
+    };
   };
   security.sudo.extraConfig = ''
     ${mainUser.name} ALL=(bitcoin) NOPASSWD: ALL
+    ${mainUser.name} ALL=(bch) NOPASSWD: ALL
   '';
 }
