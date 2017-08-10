Mic92/stockholm

commit a4c61396ed53bb640b8b557d80b3b283264ccd1a

override all Haskell packages

krebs/5pkgs/haskell/default.nix
@@ -1,15 +1,20 @@
 with import <stockholm/lib>;
-
+let
+  overrides = self: super:
+    listToAttrs
+      (map
+        (name: nameValuePair (removeSuffix ".nix" name)
+                             (self.callPackage (./. + "/${name}") {}))
+        (filter
+          (name: name != "default.nix" && !hasPrefix "." name)
+          (attrNames (readDir ./.))));
+in
 self: super:
 {
+  haskell.packages = mapAttrs (_: pkgs: pkgs.override {
+    inherit overrides;
+  }) super.haskell.packages;
   haskellPackages = super.haskellPackages.override {
-    overrides = self: super:
-      listToAttrs
-        (map
-          (name: nameValuePair (removeSuffix ".nix" name)
-                               (self.callPackage (./. + "/${name}") {}))
-          (filter
-            (name: name != "default.nix" && !hasPrefix "." name)
-            (attrNames (readDir ./.))));
+    inherit overrides;
   };
 }

commit 585092f686f82316911641c9eeecd3843e9031f0

haskell overlay: only override packages
Fixes a regression introduced by a4c61396ed53bb640b8b557d80b3b283264ccd1a.

krebs/5pkgs/haskell/default.nix
@@ -11,9 +11,13 @@ let
 in
 self: super:
 {
-  haskell.packages = mapAttrs (_: pkgs: pkgs.override {
-    inherit overrides;
-  }) super.haskell.packages;
+  haskell = super.haskell // {
+    packages = mapAttrs (name: value:
+      if hasAttr "override" value
+        then value.override { inherit overrides; }
+        else value
+    ) super.haskell.packages;
+  };
   haskellPackages = super.haskellPackages.override {
     inherit overrides;
   };