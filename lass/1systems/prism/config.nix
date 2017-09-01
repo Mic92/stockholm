66f76c34b72f295a341cebe5e80a7574960592e6
lassulus <lassulus@lassul.us>
2017-09-01 22:31:54 +0200

    l: no longer sync nixpkgs

--- lass/1systems/prism/config.nix
+++ lass/1systems/prism/config.nix
@@ -245,10 +245,6 @@ in {
         OnUnitInactiveSec = "2min";
         RandomizedDelaySec = "2min";
       };
-      krebs.repo-sync.repos.nixpkgs.timerConfig = {
-        OnBootSec = "90min";
-        OnUnitInactiveSec = "24h";
-      };
     }
     {
       lass.usershadow = {
