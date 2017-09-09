f82de12a9a235a97085b768848c97dd023285066
makefu <github@syntax-fehler.de>
2017-09-09 20:58:50 +0200

    ma docker: add self to docker group

--- makefu/2configs/virtualisation/docker.nix
+++ makefu/2configs/virtualisation/docker.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs;[
     docker
     docker_compose
   ];
+  users.extraUsers.${config.krebs.build.user.name}.extraGroups = [ "docker" ];
 }
