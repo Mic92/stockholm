4b19401a82378ef9d091c8f58da0a67b33ec8ac6
makefu <github@syntax-fehler.de>
2017-09-11 23:02:58 +0200

    ma printer: add SXC-3205W network scanner

--- makefu/2configs/printer.nix
+++ makefu/2configs/printer.nix
@@ -14,17 +14,20 @@ in {
 
   # scanners are printers just in reverse anyway
   services.saned.enable = true;
-  users.users."${mainUser}".extraGroups = [ "scanner" ];
+  users.users."${mainUser}".extraGroups = [ "scanner" "lp" ];
 
   hardware.sane = {
     enable = true;
-    extraBackends = [ pkgs.samsungUnifiedLinuxDriver ];
+    extraBackends = [ ];
 
     # $ scanimage -p --format=jpg --mode=Gray --source="Automatic Document Feeder" -v --batch="lol%d.jpg" --resolution=150
 
     # requires 'sane-extra', scan via:
-    extraConfig."magicolor" = ''
-      net 10.42.20.30 0x2098
-    ''; # 10.42.20.30: uhrenkind.shack magicolor 1690mf
+    #extraConfig."magicolor" = ''
+    #  net 10.42.20.30 0x2098
+    #''; # 10.42.20.30: uhrenkind.shack magicolor 1690mf
+    extraConfig."xerox_mfp" = ''
+      tcp 192.168.1.5
+    ''; #home printer SCX-3205W
   };
 }
