d3b17d180642d3a344495468c27355f6a7521d42
lassulus <lassulus@lassul.us>
2017-10-02 17:57:24 +0200

    l nixpkgs: 670b4e2 -> b61d084

--- lass/source.nix
+++ lass/source.nix
@@ -10,7 +10,7 @@ in
       nixos-config.symlink = "stockholm/lass/1systems/${name}/config.nix";
       nixpkgs.git = {
         url = https://github.com/nixos/nixpkgs;
-        ref = "670b4e2";
+        ref = "b61d084";
       };
       secrets.file = getAttr builder {
         buildbot = toString <stockholm/lass/2configs/tests/dummy-secrets>;
