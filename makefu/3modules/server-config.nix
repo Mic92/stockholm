52f9105027a7c2d70145d7d2db69452e148b2158
makefu <github@syntax-fehler.de>
2017-10-03 15:44:13 +0200

    ma server-config: retab

--- makefu/3modules/server-config.nix
+++ makefu/3modules/server-config.nix
@@ -1,15 +1,14 @@
 {config, lib, pkgs, ... }:
 
-with import <stockholm/lib>;
-{
+with lib;{
   options.makefu.server.primary-itf = lib.mkOption {
-		type = types.str;
-		description = "Primary interface of the server";
-	};
+    type = types.str;
+    description = "Primary interface of the server";
+  };
   options.makefu.gui.user = lib.mkOption {
-		type = types.str;
-		description = "GUI user";
+    type = types.str;
+    description = "GUI user";
     default = config.krebs.build.user.name;
-	};
+  };
 }
 
