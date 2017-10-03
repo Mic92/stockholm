c54d84b9efe01a7f4f8837b2308b7e2d61f1926f
lassulus <lassulus@lassul.us>
2017-10-03 13:43:13 +0200

    l sqlBackup: set mysql.dataDir to /var/mysql

--- lass/2configs/websites/sqlBackup.nix
+++ lass/2configs/websites/sqlBackup.nix
@@ -3,12 +3,13 @@
 {
   krebs.secret.files.mysql_rootPassword = {
     path = "${config.services.mysql.dataDir}/mysql_rootPassword";
-    owner.name = "root";
+    owner.name = "mysql";
     source-path = toString <secrets> + "/mysql_rootPassword";
   };
 
   services.mysql = {
     enable = true;
+    dataDir = "/var/mysql";
     package = pkgs.mariadb;
     rootPassword = config.krebs.secret.files.mysql_rootPassword.path;
   };
