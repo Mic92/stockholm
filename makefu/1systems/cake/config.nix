f7f1d7a4462a801f23c3483fb1c3d2a4130a5240
makefu <github@syntax-fehler.de>
2017-10-08 22:21:16 +0200

    ma cake.r: add firmware for wifi

--- makefu/1systems/cake/config.nix
+++ makefu/1systems/cake/config.nix
@@ -22,6 +22,20 @@
   boot.tmpOnTmpfs = lib.mkForce false;
 
   hardware.enableRedistributableFirmware = true;
+  hardware.firmware = [
+    (pkgs.stdenv.mkDerivation {
+     name = "broadcom-rpi3-rest";
+     src = pkgs.fetchurl {
+     url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/54bab3d/brcm80211/brcm/brcmfmac43430-sdio.txt";
+     sha256 = "19bmdd7w0xzybfassn7x4rb30l70vynnw3c80nlapna2k57xwbw7";
+     };
+     phases = [ "installPhase" ];
+     installPhase = ''
+     mkdir -p $out/lib/firmware/brcm
+     cp $src $out/lib/firmware/brcm/brcmfmac43430-sdio.txt
+     '';
+     })
+  ];
   networking.wireless.enable = true;
 
 # File systems configuration for using the installer's partition layout
