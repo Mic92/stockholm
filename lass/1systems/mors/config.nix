f758f8cb07f0c2922ba00e06c6a59135c39a613a
lassulus <lassulus@lassul.us>
2017-11-12 13:01:59 +0100

    l mors.r: import br.nix from lass

--- lass/1systems/mors/config.nix
+++ lass/1systems/mors/config.nix
@@ -29,7 +29,7 @@ with import <stockholm/lib>;
     <stockholm/lass/2configs/syncthing.nix>
     <stockholm/lass/2configs/otp-ssh.nix>
     <stockholm/lass/2configs/c-base.nix>
-    <stockholm/tv/2configs/br.nix>
+    <stockholm/lass/2configs/br.nix>
     {
       #risk of rain port
       krebs.iptables.tables.filter.INPUT.rules = [
