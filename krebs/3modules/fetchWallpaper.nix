3c631cfa76849cb4640e17d8cdda969e4b6f423e
lassulus <lassulus@lassul.us>
2017-12-03 22:29:18 +0100

    fetchWallpaper: get DISPLAY from xserver.display

--- krebs/3modules/fetchWallpaper.nix
+++ krebs/3modules/fetchWallpaper.nix
@@ -26,8 +26,8 @@ let
       default = "/var/lib/wallpaper";
     };
     display = mkOption {
-      type = types.str;
-      default = ":0";
+      type = types.int;
+      default = config.services.xserver.display;
     };
     unitConfig = mkOption {
       type = types.attrsOf types.str;
@@ -81,7 +81,7 @@ let
 
       environment = {
         URL = cfg.url;
-        DISPLAY = cfg.display;
+        DISPLAY = ":${toString cfg.display}";
       };
       restartIfChanged = true;
 
