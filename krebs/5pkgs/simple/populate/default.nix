939976b0cc1166f0a212b283cc1c8022a784b648
tv <tv@krebsco.de>
2017-12-15 00:14:30 +0100

    populate: 1.2.5 -> 2.0.0

--- krebs/5pkgs/simple/populate/default.nix
+++ krebs/5pkgs/simple/populate/default.nix
@@ -1,24 +1,24 @@
-{ coreutils, fetchgit, git, jq, openssh, proot, rsync, stdenv, ... }:
+{ coreutils, fetchgit, git, gnused, jq, openssh, rsync, stdenv, ... }:
 
 let
   PATH = stdenv.lib.makeBinPath [
     coreutils
     git
+    gnused
     jq
     openssh
-    proot
     rsync
   ];
 in
 
 stdenv.mkDerivation rec {
   name = "populate";
-  version = "1.2.5";
+  version = "2.0.0";
 
   src = fetchgit {
     url = http://cgit.ni.krebsco.de/populate;
     rev = "refs/tags/v${version}";
-    sha256 = "10s4x117zp5whqq991xzw1i2jc1xhl580kx8hhzv8f1b4c9carx1";
+    sha256 = "01cvrg3m2ypg59in1qlr3rd8yzpf002k6pzjls2qb68jwkyf0h2n";
   };
 
   phases = [
