7d1d831b9763725496d441aee0d78de5f3fe010a
nin <nineinchnade@gmail.com>
2017-12-14 21:56:31 +0100

    nin 1 h config: delete skype

--- nin/1systems/hiawatha/config.nix
+++ nin/1systems/hiawatha/config.nix
@@ -15,7 +15,6 @@ with lib;
     <stockholm/nin/2configs/git.nix>
     <stockholm/nin/2configs/retiolum.nix>
     <stockholm/nin/2configs/termite.nix>
-    <stockholm/nin/2configs/skype.nix>
   ];
 
   krebs.build.host = config.krebs.hosts.hiawatha;
