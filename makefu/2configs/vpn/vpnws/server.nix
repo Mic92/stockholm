e2369d551e211c0eeb360868c2bba30564e33ca0
makefu <github@syntax-fehler.de>
2017-12-18 21:27:26 +0100

    ma vpn: add vpnws client and server

--- makefu/2configs/vpn/vpnws/server.nix
+++ makefu/2configs/vpn/vpnws/server.nix
@@ -4,11 +4,20 @@ let
   uid = "nginx";
   gid = "nginx";
   ip = "${pkgs.iproute}/bin/ip";
+  socket = "/run/vpn.sock";
+  htpasswd = (toString <secrets>) + "/vpn-ws-auth";
+  nginx-prepared-secrets = "/var/spool/nginx/vpn-ws-auth";
 in {
+  systemd.services.vpn-ws-auth-prepare = {
+    wantedBy = [ "multi-user.target" ];
+    before = [ "nginx.service" ];
+    script = "install -m700 -o${uid} -g${gid} ${htpasswd} ${nginx-prepared-secrets}";
+  };
   services.nginx.virtualHosts."euer.krebsco.de".locations."/vpn" = {
-    # TODO client auth
     extraConfig = ''
-      uwsgi_pass   unix:/run/vpn.sock;
+      auth_basic "please stand by...";
+      auth_basic_user_file ${nginx-prepared-secrets};
+      uwsgi_pass   unix:${socket};
       include      ${pkgs.nginx}/conf/uwsgi_params;
     '';
   };
@@ -27,9 +36,7 @@ in {
         ${ip} link set vpnws up
         ${ip} addr add 10.244.1.1/24 dev vpnws || :
       '';
-      ExecStart = pkgs.writeDash "vpnws-start" ''
-        ${pkg}/bin/vpn-ws --tuntap vpnws /run/vpn.sock
-      '';
+      ExecStart = "${pkg}/bin/vpn-ws --uid ${uid} --gid ${gid} --tuntap vpnws ${socket}";
     };
   };
 }
