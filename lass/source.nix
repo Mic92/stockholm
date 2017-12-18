8f80cd6fb41f963362d8044187f38b54244c227a
lassulus <lassulus@lassul.us>
2017-12-18 11:01:35 +0100

    l nixpkgs: cb751f9 -> af7e479

--- lass/source.nix
+++ lass/source.nix
@@ -10,7 +10,7 @@ in
       nixos-config.symlink = "stockholm/lass/1systems/${name}/config.nix";
       nixpkgs.git = {
         url = https://github.com/nixos/nixpkgs;
-        ref = "cb751f9";
+        ref = "af7e479";
       };
       secrets = getAttr builder {
         buildbot.file = toString <stockholm/lass/2configs/tests/dummy-secrets>;
