dd817f9e26ee2aeae839dddc73b869d218a337cb
lassulus <lassulus@lassul.us>
2017-12-29 15:27:06 +0100

    l nixpkgs: 53e6d67 -> 3aec59c

--- lass/source.nix
+++ lass/source.nix
@@ -10,7 +10,7 @@ in
       nixos-config.symlink = "stockholm/lass/1systems/${name}/config.nix";
       nixpkgs.git = {
         url = https://github.com/nixos/nixpkgs;
-        ref = "53e6d67";
+        ref = "3aec59c";
       };
       secrets = getAttr builder {
         buildbot.file = toString <stockholm/lass/2configs/tests/dummy-secrets>;
