e48b4eb4606f6d0ec0b930016a53e7e7cfcbfb64
lassulus <lassulus@lassul.us>
2018-01-02 20:58:32 +0100

    hw x220 rngd: mkForce -> mkDefault

--- krebs/2configs/hw/x220.nix
+++ krebs/2configs/hw/x220.nix
@@ -23,7 +23,7 @@ with import <stockholm/lib>;
     pkgs.vaapiVdpau
   ];
 
-  security.rngd.enable = mkForce true;
+  security.rngd.enable = mkDefault true;
 
   services.xserver = {
     videoDriver = "intel";
