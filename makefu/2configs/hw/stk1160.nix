79ac162a9e967993252d910f5a6bb74c810fd0a1
makefu <github@syntax-fehler.de>
2018-01-28 14:16:11 +0100

    ma hw/stk1160: bump linux kernel

--- makefu/2configs/hw/stk1160.nix
+++ makefu/2configs/hw/stk1160.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   # TODO: un-pin linuxPackages somehow
-  boot.kernelPackages = builtins.trace "Warning: overriding kernel Packages with 4.9" pkgs.linuxPackages;
   nixpkgs.config.packageOverrides = pkgs: {
-    linux_4_9 = pkgs.linux_4_9.override {
+    linux_4_14 = pkgs.linux_4_14.override {
         extraConfig = ''
           MEDIA_ANALOG_TV_SUPPORT y
           VIDEO_STK1160_COMMON m
