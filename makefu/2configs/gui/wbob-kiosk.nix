3ba905f2b0cff91cdeaa46c3eaed34d4bfc01215
makefu <github@syntax-fehler.de>
2018-01-29 14:45:26 +0100

    ma wbob-kiosk: provide chromium

--- makefu/2configs/gui/wbob-kiosk.nix
+++ makefu/2configs/gui/wbob-kiosk.nix
@@ -1,11 +1,13 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 {
 
   imports = [
       ./base.nix
   ];
+  users.users.makefu.packages = [ pkgs.chromium ];
   services.xserver = {
     layout = lib.mkForce "de";
+    xkbVariant = lib.mkForce "";
 
     windowManager = lib.mkForce {
       awesome.enable = false;
@@ -16,7 +18,7 @@
     # xrandrHeads = [ "HDMI1" "HDMI2" ];
     # prevent screen from turning off, disable dpms
     displayManager.sessionCommands = ''
-      xset s off -dpms
+      xset -display :0 s off -dpms
       xrandr --output HDMI2 --right-of HDMI1
     '';
   };
