119856187d954204592ac9d702b9c0327db82eb1
lassulus <lassulus@lassul.us>
2018-02-13 17:13:18 +0100

    l: add dunst

--- lass/1systems/mors/config.nix
+++ lass/1systems/mors/config.nix
@@ -31,6 +31,7 @@ with import <stockholm/lib>;
     <stockholm/lass/2configs/c-base.nix>
     <stockholm/lass/2configs/br.nix>
     <stockholm/lass/2configs/ableton.nix>
+    <stockholm/lass/2configs/dunst.nix>
     {
       #risk of rain port
       krebs.iptables.tables.filter.INPUT.rules = [
