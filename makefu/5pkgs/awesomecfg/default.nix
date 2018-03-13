28324a02d902b0ca31a383f162c585fb8f9b2972
makefu <github@syntax-fehler.de>
2018-03-13 11:34:28 +0100

    ma awesome: make locker configurable

--- makefu/5pkgs/awesomecfg/default.nix
+++ makefu/5pkgs/awesomecfg/default.nix
@@ -1,23 +1,23 @@
 { pkgs
 , lib
 , alsaUtils
-, xlockmore
 , xbacklight
 , modkey?"Mod4"
+, locker? "${pkgs.xlock}/bin/xlock -mode blank"
 , ... }:
 
 {
   # replace: @alsaUtils@ @xlockmore@ @xbacklight@ @modkey@
   full = lib.makeOverridable pkgs.substituteAll {
     name = "awesome_full_config";
-    inherit alsaUtils xlockmore xbacklight modkey;
+    inherit alsaUtils locker xbacklight modkey;
     isExecutable = false;
     src = ./full.cfg;
   };
 
   kiosk = lib.makeOverridable pkgs.substituteAll {
     name = "awesome_kiosk_config";
-    inherit alsaUtils xlockmore xbacklight modkey;
+    inherit alsaUtils locker xbacklight modkey;
     isExecutable = false;
     src = ./kiosk.lua;
   };
