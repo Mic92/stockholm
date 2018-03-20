0c04595ddfdd25b7d5cbb508609d731736a2f00e
jeschli <jeschli@gmail.com>
2018-03-20 16:37:12 +0100

    jeschli bln: config cosmetics

--- jeschli/1systems/bln/config.nix
+++ jeschli/1systems/bln/config.nix
@@ -1,30 +1,18 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, lib, pkgs, ... }:
 # bln config file
 {
   imports =
-    [ # Include the results of the hardware scan.
-      <stockholm/jeschli>
+    [ <stockholm/jeschli>
       <stockholm/jeschli/2configs/virtualbox.nix>
       <stockholm/jeschli/2configs/urxvt.nix>
       ./hardware-configuration.nix
-      # ./dcso-vpn.nix
     ];
-  # Use the systemd-boot EFI boot loader.
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   jeschliFontSize = 20;
-  # Use the GRUB 2 boot loader.
-#  boot.loader.grub.enable = true;
-#  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
+
   environment.shellAliases = {
     n = "nix-shell";
     gd = "cd /home/markus/go/src/gitlab.dcso.lolcat";
@@ -34,10 +22,12 @@
       LOGNAME=jeschli exec nix-shell -I stockholm="$PWD" --run 'deploy  --system="bln"'
     '';
   };
-  networking.hostName = lib.mkForce "BLN02NB0232"; # Define your hostname.
+  networking.hostName = lib.mkForce "BLN02NB0232";
   networking.networkmanager.enable = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
+
   # Setup Packages
   nixpkgs.config.allowUnfree = true;
   environment.variables = { GOROOT= [ "${pkgs.go.out}/share/go" ]; };
@@ -61,7 +51,7 @@
     emacs
   # databases
     sqlite
-  # internet 
+  # internet
     thunderbird
     hipchat
     chromium
@@ -92,41 +82,22 @@
 
   programs.bash.enableCompletion = true;
   programs.vim.defaultEditor = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.printing.drivers = [ pkgs.postscript-lexmark ];
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  # services.xserver.xrandrHeads = [
-  #  { output = "eDP1"; }
-  #  { output = "DP-2-2-8"; primary = true; }
-  #  { output = "DP-2-1-8"; monitorConfig = ''Option "Rotate" "left"''; }
-  # ];
+
   services.xserver.windowManager.xmonad.enable = true;
   services.xserver.windowManager.xmonad.enableContribAndExtras = true;
   services.xserver.displayManager.sddm.enable = true;
-#  services.xserver.desktopManager.gnome3.enable = true;
   services.xserver.dpi = 100;
   fonts.fontconfig.dpi = 100;
-
-#  services.xserver.displayManager.sessionCommands = ''
-#    (sleep 1 && ${pkgs.xorg.xrandr}/bin/xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 5120x688 --rotate normal --output DP1 --off --output DP2-1 --mode 2560x1440 --pos 2560x328 --rotate normal --output DP2-2 --primary --mode 2560x1440 --pos 0x328 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
-#'';
 
   users.extraUsers.jeschli = {
     isNormalUser = true;
@@ -134,25 +105,19 @@
     uid = 1000;
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
-
+  system.stateVersion = "17.09";
   # Gogland Debugger workaround
-#  nixpkgs.config.packageOverrides = super: {
-#    idea.gogland = lib.overrideDerivation super.idea.gogland (attrs: {
-#      postFixup = ''
-#	interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
-#	patchelf --set-interpreter $interp $out/gogland*/plugins/intellij-go-plugin/lib/dlv/linux/dlv
-#        chmod +x $out/gogland*/plugins/intellij-go-plugin/lib/dlv/linux/dlv
-#     '';
-#    });
-#  };
+  #  nixpkgs.config.packageOverrides = super: {
+  #    idea.gogland = lib.overrideDerivation super.idea.gogland (attrs: {
+  #      postFixup = ''
+  #	interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
+  #	patchelf --set-interpreter $interp $out/gogland*/plugins/intellij-go-plugin/lib/dlv/linux/dlv
+  #        chmod +x $out/gogland*/plugins/intellij-go-plugin/lib/dlv/linux/dlv
+  #     '';
+  #    });
+  #  };
 
   virtualisation.docker.enable = true;
-  
 
   # DCSO Certificates
   security.pki.certificateFiles = [
@@ -165,7 +130,7 @@
    (pkgs.fetchurl { url = "http://pki.dcso.de/ca/PEM/DCSOCAIDENC3G1.pem"; sha256 = "0smdjjvz95n652cb45yhzdb2lr83zg52najgbzf6lm3w71f8mv7f"; })
    # VBOX certs
    ./services.bundled.crt
-  ]; 
+  ];
 
   hardware.bluetooth.enable = true;
   krebs.build.host = config.krebs.hosts.bln;
