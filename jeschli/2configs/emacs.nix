7303238443b3a76af6d12df1992ee499d98a7902
jeschli <jeschli@gmail.com>
2018-03-22 16:19:29 +0100

    j emacs: +orgMode +evil-org

--- jeschli/2configs/emacs.nix
+++ jeschli/2configs/emacs.nix
@@ -21,6 +21,10 @@ let
     (require 'evil)
     (evil-mode 1)
     (require 'evil-org)
+    (add-hook 'org-mode-hook 'evil-org-mode)
+    (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+    (require 'evil-org-agenda)
+    (evil-org-agenda-set-keys)
   '';
   windowCosmetics = ''
     (tool-bar-mode -1)                  ; Disable the button bar atop screen
@@ -30,6 +34,16 @@ let
     (setq tab-width 2)                  ; Four spaces is a tab
     (setq visible-bell nil)             ; Disable annoying visual bell graphic
     (setq ring-bell-function 'ignore)   ; Disable super annoying audio bell
+  '';
+  orgMode = ''
+    (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+    (global-set-key "\C-cl" 'org-store-link)
+    (global-set-key "\C-ca" 'org-agenda)
+    (global-set-key "\C-cb" 'org-iswitchb)
+    (if (boundp 'org-user-agenda-files)
+      (setq org-agenda-files org-user-agenda-files)
+      (setq org-agenda-files (quote ("~/projects/notes")))
+    )
   '';
   emacsFile = ''
     ${packageRepos}
@@ -45,12 +59,7 @@ let
      '(package-selected-packages
        (quote
         (smex ox-jira org-plus-contrib org-mime org-jira neotree molokai-theme let-alist helm-fuzzy-find go-guru go-autocomplete flymake-go exec-path-from-shell evil-org cl-lib-highlight bbdb atom-one-dark-theme))))
-    (custom-set-faces
-     ;; custom-set-faces was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-     )
+     ${orgMode}
   '';
   dotEmacs = pkgs.writeText "dot-emacs" emacsFile;
   emacs = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages (epkgs: [
