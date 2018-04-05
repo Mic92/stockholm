1dd03483619d00d1afc6a278ded0cca6cff2d9ed
lassulus <lassulus@lassul.us>
2018-04-05 19:31:39 +0200

    add minimal working kops for stockholm

--- lass/1systems/mors/config.nix
+++ lass/1systems/mors/config.nix
@@ -168,14 +168,8 @@ with import <stockholm/lib>;
   environment.shellAliases = {
     deploy = pkgs.writeDash "deploy" ''
       set -eu
-      export PATH=${makeBinPath [
-        pkgs.bash
-        pkgs.coreutils
-        pkgs.nixUnstable
-      ]}
-      cd ~/stockholm
       export SYSTEM="$1"
-      exec nix-shell -I stockholm="$PWD" --run 'deploy --system="$SYSTEM"'
+      $(nix-build $HOME/stockholm/lass/kops.nix --no-out-link --argstr name "$SYSTEM" -A deploy)
     '';
   };
 
