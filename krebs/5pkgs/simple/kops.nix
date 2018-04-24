9a3d7eab701d354bfacc425ed5edc841514a8034
tv <tv@krebsco.de>
2018-04-24 19:59:40 +0200

    kops: 1.0.0 -> 1.1.0

--- krebs/5pkgs/simple/kops.nix
+++ krebs/5pkgs/simple/kops.nix
@@ -2,6 +2,6 @@
 
 fetchgit {
   url = https://cgit.krebsco.de/kops;
-  rev = "refs/tags/v1.0.0";
-  sha256 = "0wg8d80sxa46z4i7ir79sci2hwmv3qskzqdg0si64p6vazy8vckb";
+  rev = "refs/tags/v1.1.0";
+  sha256 = "0k3zhv2830z4bljcdvf6ciwjihk2zzcn9y23p49c6sba5hbsd6jb";
 }
