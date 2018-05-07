4b9ad61e03c18ae2687d49a365fb4e95ac2dbeec
lassulus <lassulus@lassul.us>
2018-05-07 19:51:21 +0200

    l icarus.r: add dpass & macchanger

--- lass/1systems/icarus/config.nix
+++ lass/1systems/icarus/config.nix
@@ -33,4 +33,9 @@
     SUBSYSTEM=="net", ATTR{address}=="00:24:d7:f0:a0:0c", NAME="wl0"
     SUBSYSTEM=="net", ATTR{address}=="f0:de:f1:71:cb:35", NAME="et0"
   '';
+
+  environment.systemPackages = with pkgs; [
+    macchanger
+    dpass
+  ];
 }
