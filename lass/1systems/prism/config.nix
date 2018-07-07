279bf4e5530c6337f05916d2d7f86f62921eb7b9
lassulus <lass@blue.r>
2018-07-08 01:02:22 +0200

    l prism.r: activate earlyoom

--- lass/1systems/prism/config.nix
+++ lass/1systems/prism/config.nix
@@ -376,4 +376,8 @@ with import <stockholm/lib>;
   krebs.build.host = config.krebs.hosts.prism;
   # workaround because grub store paths are broken
   boot.copyKernels = true;
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 5;
+  };
 }
