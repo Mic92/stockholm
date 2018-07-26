45a93b32a646a57a4edd7e1febf6d1be373a69da
makefu <github@syntax-fehler.de>
2018-07-26 21:50:16 +0200

    ma owncloud: add required zend_extension to enable opcache

--- makefu/2configs/deployment/owncloud.nix
+++ makefu/2configs/deployment/owncloud.nix
@@ -108,7 +108,6 @@ let
           # Add headers to serve security related headers
           add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
           add_header X-Content-Type-Options nosniff;
-          add_header X-Frame-Options "SAMEORIGIN";
           add_header X-XSS-Protection "1; mode=block";
           add_header X-Robots-Tag none;
           # Optional: Don't log access to assets
@@ -144,6 +143,7 @@ let
         opcache.memory_consumption=128
         opcache.save_comments=1
         opcache.revalidate_freq=1
+        zend_extension=${pkgs.php}/lib/php/extensions/opcache.so
 
         display_errors = on
         display_startup_errors = on
