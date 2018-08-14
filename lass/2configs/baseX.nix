c02fae77f38a553ee41452254ad6db5161561082
lassulus <lassulus@lassul.us>
2018-08-14 14:15:29 +0200

    l baseX: cleanup

--- lass/2configs/baseX.nix
+++ lass/2configs/baseX.nix
@@ -8,7 +8,6 @@ in {
     ./mpv.nix
     ./power-action.nix
     ./copyq.nix
-    ./livestream.nix
     ./urxvt.nix
     ./network-manager.nix
     {
@@ -61,19 +60,15 @@ in {
   environment.systemPackages = with pkgs; [
     acpi
     ag
-    bank
     cabal2nix
     cholerab
     dic
     dmenu
     font-size
-    gi
     gitAndTools.qgit
     git-preview
     gnome3.dconf
     lm_sensors
-    mpv-poll
-    much
     ncdu
     nix-index
     nix-repl
@@ -82,18 +77,14 @@ in {
     powertop
     push
     rxvt_unicode_with-plugins
-    slock
     sxiv
     taskwarrior
     termite
-    timewarrior
     xclip
     xephyrify
     xorg.xbacklight
     xorg.xhost
     xsel
-    youtube-tools
-    yt-next
     zathura
   ];
 
@@ -104,7 +95,6 @@ in {
     xlibs.fontschumachermisc
   ];
 
-  #lass.xserver.enable = true;
   services.xserver = {
     enable = true;
     layout = "us";
