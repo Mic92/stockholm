89951ebd816248bc4e62b2dad78b5db906c3cefe
lassulus <lassulus@lassul.us>
2018-08-14 21:52:56 +0200

    xmonad-stockholm: 1.1.1 -> 1.2.0

--- krebs/5pkgs/haskell/xmonad-stockholm.nix
+++ krebs/5pkgs/haskell/xmonad-stockholm.nix
@@ -3,11 +3,11 @@
 }:
 mkDerivation rec {
   pname = "xmonad-stockholm";
-  version = "1.1.1";
+  version = "1.2.0";
   src = fetchgit {
     url = http://cgit.ni.krebsco.de/xmonad-stockholm;
     rev = "refs/tags/v${version}";
-    sha256 = "05nnfg6q35z3qgf507qa80bz32jl4k719dl5phlmchplp3769585";
+    sha256 = "13mvmh3kk9a79l1nii028p0n7l95pb78wz9c4j42l90m02mg6cis";
   };
   libraryHaskellDepends = [
     base containers X11 X11-xft X11-xshape xmonad xmonad-contrib
