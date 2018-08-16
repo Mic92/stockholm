fdd7b3cacf398d40df9e71dc38db1a64faa14537
makefu <github@syntax-fehler.de>
2018-08-16 16:33:12 +0200

    ma nur.nix: don't export customized config

--- makefu/5pkgs/nur.nix
+++ makefu/5pkgs/nur.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
-  lib = import ./lib { inherit pkgs; }; # functions
   modules = import ../3modules/default.nix;
   overlays.full = import ./default.nix;
   pkgs = import ./default.nix pkgs pkgs;
