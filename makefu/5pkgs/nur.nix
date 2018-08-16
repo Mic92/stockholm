c603d3f78100c0b0d1f06837e2742a3ac03e236b
makefu <github@syntax-fehler.de>
2018-08-17 00:15:25 +0200

    ma nur.nix: remove modules part (for now)

--- makefu/5pkgs/nur.nix
+++ makefu/5pkgs/nur.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
-  modules = import ../3modules/default.nix;
   overlays.full = import ./default.nix;
   pkgs = import ./default.nix pkgs pkgs;
 } // (import ./default.nix pkgs pkgs)
