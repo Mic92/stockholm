7a1441bf1c053411921eff0f25c7e9099a13029c
tv <tv@krebsco.de>
2018-08-23 13:07:33 +0200

    krops: 1.3.0 -> 1.3.1

--- krebs/5pkgs/simple/krops.nix
+++ krebs/5pkgs/simple/krops.nix
@@ -2,6 +2,6 @@
 
 fetchgit {
   url = https://cgit.krebsco.de/krops;
-  rev = "refs/tags/v1.3.0";
-  sha256 = "1vfmm7aqi6y6cjz7vivamc70dkaxxxlihj48qvqc0dlj1bi331c2";
+  rev = "refs/tags/v1.3.1";
+  sha256 = "0bv984bjc6r1ys1q0wnszv1v1g1wdvjb6i0ibj7namwz0mhg67a7";
 }
