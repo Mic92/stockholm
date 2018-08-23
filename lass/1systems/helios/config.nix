e3884076fabf500e92a2a6b11452a7c1e74ea310
lassulus <lass@blue.r>
2018-08-23 11:34:45 +0200

    l helios.r: get rid of nearly everything

--- lass/1systems/helios/config.nix
+++ lass/1systems/helios/config.nix
@@ -1,48 +1,7 @@
 with import <stockholm/lib>;
-{ config, lib, pkgs, ... }:
-
+{ pkgs, ... }:
 {
-  imports = [
-    <stockholm/lass>
-    <stockholm/lass/2configs/baseX.nix>
-    <stockholm/lass/2configs/browsers.nix>
-    <stockholm/lass/2configs/mouse.nix>
-    <stockholm/lass/2configs/pass.nix>
-    {
-      services.xserver.dpi = 200;
-      fonts.fontconfig.dpi = 200;
-      lass.fonts.regular = "xft:Hack-Regular:pixelsize=22,xft:Symbola";
-      lass.fonts.bold =    "xft:Hack-Bold:pixelsize=22,xft:Symbola";
-      lass.fonts.italic =  "xft:Hack-RegularOblique:pixelsize=22,xft:Symbol";
-    }
-  ];
-  krebs.build.host = config.krebs.hosts.helios;
-
   environment.systemPackages = with pkgs; [
-    ag
-    vim
-    git
-    rsync
-    hashPassword
-    thunderbird
     dpass
-
-    # we want tensorflow! (with GPU acceleration)
-    python3Packages.tensorflowWithCuda
   ];
-
-  users.users = {
-    root.openssh.authorizedKeys.keys = [
-      config.krebs.users.lass-helios.pubkey
-    ];
-  };
-
-  services.tlp.enable = true;
-
-  services.printing.drivers = [ pkgs.postscript-lexmark ];
-
-  services.logind.extraConfig = ''
-    HandleLidSwitch=ignore
-  '';
-
 }
