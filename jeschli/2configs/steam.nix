228ca863767edc07ae50c82034d873ee7ef17310
jeschli <jeschli@gmail.com>
2018-09-04 20:09:24 +0200

    j steam: remove dead code

--- jeschli/2configs/steam.nix
+++ jeschli/2configs/steam.nix
@@ -9,14 +9,4 @@
   hardware.opengl.driSupport32Bit = true;
 
   #ports for inhome streaming
-  krebs.iptables = {
-    tables = {
-      filter.INPUT.rules = [
-        { predicate = "-p tcp --dport 27031"; target = "ACCEPT"; }
-        { predicate = "-p tcp --dport 27036"; target = "ACCEPT"; }
-        { predicate = "-p udp --dport 27031"; target = "ACCEPT"; }
-        { predicate = "-p udp --dport 27036"; target = "ACCEPT"; }
-      ];
-    };
-  };
 }
