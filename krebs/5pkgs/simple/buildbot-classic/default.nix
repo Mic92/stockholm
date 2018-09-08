ee74ba6b405a09fbfa718b0308cb49a984b8b0ef
lassulus <lass@blue.r>
2018-09-09 00:17:26 +0200

    buildbot-classic: 0.8.16 -> 0.8.17

--- krebs/5pkgs/simple/buildbot-classic/default.nix
+++ krebs/5pkgs/simple/buildbot-classic/default.nix
@@ -2,7 +2,7 @@
 
 python2Packages.buildPythonApplication rec {
   name = "buildbot-classic-${version}";
-  version = "0.8.16";
+  version = "0.8.17";
   namePrefix = "";
   patches = [];
 
@@ -10,7 +10,7 @@ python2Packages.buildPythonApplication rec {
     owner = "krebs";
     repo = "buildbot-classic";
     rev = version;
-    sha256 = "0g686n6m0cjfyympl0ksansllx503gby3hx9gmc8hiyx6x5fkjha";
+    sha256 = "0yn0n37rs2bhz9q0simnvyzz5sfrpqhbdm6pdj6qk7sab4y6xbq8";
   };
   postUnpack = "sourceRoot=\${sourceRoot}/master";
 
