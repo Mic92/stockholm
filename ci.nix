10135cbeacd74a36971582d421742f576528606d
lassulus <lass@blue.r>
2018-09-09 19:17:11 +0200

    add ci.nix

---
+++ ci.nix
@@ -0,0 +1,21 @@
+# usage: nix-instantiate --eval --strict --json ./ci.nix
+with import ./lib;
+let
+  pkgs = import <nixpkgs> { overlays = [ (import ./submodules/nix-writers/pkgs) ]; };
+  system =
+    import <nixpkgs/nixos/lib/eval-config.nix> {
+      modules = [{
+        imports = [
+          ./krebs
+          ./krebs/2configs
+        ];
+      }];
+    }
+  ;
+
+  ci-systems = filterAttrs (_: v: v.ci) system.config.krebs.hosts;
+
+  build = host: owner:
+  ((import (toString ./. + "/${owner}/krops.nix") { name = host; }).test {target = "${getEnv "HOME"}/stockholm-build";});
+
+in mapAttrs (n: h: build n h.owner.name) ci-systems
