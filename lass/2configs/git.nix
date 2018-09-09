f6e69c6ecb25fc96655ec1749747d9ccb7880365
lassulus <lass@blue.r>
2018-09-09 21:25:09 +0200

    l git: add lass as admin

--- lass/2configs/git.nix
+++ lass/2configs/git.nix
@@ -156,7 +156,7 @@ let
     with git // config.krebs.users;
     repo:
       singleton {
-        user = [ lass-mors lass-shodan lass-icarus lass-blue ];
+        user = [ lass lass-mors lass-shodan lass-icarus lass-blue ];
         repo = [ repo ];
         perm = push "refs/*" [ non-fast-forward create delete merge ];
       } ++
