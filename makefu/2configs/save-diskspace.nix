e576c3182a8d4bfc85ec0755d3eeae40603183d7
makefu <github@syntax-fehler.de>
2018-09-17 01:34:23 +0200

    Revert "ma save-diskspace: use new "documentation" key"
    This reverts commit 14cb17d0ce26f74434e68d2266ed2f3b8a1a3f0b.

--- makefu/2configs/save-diskspace.nix
+++ makefu/2configs/save-diskspace.nix
@@ -4,8 +4,8 @@ _:
   environment.noXlibs = true;
   nix.gc.automatic = true;
   nix.gc.dates = "03:10";
-  documentation.info.enable = false;
-  documentation.man.enable = false;
+  programs.info.enable = false;
+  programs.man.enable = false;
   services.journald.extraConfig = "SystemMaxUse=50M";
   services.nixosManual.enable = false;
 }
