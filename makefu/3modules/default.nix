49e0ae20c9ac96c3f2e12e0faf6d2bd7e9348d61
makefu <github@syntax-fehler.de>
2018-09-24 14:34:27 +0200

    ma modules: add airdcpp

--- makefu/3modules/default.nix
+++ makefu/3modules/default.nix
@@ -2,17 +2,18 @@ _:
 
 {
   imports = [
-    ./state.nix
-    ./populate.nix
+    ./airdcpp.nix
     ./awesome-extra.nix
     ./deluge.nix
     ./forward-journal.nix
     ./opentracker.nix
     ./ps3netsrv.nix
     ./logging-config.nix
+    ./populate.nix
     ./sane-extra.nix
     ./server-config.nix
     ./snapraid.nix
+    ./state.nix
     ./torrent.nix
     ./udpt.nix
   ];
