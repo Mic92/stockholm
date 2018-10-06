65dc542a2ae9ba565eb3848a9cbdb5da11a1563d
tv <tv@krebsco.de>
2018-10-06 21:46:42 +0200

    blessings: 1.1.0 -> 1.2.0

--- krebs/5pkgs/haskell/blessings.nix
+++ krebs/5pkgs/haskell/blessings.nix
@@ -1,11 +1,11 @@
 { mkDerivation, base, fetchgit, stdenv }:
 mkDerivation rec {
   pname = "blessings";
-  version = "1.1.0";
+  version = "1.2.0";
   src = fetchgit {
     url = http://cgit.ni.krebsco.de/blessings;
     rev = "refs/tags/v${version}";
-    sha256 = "1k908zap3694fcxdk4bb29s54b0lhdh557y10ybjskfwnym7szn1";
+    sha256 = "03hz43ixww0h4fwxqrlrlvmj3pxswhb50ijaapwjz8457il2r300";
   };
   libraryHaskellDepends = [ base ];
   doHaddock = false;
