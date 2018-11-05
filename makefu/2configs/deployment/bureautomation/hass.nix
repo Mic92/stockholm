2487cbc8829b9c81545d1627d4a03b8fed12de01
makefu <github@syntax-fehler.de>
2018-11-05 13:51:28 +0100

    ma wbob.r: more automation

--- makefu/2configs/deployment/bureautomation/hass.nix
+++ makefu/2configs/deployment/bureautomation/hass.nix
@@ -12,7 +12,7 @@ let
     payload_not_available= "Offline";
   };
   tasmota_stecki = name: topic:
-    ( tasmota_plug name topic) // 
+    ( tasmota_plug name topic) //
     { state_topic = "/bam/${topic}/stat/POWER";
       command_topic = "/bam/${topic}/cmnd/POWER";
   };
@@ -43,9 +43,6 @@ let
   };
 in {
   networking.firewall.allowedTCPPorts = [ 8123 ];
-  nixpkgs.config.permittedInsecurePackages = [
-    "homeassistant-0.65.5"
-  ];
 
   services.home-assistant = {
     enable = true;
@@ -53,6 +50,9 @@ in {
       homeassistant = {
         name = "Bureautomation";
         time_zone = "Europe/Berlin";
+        latitude = "48.8265";
+        longitude = "9.0676";
+        elevation = 303;
       };
 
       mqtt = {
@@ -101,26 +101,109 @@ in {
             sensorid = "5341";
             monitored_conditions = [ "P1" "P2" ];
           }
-          { platform = "influxdb";
-            queries = [
-              { name = "mean value of feinstaub P1";
-                where = '' "node" = 'esp8266-1355142' '';
-                measurement = "feinstaub";
-                database = "telegraf";
-                field = "P1";
-              }
-              { name = "mean value of feinstaub P2";
-                where = '' "node" = 'esp8266-1355142' '';
-                measurement = "feinstaub";
-                database = "telegraf";
-                field = "P2";
-              }
-            ];
+
+          { platform = "darksky";
+            api_key = lib.removeSuffix "\n"
+              (builtins.readFile <secrets/hass/darksky.apikey>);
+            language = "de";
+            monitored_conditions = [ "summary" "icon"
+            "nearest_storm_distance" "precip_probability"
+            "precip_intensity"
+            "temperature" # "temperature_high" "temperature_low"
+            "apparent_temperature"
+            "hourly_summary" # next 24 hours text
+            "minutely_summary"
+            "humidity"
+            "pressure"
+            "uv_index" ];
+            units =  "si" ;
+            update_interval = {
+                  days = 0;
+                  hours = 0;
+                  minutes = 30;
+                  seconds = 0;
+            };
+          }
+          #{ platform = "influxdb";
+          #  queries = [
+          #    { name = "mean value of feinstaub P1";
+          #      where = '' "node" = 'esp8266-1355142' '';
+          #      measurement = "feinstaub";
+          #      database = "telegraf";
+          #      field = "P1";
+          #    }
+          #    { name = "mean value of feinstaub P2";
+          #      where = '' "node" = 'esp8266-1355142' '';
+          #      measurement = "feinstaub";
+          #      database = "telegraf";
+          #      field = "P2";
+          #    }
+          #  ];
+          #}
+        ];
+        camera = [
+          { name = "Baumarkt";
+            platform = "generic";
+            still_image_url = http://t4915209254324-p80-c0-h6jv2afnujcoftrcstsafb45kdrqv4buy.webdirect.mdex.de/oneshotimage ;# baumarkt
+          }
+          { name = "Autobahn Heilbronn";
+            platform = "generic";
+            still_image_url = https://api.svz-bw.de/v2/verkehrskameras/kameras/K10 ;
+          }
+          { name = "Autobahn Singen";
+            platform = "generic";
+            still_image_url = https://api.svz-bw.de/v2/verkehrskameras/kameras/K11 ;
           }
         ];
       frontend = { };
       http = { };
-      feedreader.urls = [ "http://www.heise.de/security/rss/news-atom.xml" ];
+      conversation = {};
+      history = {};
+      logbook = {};
+      tts = [ { platform = "google";} ];
+      recorder = {};
+      group =
+      { default_view =
+        { view = "yes";
+          entities = [
+              "group.sensors"
+              "group.outside"
+              "group.switches"
+              "group.automation"
+              "group.camera"
+            ];
+          };
+        automation = [
+          "automation.turn_off_fernseher_10_minutes_after_last_movement"
+        ];
+        switches = [
+          "switch.bauarbeiterlampe"
+          "switch.blitzdings"
+          "switch.fernseher"
+          "switch.pluggy"
+        ];
+        camera = [
+          "camera.Baumarkt"
+          "camera.Autobahn_Heilbronn"
+          "camera.Autobahn_Singen"
+        ];
+        sensors = [
+          "binary_sensor.motion"
+          "sensor.easy2_dht22_humidity"
+          "sensor.easy2_dht22_temperature"
+        ];
+        outside = [
+          "sensor.ditzingen_pm10"
+          "sensor.ditzingen_pm25"
+          "sensor.dark_sky_temperature"
+          "sensor.dark_sky_humidity"
+          "sensor.dark_sky_pressure"
+          "sensor.dark_sky_hourly_summary"
+          "sensor.dark_sky_minutely_summary"
+        ];
+      };
+      # only for automation
+      # feedreader.urls = [ "http://www.heise.de/security/rss/news-atom.xml" ];
       automation = [
         { alias = "Turn on Fernseher on movement";
           trigger = {
@@ -144,6 +227,12 @@ in {
             service= "homeassistant.turn_off";
             entity_id= "switch.fernseher";
           };
+          condition = [{
+            condition = "time";
+            before = "06:30:00"; #only turn off between 6:30 and 18:00
+            after  = "18:00:00";
+            weekday = [ "mon" "tue" "wed" "thu" "fri" ];
+          }];
         }
       ];
     };
