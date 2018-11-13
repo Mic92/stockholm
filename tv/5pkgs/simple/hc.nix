b1dfd4b4937d48c4c14b12251bb554f562ef4737
tv <tv@krebsco.de>
2018-11-13 13:34:31 +0100

    tv hc: init at 1.0.0

---
+++ tv/5pkgs/simple/hc.nix
@@ -0,0 +1,37 @@
+{ coreutils, fetchgit, findutils, gawk, gnugrep, makeWrapper, qrencode, stdenv, texlive, utillinux, zbar }:
+
+stdenv.mkDerivation rec {
+  name = "hc-${meta.version}";
+
+  src = fetchgit {
+    url = "https://cgit.krebsco.de/hc";
+    rev = "refs/tags/v${meta.version}";
+    sha256 = "09349gja22p0j3xs082kp0fnaaada14bafszn4r3q7rg1id2slfb";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  buildPhase = null;
+
+  installPhase = ''
+    mkdir -p $out/bin
+
+    cp $src/bin/hc $out/bin/hc
+
+    wrapProgram $out/bin/hc \
+      --prefix PATH : ${stdenv.lib.makeBinPath [
+        coreutils
+        findutils
+        gawk
+        gnugrep
+        qrencode
+        texlive.combined.scheme-full
+        utillinux
+        zbar
+      ]}
+  '';
+
+  meta = {
+    version = "1.0.0";
+  };
+}
