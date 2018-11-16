083a8ca2aed285e94383b216347ee4e6ef44c6a6
tv <tv@krebsco.de>
2018-11-16 17:19:13 +0100

    tv vim elixir: b916c00 -> 0a847f0

--- tv/2configs/vim.nix
+++ tv/2configs/vim.nix
@@ -25,7 +25,14 @@ let {
     })
     pkgs.vimPlugins.fzfWrapper
     pkgs.vimPlugins.undotree
-    pkgs.vimPlugins.vim-elixir
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "vim-elixir-2018-08-17";
+      src = pkgs.fetchgit {
+        url = https://github.com/elixir-editors/vim-elixir;
+        rev = "0a847f0faed5ba2d94bb3d51f355c50f37ba025b";
+        sha256 = "1jl85wpgywhcvhgw02y8zpvqf0glr4i8522kxpvhsiacb1v1xh04";
+      };
+    })
     (pkgs.vimUtils.buildVimPlugin {
       name = "vim-syntax-jq";
       src = pkgs.fetchgit {
