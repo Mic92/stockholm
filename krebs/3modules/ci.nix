09ee7ca4d832bfdc836c9463513891f1e97db10b
lassulus <lassulus@lassul.us>
2018-11-27 00:58:07 +0100

    ci: add gcroot for build-scripts

--- krebs/3modules/ci.nix
+++ krebs/3modules/ci.nix
@@ -28,7 +28,13 @@ let
   getJobs = pkgs.writeDash "get_jobs" ''
     set -efu
     nix-build --no-out-link --quiet -Q ./ci.nix > /dev/null
-    nix-instantiate --quiet -Q --eval --strict --json ./ci.nix
+    js="$(nix-instantiate --quiet -Q --eval --strict --json ./ci.nix)"
+    echo "$js" | jq -r 'to_entries[] | [.key, .value] | @tsv' \
+      | while read -r host builder; do
+        gcroot=${shell.escape profileRoot}/$host-builder
+        ${pkgs.nix}/bin/nix-env -p "$gcroot" --set "$builder"
+      done
+    echo "$js"
   '';
 
   profileRoot = "/nix/var/nix/profiles/ci";
