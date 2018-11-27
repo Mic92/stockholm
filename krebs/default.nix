dae35c44c7cf67d10152a4fb32d212310f86199a
tv <tv@krebsco.de>
2018-11-27 11:53:19 +0100

    krebs: integrate 5pkgs as overlay

--- krebs/default.nix
+++ krebs/default.nix
@@ -1,12 +1,14 @@
-{ config, lib, pkgs, ... }:
-with import <stockholm/lib>;
 {
+
   imports = [
     ./3modules
-    {
-      nixpkgs.config.packageOverrides =
-        import ../submodules/nix-writers/pkgs pkgs;
-    }
   ];
-  nixpkgs.config.packageOverrides = import ./5pkgs pkgs;
+
+  nixpkgs = {
+    overlays = [
+      (import ./5pkgs)
+      (import ../submodules/nix-writers/pkgs)
+    ];
+  };
+
 }
