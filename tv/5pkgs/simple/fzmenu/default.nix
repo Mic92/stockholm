09556c7538565078ba9c55f834859ef6f96af90b
tv <tv@krebsco.de>
2018-11-28 09:20:25 +0100

    tv fzmenu: init

---
+++ tv/5pkgs/simple/fzmenu/default.nix
@@ -0,0 +1,34 @@
+{ coreutils, dash, gnused, fzf, pass, runCommand, rxvt_unicode, stdenv, utillinux, xdotool }:
+
+runCommand "fzmenu" {
+} /* sh */ ''
+  mkdir $out
+
+  cp -r ${./bin} $out/bin
+
+  substituteInPlace $out/bin/otpmenu \
+      --replace '#! /bin/sh' '#! ${dash}/bin/dash' \
+      --replace '#PATH=' PATH=${stdenv.lib.makeBinPath [
+        coreutils
+        dash
+        fzf
+        gnused
+        pass
+        rxvt_unicode
+        utillinux
+        xdotool
+      ]}
+
+  substituteInPlace $out/bin/passmenu \
+      --replace '#! /bin/sh' '#! ${dash}/bin/dash' \
+      --replace '#PATH=' PATH=${stdenv.lib.makeBinPath [
+        coreutils
+        dash
+        fzf
+        gnused
+        pass
+        rxvt_unicode
+        utillinux
+        xdotool
+      ]}
+''
