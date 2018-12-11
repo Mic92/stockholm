facbcdafc891094fa62857089b13fcc9926a4485
tv <tv@krebsco.de>
2018-12-11 21:59:41 +0100

    lib.setAttr: RIP

--- lib/default.nix
+++ lib/default.nix
@@ -29,8 +29,6 @@ let
       listToAttrs (map (name: nameValuePair name set.${name})
                        (filter (flip hasAttr set) names));
 
-    setAttr = name: value: set: set // { ${name} = value; };
-
     test = re: x: isString x && testString re x;
 
     testString = re: x: match re x != null;
