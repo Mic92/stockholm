5f95c191932826c33f75b590f8f34f2ceb9cb492
makefu <github@syntax-fehler.de>
2018-12-13 01:31:41 +0100

    ma bureautomation: try to fix automation for fernseher

--- makefu/2configs/bureautomation/hass.nix
+++ makefu/2configs/bureautomation/hass.nix
@@ -112,7 +112,6 @@ in {
             "temperature" # "temperature_high" "temperature_low"
             "apparent_temperature"
             "hourly_summary" # next 24 hours text
-            "minutely_summary"
             "humidity"
             "pressure"
             "uv_index" ];
@@ -212,27 +211,44 @@ in {
             to = "on";
           };
           action = {
-            service= "homeassistant.turn_on";
-            entity_id= "switch.fernseher";
+            service = "homeassistant.turn_on";
+            entity_id =  [ "switch.fernseher" "switch.blitzdings" ];
           };
         }
         { alias = "Turn off Fernseher 10 minutes after last movement";
-          trigger = {
+          trigger = [ 
+          { # trigger when movement was detected at the time
             platform = "state";
             entity_id = "binary_sensor.motion";
             to = "off";
             for.minutes = 10;
-          };
+          }
+          { # trigger at 20:00 no matter what
+            # to avoid 'everybody left before 18:00:00'
+            platform = "time";
+            at = "18:00:00";
+          }
+        ];
           action = {
-            service= "homeassistant.turn_off";
-            entity_id= "switch.fernseher";
+            service = "homeassistant.turn_off";
+            entity_id =  [ "switch.fernseher" "switch.blitzdings" ];
           };
-          condition = [{
-            condition = "time";
-            before = "06:30:00"; #only turn off between 6:30 and 18:00
-            after  = "18:00:00";
-            weekday = [ "mon" "tue" "wed" "thu" "fri" ];
-          }];
+          condition =
+          { condition = "and";
+            conditions = [
+              {
+                condition = "time";
+                before = "06:30:00"; #only turn off between 6:30 and 18:00
+                after  = "18:00:00";
+                # weekday = [ "mon" "tue" "wed" "thu" "fri" ];
+              }
+              {
+                condition = "state";
+                entity_id = "binary_sensor.motion";
+                state = "off";
+              }
+            ];
+          };
         }
       ];
     };
