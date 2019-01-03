2ce824f21f933710ef9f36864e1e585644da6eb5
lassulus <lassulus@lassul.us>
2019-01-03 21:49:05 +0100

    ci: build in stockholm-build

--- ci.nix
+++ ci.nix
@@ -16,6 +16,6 @@ let
   ci-systems = filterAttrs (_: v: v.ci) system.config.krebs.hosts;
 
   build = host: owner:
-  ((import (toString ./. + "/${owner}/krops.nix") { name = host; }).test {target = "${getEnv "HOME"}/stockholm-tmp";});
+  ((import (toString ./. + "/${owner}/krops.nix") { name = host; }).test {target = "${getEnv "HOME"}/stockholm-build";});
 
 in mapAttrs (n: h: build n h.owner.name) ci-systems
