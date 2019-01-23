31a987903f0d007a19a84d7db4c18b6641d0454a
tv <tv@krebsco.de>
2019-01-23 13:50:09 +0100

    reaktor2: 0.1.0 -> 0.1.1

--- krebs/5pkgs/haskell/reaktor2.nix
+++ krebs/5pkgs/haskell/reaktor2.nix
@@ -1,16 +1,16 @@
 { mkDerivation, aeson, attoparsec, base, blessings, bytestring
 , containers, data-default, fetchgit, filepath, lens, lens-aeson
 , network, network-simple, network-simple-tls, pcre-heavy
-, pcre-light, process, random, stdenv, text, time, transformers
-, unagi-chan, unix, unordered-containers
+, pcre-light, process, random, stdenv, stringsearch, text, time
+, transformers, unagi-chan, unix, unordered-containers, vector
 }:
 mkDerivation {
   pname = "reaktor2";
-  version = "0.1.0";
+  version = "0.1.1";
   src = fetchgit {
     url = "https://cgit.krebsco.de/reaktor2";
-    sha256 = "0g5b40y2gbknghzw12jar1im87k0g4hjg259wf1gp5v55dh3xwk6";
-    rev = "d40815fd56bf1895af89b72b1171675a2e0ae5f7";
+    sha256 = "0d78560gj8hm02zaps63f9mby4lzz82f45i8ypwc9l3lnsypdsr3";
+    rev = "4fa5cb937c016f8c10bf8f40d017ca3a436db2d3";
     fetchSubmodules = true;
   };
   isLibrary = false;
@@ -18,8 +18,8 @@ mkDerivation {
   executableHaskellDepends = [
     aeson attoparsec base blessings bytestring containers data-default
     filepath lens lens-aeson network network-simple network-simple-tls
-    pcre-heavy pcre-light process random text time transformers
-    unagi-chan unix unordered-containers
+    pcre-heavy pcre-light process random stringsearch text time
+    transformers unagi-chan unix unordered-containers vector
   ];
   license = stdenv.lib.licenses.mit;
 }
