2e5ed5d1bbf158a933f330119b9a5f931e192560
tv <tv@krebsco.de>
2019-01-24 21:16:44 +0100

    reaktor2: 0.1.2 -> 0.1.3

--- krebs/5pkgs/haskell/reaktor2.nix
+++ krebs/5pkgs/haskell/reaktor2.nix
@@ -6,11 +6,11 @@
 }:
 mkDerivation {
   pname = "reaktor2";
-  version = "0.1.2";
+  version = "0.1.3";
   src = fetchgit {
     url = "https://cgit.krebsco.de/reaktor2";
-    sha256 = "1198sajpkd6m87j402y796270hwifyn9wk2by6wcrxvwhq1vgs9k";
-    rev = "d5f66b27b2cd7c36eb7c2e81b0cdca10c5a5ef90";
+    sha256 = "15qhycfja7psnd7v5hn4qb5wrs6bjx4qhny49nkhb7agj4vzwnwi";
+    rev = "6c629a0cc422872abdfc40f9621ac0c4f6a420a8";
     fetchSubmodules = true;
   };
   isLibrary = false;
