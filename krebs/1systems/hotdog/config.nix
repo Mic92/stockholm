aa070d5db7e9fac66addcbf2005f4c404c84c019
lassulus <lassulus@lassul.us>
2019-01-27 03:33:30 +0100

    hotdog.r: import nscd-fix

--- krebs/1systems/hotdog/config.nix
+++ krebs/1systems/hotdog/config.nix
@@ -14,6 +14,7 @@
     <stockholm/krebs/2configs/ircd.nix>
     <stockholm/krebs/2configs/reaktor-retiolum.nix>
     <stockholm/krebs/2configs/reaktor-krebs.nix>
+    <stockholm/krebs/2configs/nscd-fix.nix>
     <stockholm/krebs/2configs/repo-sync.nix>
   ];
 
