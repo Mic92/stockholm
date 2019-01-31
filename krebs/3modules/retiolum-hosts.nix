5c076d9f37dc495b7a61cae84c5600b1ff7a5d09
tv <tv@krebsco.de>
2019-01-31 14:40:57 +0100

    retiolum-hosts: RIP

--- krebs/3modules/retiolum-hosts.nix
+++
@@ -1,8 +0,0 @@
-with import <stockholm/lib>;
-{ config, pkgs, ... }: {
-  nixpkgs.config.packageOverrides = super: {
-    retiolum-hosts =
-      trace "pkgs.retiolum-hosts is deprecated, use pkgs.krebs-hosts-retiolum instead"
-      pkgs.krebs-hosts-retiolum;
-  };
-}
