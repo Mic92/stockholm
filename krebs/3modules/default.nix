3a2d5affbb7766af4776f21b308c425008a4943d
lassulus <lassulus@lassul.us>
2019-02-08 09:43:33 +0100

    external: actually import palo.nix

--- krebs/3modules/default.nix
+++ krebs/3modules/default.nix
@@ -100,6 +100,7 @@ let
     { krebs = import ./krebs  { inherit config; }; }
     { krebs = import ./lass   { inherit config; }; }
     { krebs = import ./makefu { inherit config; }; }
+    { krebs = import ./external/palo.nix { inherit config; }; }
     { krebs = import ./tv     { inherit config; }; }
     {
       krebs.dns.providers = {
