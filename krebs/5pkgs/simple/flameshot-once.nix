7f40ff9b5c763f5706b8e71f3f618363b6cd9781
tv <tv@krebsco.de>
2019-02-08 17:26:44 +0100

    flameshot-once: use qt5.qtbase

--- krebs/5pkgs/simple/flameshot-once.nix
+++ krebs/5pkgs/simple/flameshot-once.nix
@@ -6,7 +6,8 @@ pkgs.symlinkJoin {
     (pkgs.writeDashBin "flameshot-once" ''
       export PATH=${pkgs.stdenv.lib.makeBinPath [
         pkgs.flameshot
-      ]}''${PATH:+:$PATH}
+        pkgs.qt5.qtbase
+      ]}
       exec ${pkgs.haskellPackages.flameshot-once}/bin/flameshot-once "$@"
     '')
     pkgs.haskellPackages.flameshot-once
