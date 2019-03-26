bd539533789a85a1786ae6f997a76d16d453b7f4
lassulus <lassulus@lassul.us>
2019-03-26 14:39:52 +0100

    l blue: use file nixpkgs deployment when not testing

--- lass/1systems/blue/source.nix
+++ lass/1systems/blue/source.nix
@@ -1,20 +1,11 @@
 { lib, pkgs, ... }:
 {
   nixpkgs = lib.mkForce {
-    derivation = let
+    file = toString (pkgs.fetchFromGitHub {
+      owner = "nixos";
+      repo = "nixpkgs";
       rev = (lib.importJSON ../../../krebs/nixpkgs.json).rev;
       sha256 = (lib.importJSON ../../../krebs/nixpkgs.json).sha256;
-    in ''
-      with import (builtins.fetchTarball {
-        url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
-        sha256 = "${sha256}";
-      }) {};
-      pkgs.fetchFromGitHub {
-        owner = "nixos";
-        repo = "nixpkgs";
-        rev = "${rev}";
-        sha256 = "${sha256}";
-      }
-    '';
+    });
   };
 }
