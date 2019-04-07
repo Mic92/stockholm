15fe4f74471fd4da438a7939fbad50ca41ae36b5
lassulus <lassulus@lassul.us>
2019-04-07 18:58:28 +0200

    l backups: don't mount /backups

--- lass/2configs/backup.nix
+++ lass/2configs/backup.nix
@@ -2,19 +2,11 @@
 with import <stockholm/lib>;
 
 {
-  fileSystems = {
-    "/backups" = {
-      device = "/dev/pool/backup";
-      fsType = "ext4";
-    };
-  };
   users.users.backup = {
     useDefaultShell = true;
     home = "/backups";
     createHome = true;
     openssh.authorizedKeys.keys = with config.krebs.hosts; [
-      mors.ssh.pubkey
-      prism.ssh.pubkey
       blue.ssh.pubkey
     ];
   };
