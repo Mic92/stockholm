fc85c6d16800f73eb088c42cf67f9f6918ce3001
lassulus <lassulus@lassul.us>
2019-04-07 18:56:50 +0200

    l blue.r: add checksum check to nixpkgs populate

--- lass/1systems/blue/source.nix
+++ lass/1systems/blue/source.nix
@@ -1,11 +1,14 @@
 { lib, pkgs, ... }:
 {
   nixpkgs = lib.mkForce {
-    file = toString (pkgs.fetchFromGitHub {
-      owner = "nixos";
-      repo = "nixpkgs";
-      rev = (lib.importJSON ../../../krebs/nixpkgs.json).rev;
-      sha256 = (lib.importJSON ../../../krebs/nixpkgs.json).sha256;
-    });
+    file = {
+      path = toString (pkgs.fetchFromGitHub {
+        owner = "nixos";
+        repo = "nixpkgs";
+        rev = (lib.importJSON ../../../krebs/nixpkgs.json).rev;
+        sha256 = (lib.importJSON ../../../krebs/nixpkgs.json).sha256;
+      });
+      useChecksum = true;
+    };
   };
 }
