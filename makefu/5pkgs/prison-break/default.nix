6fa6659b8fc5d2e00131aa470408a17c9002ac75
makefu <github@syntax-fehler.de>
2019-04-17 20:46:12 +0200

    ma pkgs.prison-break: 0.1.0 -> 1.0.0

--- makefu/5pkgs/prison-break/default.nix
+++ makefu/5pkgs/prison-break/default.nix
@@ -3,12 +3,12 @@ with pkgs.python3.pkgs;
 
 buildPythonPackage rec {
   pname = "prison-break";
-  version = "0.1.0";
+  version = "1.0.0";
   src = fetchFromGitHub {
     owner = "makefu";
     repo = pname;
-    rev = "5eed6371e151e716faafa054e005bd98d77b4b5d";
-    sha256 = "170zs9grbgkx83ghg6pm13v7vhi604y44j550ypp2x26nidaw63j";
+    rev = "1.0.0";
+    sha256 = "0ab42z6qr42vz4fc077irn9ykrrylagx1dzlw8dqcanf49dxd961";
   };
   propagatedBuildInputs = [
     docopt
