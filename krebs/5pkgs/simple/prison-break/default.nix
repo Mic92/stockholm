795ba0b57fec8fd5c7ce732a8d478c7cada762f8
makefu <github@syntax-fehler.de>
2019-05-08 13:56:33 +0200

    ma pkgs.prison-break: bump to 1.0.1

--- krebs/5pkgs/simple/prison-break/default.nix
+++ krebs/5pkgs/simple/prison-break/default.nix
@@ -3,12 +3,12 @@ with pkgs.python3.pkgs;
 
 buildPythonPackage rec {
   pname = "prison-break";
-  version = "1.0.0";
+  version = "1.0.1";
   src = fetchFromGitHub {
     owner = "makefu";
     repo = pname;
-    rev = "1.0.0";
-    sha256 = "0ab42z6qr42vz4fc077irn9ykrrylagx1dzlw8dqcanf49dxd961";
+    rev = version;
+    sha256 = "1q9bw1hbz0cayclixplyc85kaq05mg6n2zz8mbydljvknidd4p6a";
   };
   propagatedBuildInputs = [
     docopt
