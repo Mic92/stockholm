b46cb34eed46dab1cb77f00c3d42efce3e075431
tv <tv@krebsco.de>
2019-05-21 21:52:20 +0200

    tv vim: move jq to overlay

--- tv/2configs/vim.nix
+++ tv/2configs/vim.nix
@@ -16,16 +16,9 @@ let {
   extra-runtimepath = concatMapStringsSep "," (pkg: "${pkg.rtp}") [
     pkgs.tv.vimPlugins.elixir
     pkgs.tv.vimPlugins.fzf
+    pkgs.tv.vimPlugins.jq
     pkgs.vimPlugins.fzfWrapper
     pkgs.vimPlugins.undotree
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "vim-syntax-jq";
-      src = pkgs.fetchgit {
-        url = https://github.com/vito-c/jq.vim;
-        rev = "99d55a300047946a82ecdd7617323a751199ad2d";
-        sha256 = "09c94nah47wx0cr556w61h6pfznxld18pfblc3nv51ivbw7cjqyx";
-      };
-    })
     (pkgs.vimUtils.buildVimPlugin {
       name = "file-line-1.0";
       src = pkgs.fetchgit {
