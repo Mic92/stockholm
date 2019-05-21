9b573a35f24b4d259f909fc191c8123a1aeec7b3
tv <tv@krebsco.de>
2019-05-21 21:56:25 +0200

    tv vim: move vim to overlay

--- tv/2configs/vim.nix
+++ tv/2configs/vim.nix
@@ -19,21 +19,9 @@ let {
     pkgs.tv.vimPlugins.fzf
     pkgs.tv.vimPlugins.hack
     pkgs.tv.vimPlugins.jq
+    pkgs.tv.vimPlugins.vim
     pkgs.vimPlugins.fzfWrapper
     pkgs.vimPlugins.undotree
-    ((rtp: rtp // { inherit rtp; }) (pkgs.writeTextFile (let
-      name = "vim";
-    in {
-      name = "vim-syntax-${name}-1.0.0";
-      destination = "/syntax/${name}.vim";
-      text = /* vim */ ''
-        ${concatMapStringsSep "\n" (s: /* vim */ ''
-          syn keyword vimColor${s} ${s}
-            \ containedin=ALLBUT,vimComment,vimLineComment
-          hi vimColor${s} ctermfg=${s}
-        '') (map (i: lpad 3 "0" (toString i)) (range 0 255))}
-      '';
-    })))
     ((rtp: rtp // { inherit rtp; }) (pkgs.writeTextFile (let
       name = "showsyntax";
     in {
