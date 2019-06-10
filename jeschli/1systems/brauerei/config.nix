8079877eee34d0a658e8419adfa8987e648388a8
jeschli <jeschli@gmail.com>
2019-06-10 18:56:19 +0200

    j brauerei: -python35 +python37

--- jeschli/1systems/brauerei/config.nix
+++ jeschli/1systems/brauerei/config.nix
@@ -78,19 +78,9 @@ in
     gcc
     ghc
     go
-    python35
-    python35Packages.pip
-    (vagrant.override {
-      bundlerEnv = bundlerEnv.override {
-        bundler = bundler.overrideAttrs (old: {
-          name = "bundler-1.16.1";
-          src = fetchurl {
-            url = "https://rubygems.org/gems/bundler-1.16.1.gem";
-            sha256 = "1s2nq4qnffxg3kwrk7cnwxcvfihlhxm9absl2l6d3qckf3sy1f22";
-          };
-        });
-      };
-    })
+    python37
+    python37Packages.pip
+    pipenv
   # dev tools
     gnumake
     jetbrains.clion
