405d9555e4cf2fc171900f03621acfd523641dee
makefu <github@syntax-fehler.de>
2019-06-18 07:47:49 +0200

    ma mail: pin tarball

--- makefu/2configs/mail/mail.euer.nix
+++ makefu/2configs/mail/mail.euer.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 {
   imports = [
-    (builtins.fetchTarball "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/v2.2.1/nixos-mailserver-v2.2.1.tar.gz")
+    (builtins.fetchTarball {
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/v2.2.1/nixos-mailserver-v2.2.1.tar.gz";
+      sha256 = "03d49v8qnid9g9rha0wg2z6vic06mhp0b049s3whccn1axvs2zzx";
+    }
+    )
   ];
 
   mailserver = {
