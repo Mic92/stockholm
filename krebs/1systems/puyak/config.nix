f880fb21ca540506ba27e49c6ba92aa83923cd43
makefu <github@syntax-fehler.de>
2019-06-19 19:19:39 +0200

    ma extra-gui: no more rambox

--- krebs/1systems/puyak/config.nix
+++ krebs/1systems/puyak/config.nix
@@ -7,13 +7,13 @@
     <stockholm/krebs/2configs/secret-passwords.nix>
     <stockholm/krebs/2configs/hw/x220.nix>
 
-    <stockholm/krebs/2configs/stats/puyak-client.nix>
     <stockholm/krebs/2configs/binary-cache/nixos.nix>
     <stockholm/krebs/2configs/binary-cache/prism.nix>
     <stockholm/krebs/2configs/go.nix>
     <stockholm/krebs/2configs/ircd.nix>
     <stockholm/krebs/2configs/news.nix>
     <stockholm/krebs/2configs/news-spam.nix>
+    <stockholm/krebs/2configs/shack/prometheus/node.nix>
   ];
 
   krebs.build.host = config.krebs.hosts.puyak;
@@ -59,10 +59,7 @@
     };
   };
 
-  services.logind.extraConfig = ''
-    HandleLidSwitch=ignore
-  '';
-
+  services.logind.lidSwitch = "ignore";
   services.udev.extraRules = ''
     SUBSYSTEM=="net", ATTR{address}=="8c:70:5a:b2:84:58", NAME="wl0"
     SUBSYSTEM=="net", ATTR{address}=="3c:97:0e:07:b9:14", NAME="et0"
--- makefu/2configs/tools/extra-gui.nix
+++ makefu/2configs/tools/extra-gui.nix
@@ -14,6 +14,6 @@
     saleae-logic
     gitAndTools.gitFull
     signal-desktop
-    rambox
+    # rambox
   ];
 }
