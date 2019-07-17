8e0c6fc8c5a4d76f6485ee317dc91d5cfffe70f6
makefu <github@syntax-fehler.de>
2019-07-17 10:55:36 +0200

    shack/mqtt: update permissions

--- krebs/2configs/shack/mqtt.nix
+++ krebs/2configs/shack/mqtt.nix
@@ -1,13 +1,13 @@
 {
-  network.firewall.allowedTCPPorts = [ 1883 ];
-  network.firewall.allowedUDPPorts = [ 1883 ];
+  networking.firewall.allowedTCPPorts = [ 1883 ];
+  networking.firewall.allowedUDPPorts = [ 1883 ];
   services.mosquitto = {
     enable = true;
     host = "0.0.0.0";
     users = {};
     # TODO: secure that shit
     aclExtraConf = ''
-      pattern readwrite /#
+      pattern readwrite #
     '';
     allowAnonymous = true;
   };
