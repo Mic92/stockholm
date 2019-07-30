f15434475faf17795bd296fc3570bc7dca34591a
tv <tv@krebsco.de>
2019-07-30 19:20:32 +0200

    tv sshd: drop default generated host key
    Fixes an issue with 222160fc3a195415e859048bdef21f703154b3ee which
    caused the generated ed25519 host key to be preferred over the one
    defined in krebs/3modules/*/default.nix

--- tv/2configs/sshd.nix
+++ tv/2configs/sshd.nix
@@ -5,12 +5,6 @@ with import <stockholm/lib>;
 {
   services.openssh = {
     enable = true;
-    hostKeys = [
-      {
-        type = "ed25519";
-        path = "/etc/ssh/ssh_host_ed25519_key";
-      }
-    ];
   };
   tv.iptables.input-internet-accept-tcp = singleton "ssh";
 }
