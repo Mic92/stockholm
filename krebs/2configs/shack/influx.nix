86592a723309d45dc09ff90872c0ee4aa7e52dc0
makefu <github@syntax-fehler.de>
2019-09-02 13:56:34 +0200

    shack/influx: enable collectd receiver

--- krebs/2configs/shack/influx.nix
+++ krebs/2configs/shack/influx.nix
@@ -1,9 +1,12 @@
-# hostname: influx.shack
+{pkgs, ... }: # hostname: influx.shack
 let
   port = 8086;
+  collectd-port = 25826;
+  db = "collectd_db";
 in
 {
   networking.firewall.allowedTCPPorts = [ port ]; # for legacy applications
+  networking.firewall.allowedUDPPorts = [ collectd-port ];
   services.nginx.virtualHosts."influx.shack" = {
     locations."/" = {
       proxyPass = "http://localhost:${toString port}/";
@@ -14,6 +17,17 @@ in
     extraConfig = {
       http.bind-address = "0.0.0.0:${toString port}";
       http.log-enabled = false;
+      http.write-tracing = false;
+      http.suppress-write-log = true;
+      data.trace-logging-enabled = false;
+      data.query-log-enabled = false;
+      monitoring.enabled = false;
+      collectd = [{
+        enabled = true;
+        typesdb = "${pkgs.collectd}/share/collectd/types.db";
+        database = db;
+        bind-address = ":${toString collectd-port}";
+      }];
     };
   };
 }
