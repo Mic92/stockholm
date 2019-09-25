acf23122acaf1c0161b4fe3315362dfc159fab86
makefu <github@syntax-fehler.de>
2019-09-25 14:51:43 +0200

    ma stats/telegraf: export to mqtt

--- makefu/2configs/stats/telegraf/default.nix
+++ makefu/2configs/stats/telegraf/default.nix
@@ -1,15 +1,23 @@
 {...}:
 let
   url = "http://localhost:8086";
+  mqtt_server = "localhost:1883";
 in {
   services.telegraf = {
     enable = true;
     extraConfig = {
-      agent.debug = true;
+      agent.debug = false;
       outputs = {
         influxdb = [{
           urls = [ url ];
           database = "telegraf";
+        }];
+        mqtt = [{
+          servers = [ mqtt_server ];
+          topic_prefix = "/telegraf";
+          data_format = "json";
+          qos = 0;
+          batch = false;
         }];
       };
     };
