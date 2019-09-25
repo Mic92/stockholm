9fb322853726c5dd4a027aa1ce681278434fc256
makefu <github@syntax-fehler.de>
2019-09-25 14:07:28 +0200

    ma editor/vim: use youcompleteme,ultisnips

--- makefu/2configs/editor/vim.nix
+++ makefu/2configs/editor/vim.nix
@@ -22,7 +22,7 @@ in {
       vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
       vimrcConfig.vam.pluginDictionaries = [
         { names = [ "undotree"
-          # "YouCompleteMe"
+          "YouCompleteMe" "UltiSnips"
           "vim-better-whitespace" ]; }
         # vim-nix handles indentation better but does not perform sanity
         { names = [ "vim-addon-nix" ]; ft_regex = "^nix\$"; }
