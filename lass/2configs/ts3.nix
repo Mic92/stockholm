65919b90f2d25a0c8a8e184d57a586a09163c753
lassulus <lassulus@lassul.us>
2019-10-14 15:40:59 +0200

    l ts3: open file transfer port

--- lass/2configs/ts3.nix
+++ lass/2configs/ts3.nix
@@ -10,8 +10,8 @@
     { predicate = "-p tcp --dport 9987"; target = "ACCEPT"; }
     { predicate = "-p udp --dport 9987"; target = "ACCEPT"; }
     ##file transfer port
-    #{ predicate = "-p tcp --dport 30033"; target = "ACCEPT"; }
-    #{ predicate = "-p udp --dport 30033"; target = "ACCEPT"; }
+    { predicate = "-p tcp --dport 30033"; target = "ACCEPT"; }
+    { predicate = "-p udp --dport 30033"; target = "ACCEPT"; }
     ##query port
     #{ predicate = "-p tcp --dport 10011"; target = "ACCEPT"; }
     #{ predicate = "-p udp --dport 10011"; target = "ACCEPT"; }
