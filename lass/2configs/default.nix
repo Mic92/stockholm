9d148800eb4c5eb94c776818426abb36a02fcb7d
lassulus <lass@xerxes.r>
2019-10-14 15:15:58 +0200

    l: accept port 22 from wiregrill

--- lass/2configs/default.nix
+++ lass/2configs/default.nix
@@ -186,7 +186,9 @@ with import <stockholm/lib>;
     enable = true;
     tables = {
       nat.PREROUTING.rules = [
-        { predicate = "! -i retiolum -p tcp -m tcp --dport 22"; target = "REDIRECT --to-ports 0"; precedence = 100; }
+        { predicate = "-i retiolum -p tcp -m tcp --dport 22"; target = "ACCEPT"; precedence = 101; }
+        { predicate = "-i wiregrill -p tcp -m tcp --dport 22"; target = "ACCEPT"; precedence = 101; }
+        { predicate = "-p tcp -m tcp --dport 22"; target = "REDIRECT --to-ports 0"; precedence = 100; }
         { predicate = "-p tcp -m tcp --dport 45621"; target = "REDIRECT --to-ports 22"; precedence = 99; }
       ];
       nat.OUTPUT.rules = [
