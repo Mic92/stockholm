2a40b6c342b5b12ebdb45b27eca945b9de730c33
lassulus <lassulus@lassul.us>
2019-10-14 13:12:30 +0200

    cholerab: don't use IFD

--- krebs/5pkgs/simple/cholerab/default.nix
+++ krebs/5pkgs/simple/cholerab/default.nix
@@ -1,7 +1,16 @@
-{ fetchgit, callPackage }: let
-  src = fetchgit {
-    url = "https://github.com/krebs/cholerab";
+{ stdenv, fetchFromGitHub, pandoc }:
+stdenv.mkDerivation {
+  name = "cholerab";
+  src = fetchFromGitHub {
+    owner = "krebs";
+    repo = "cholerab";
     rev = "25d7ef051d6fc74d99b155e768b3c650296a230c";
     sha256 = "1pymw7v2ql42iq825ccx98s4fp9jsz5b2hjr1qad6bamfc6i7yy9";
   };
-in callPackage src {}
+  phases = [ "buildPhase" ];
+  buildPhase = ''
+    mkdir -p $out/share/man/man1
+    ${pandoc}/bin/pandoc -s -t man $src/thesauron.md -o $out/share/man/man1/thesauron.1
+    ${pandoc}/bin/pandoc -s -t man $src/enterprise-patterns.md -o $out/share/man/man1/enterprise-patterns.1
+  '';
+}
