4a1ab9bf6516f455de30b782ab5fc0c3c55e983f
lassulus <lassulus@lassul.us>
2019-12-01 16:26:04 +0100

    update-nixpkgs-unstable: fix commit msg

--- krebs/update-nixpkgs-unstable.sh
+++ krebs/update-nixpkgs-unstable.sh
@@ -6,4 +6,4 @@ nix-shell -p nix-prefetch-git --run 'nix-prefetch-git \
   --rev refs/heads/nixos-unstable' \
 > $dir/nixpkgs-unstable.json
 newrev=$(cat $dir/nixpkgs-unstable.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
-git commit $dir/nixpkgs.json -m "nixpkgs-unstable: $oldrev -> $newrev"
+git commit $dir/nixpkgs-unstable.json -m "nixpkgs-unstable: $oldrev -> $newrev"
