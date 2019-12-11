f1e7fcd50e495e0029278a1f7527dbb4c4ba9507
Jeschli <jeschli@gmail.com>
2019-12-11 19:30:49 +0100

    j reagenzglas: add haskell

--- jeschli/1systems/reagenzglas/config.nix
+++ jeschli/1systems/reagenzglas/config.nix
@@ -7,6 +7,7 @@
     <stockholm/jeschli/2configs/emacs.nix>
     <stockholm/jeschli/2configs/firefox.nix>
     <stockholm/jeschli/2configs/rust.nix>
+    <stockholm/jeschli/2configs/haskell.nix>
     <stockholm/jeschli/2configs/python.nix>
        ./desktop.nix
        ./i3-configuration.nix
