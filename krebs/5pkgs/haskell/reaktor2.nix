b20942d07d10afa68a58f7a0690ad4397e90a57f
tv <tv@krebsco.de>
2020-04-17 23:14:19 +0200

    reaktor2: 0.2.3-pre1 -> 0.3.0

--- krebs/5pkgs/haskell/reaktor2.nix
+++ krebs/5pkgs/haskell/reaktor2.nix
@@ -1,18 +1,18 @@
 { mkDerivation, aeson, async, attoparsec, base, blessings
 , bytestring, containers, data-default, fetchgit, filepath
 , hashable, lens, lens-aeson, network, network-simple
-, network-simple-tls, pcre-light, process, random, servant-server
-, stdenv, string-conversions, stringsearch, text, time
-, transformers, unagi-chan, unix, unordered-containers, vector, wai
-, warp
+, network-simple-tls, network-uri, pcre-light, process, random
+, servant-server, stdenv, string-conversions, stringsearch, text
+, time, transformers, unagi-chan, unix, unordered-containers
+, vector, wai, warp
 }:
 mkDerivation {
   pname = "reaktor2";
-  version = "0.2.3-pre1";
+  version = "0.3.0";
   src = fetchgit {
     url = "https://cgit.krebsco.de/reaktor2";
-    sha256 = "0jg8yln1np5w3bg8jpkymfqi0x3a47pfsk20sz4lp3r2hfpw0zlk";
-    rev = "56bb8aef0018650e14624e086bb8884bb7ecd4f7";
+    sha256 = "02hqpq8wcfd6rvi8qk10zy3f3lrzzqnjwqal4cbvksjn3vahz36h";
+    rev = "a6893c00f78a8acd0a4bfe7da87ab6889eabcf21";
     fetchSubmodules = true;
   };
   isLibrary = false;
@@ -20,8 +20,8 @@ mkDerivation {
   executableHaskellDepends = [
     aeson async attoparsec base blessings bytestring containers
     data-default filepath hashable lens lens-aeson network
-    network-simple network-simple-tls pcre-light process random
-    servant-server string-conversions stringsearch text time
+    network-simple network-simple-tls network-uri pcre-light process
+    random servant-server string-conversions stringsearch text time
     transformers unagi-chan unix unordered-containers vector wai warp
   ];
   license = stdenv.lib.licenses.mit;
