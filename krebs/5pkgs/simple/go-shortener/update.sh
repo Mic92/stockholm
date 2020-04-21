d623e7e09c89f99f7120a18675a2a51767a3c677
lassulus <lassulus@lassul.us>
2020-04-21 18:34:45 +0200

    go-shortener: use nodejs 10

--- krebs/5pkgs/simple/go-shortener/update.sh
+++ krebs/5pkgs/simple/go-shortener/update.sh
@@ -1,2 +1,4 @@
-node2nix -8 -i pkgs.json -c combine.nix
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p nodePackages_10_x.node2nix
+node2nix -10 -i pkgs.json -c combine.nix
 rm node-env.nix combine.nix
