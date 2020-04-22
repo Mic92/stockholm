89cf2fdfa82b994679a96f420461b593393a84ff
makefu <github@syntax-fehler.de>
2020-04-22 19:14:14 +0200

    ma virtualbox: disable extensionPack

--- makefu/2configs/virtualisation/virtualbox.nix
+++ makefu/2configs/virtualisation/virtualbox.nix
@@ -2,7 +2,7 @@
 
 {
   virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  #virtualisation.virtualbox.host.enableExtensionPack = true;
   # virtualisation.virtualbox.host.enableHardening = false;
   users.extraGroups.vboxusers.members = [ config.krebs.build.user.name ];
 }
