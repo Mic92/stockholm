343fc36f47b79a71014d0d23fafcda1f7af0a203
makefu <github@syntax-fehler.de>
2020-07-02 08:21:37 +0200

    ma exim-retiolum: enable rspamd

--- makefu/2configs/exim-retiolum.nix
+++ makefu/2configs/exim-retiolum.nix
@@ -5,6 +5,7 @@ with import <stockholm/lib>;
   networking.firewall.allowedTCPPorts = [ 25 ];
 
   krebs.exim-retiolum.enable = true;
+  krebs.exim-retiolum.rspamd.enable = true;
   environment.systemPackages = with pkgs; [
     msmtp
   ];
