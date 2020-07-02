98aceafd67b3b1fd99d9aa51c3ea43049540ad37
makefu <github@syntax-fehler.de>
2020-07-02 08:44:30 +0200

    ma nur: bump rev

--- makefu/2configs/nur.nix
+++ makefu/2configs/nur.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:{
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/NUR/archive/b7f7e072b3fa56aa0d86dfe3689cb13f0615bbbe.tar.gz";
-      sha256 = "0slxxg3r460aa1rc83j0rklmr0i1hyzfbjy0kn30fyh1l3lqb22m";
+    url = "https://github.com/nix-community/NUR/archive/28e5326ecbaef8b961bec7d18dbfe35f522fb2b1.tar.gz";
+      sha256 = "1yni27g71r8n9bgsck7lz5dzx2fciljnba249yqhr9k3mzlkr7yb";
     }
   ){
       inherit pkgs;
