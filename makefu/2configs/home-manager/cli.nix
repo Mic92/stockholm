1af471af1eec077bc21ff0c96679362f055beaaa
makefu <github@syntax-fehler.de>
2020-09-16 22:28:58 +0200

    ma home-manager/direnv: remove custom use_nix

--- makefu/2configs/home-manager/cli.nix
+++ makefu/2configs/home-manager/cli.nix
@@ -2,31 +2,6 @@
   imports = [ ./zsh.nix ];
   home-manager.users.makefu = {
     programs.direnv = {
-      stdlib = ''
-use_nix() {
-  local cache=".direnv.$(nixos-version --hash)"
-
-  if [[ ! -e "$cache" ]] || \
-    [[ "$HOME/.direnvrc" -nt "$cache" ]] || \
-    [[ ".envrc" -nt "$cache" ]] || \
-    [[ "default.nix" -nt "$cache" ]] || \
-    [[ "shell.nix" -nt "$cache" ]];
-  then
-    local tmp="$(mktemp "$${cache}.tmp-XXXXXXXX")"
-    trap "rm -rf '$tmp' >/dev/null" EXIT
-    nix-shell --show-trace "$@" --run 'direnv dump' > "$tmp" && \
-    mv "$tmp" "$cache"
-  fi
-
-  direnv_load cat "$cache"
-
-  if [[ $# = 0 ]]; then
-    watch_file default.nix
-    watch_file shell.nix
-    rm direnv.* 2>/dev/null
-  fi
-}
-'';
       enableZshIntegration = true;
     };
   };
