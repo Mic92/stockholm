d450eb6c37745af467c30c1c1a83f764fdeb6ce6
lassulus <lassulus@lassul.us>
2020-09-27 15:36:01 +0200

    l hass: make lovelace configurable

--- lass/2configs/hass/rooms/bett.nix
+++ lass/2configs/hass/rooms/bett.nix
@@ -5,4 +5,35 @@ with import ../lib.nix { inherit lib; };
   lass.hass.config = lib.mkMerge [
     (lightswitch switches.dimmer.bett lights.bett)
   ];
+
+  lass.hass.love = {
+    resources = [{
+      url = "https://raw.githubusercontent.com/ljmerza/light-entity-card/master/dist/light-entity-card.js.map";
+      type = "js";
+    }];
+    views = [{
+      title = "bett";
+      cards = [
+        {
+          type = "markdown";
+          title = "hello world";
+          content = "This is just a test";
+        }
+        {
+          type = "light";
+          entity = "light.${lights.bett}";
+        }
+        {
+          type = "custom:light-entity-card";
+          entity = "light.${lights.bett}";
+        }
+        {
+          type = "history-graph";
+          entities = [
+            "light.${lights.bett}"
+          ];
+        }
+      ];
+    }];
+  };
 }
