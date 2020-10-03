3c9945364c294ab65c62c01447d00ebc980558bc
tv <tv@krebsco.de>
2020-10-03 12:19:13 +0200

    tv ppp: add o2 config

--- tv/2configs/ppp.nix
+++ tv/2configs/ppp.nix
@@ -1,2 +1,34 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  lib = import <stockholm/lib>;
+  cfg = {
+    pin = "@${toString <secrets/o2.pin>}";
+    ttys.ppp = "/dev/ttyACM0";
+  };
+in {
+  environment.etc."ppp/peers/o2".text = /* sh */ ''
+    ${cfg.ttys.ppp}
+    921600
+    crtscts
+    defaultroute
+    holdoff 10
+    lock
+    maxfail 0
+    noauth
+    nodetach
+    noipdefault
+    passive
+    persist
+    usepeerdns
+    connect "${pkgs.ppp}/bin/chat ''${DEBUG+-v} -Ss -f ${pkgs.writeText "o2.chat" /* sh */ ''
+      ABORT "BUSY"
+      ABORT "NO CARRIER"
+      REPORT CONNECT
+      "*EMRDY: 1"
+      ATZ OK
+      AT+CFUN=1 OK
+      ${cfg.pin} TIMEOUT 2 ERROR-AT-OK
+      AT+CGDCONT=1,\042IP\042,\042internet\042 OK
+      ATDT*99***1# CONNECT
+    ''}"
+  '';
 }
