132549cded87755018df77b9c18e68d083cee4ca
tv <tv@krebsco.de>
2020-10-03 14:12:04 +0200

    tv override: customize input-fonts

--- tv/5pkgs/override/default.nix
+++ tv/5pkgs/override/default.nix
@@ -1,5 +1,15 @@
 with import <stockholm/lib>;
 self: super: {
+  input-fonts = super.input-fonts.overrideAttrs (old: rec {
+    src = self.fetchurl {
+      url = "http://xu.r/~tv/mirrors/input-fonts/Input-Font-2.zip";
+      sha256 = "1vvipqcflz4ximy7xpqy9idrdpq3a0c490hp5137r2dq03h865y0";
+    };
+    outputHash = null;
+    outputHashAlgo = null;
+    outputHashMode = null;
+  });
+
   rxvt_unicode = self.callPackage ./rxvt_unicode.nix {
     rxvt_unicode = super.rxvt_unicode;
   };
