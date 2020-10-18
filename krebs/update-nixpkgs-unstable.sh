0be43e62346062304e0cf30dbabdae173f718fcd
lassulus <lassulus@lassul.us>
2020-10-18 18:53:18 +0200

    update-nixpkgs: use nixpkgs remote

--- krebs/update-nixpkgs-unstable.sh
+++ krebs/update-nixpkgs-unstable.sh
@@ -2,7 +2,7 @@
 dir=$(dirname $0)
 oldrev=$(cat $dir/nixpkgs-unstable.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-shell -p nix-prefetch-git --run 'nix-prefetch-git \
-  --url https://github.com/NixOS/nixpkgs-channels \
+  --url https://github.com/NixOS/nixpkgs \
   --rev refs/heads/nixos-unstable' \
 > $dir/nixpkgs-unstable.json
 newrev=$(cat $dir/nixpkgs-unstable.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
--- krebs/update-nixpkgs.sh
+++ krebs/update-nixpkgs.sh
@@ -2,7 +2,7 @@
 dir=$(dirname $0)
 oldrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-shell -p nix-prefetch-git --run 'nix-prefetch-git \
-  --url https://github.com/NixOS/nixpkgs-channels \
+  --url https://github.com/NixOS/nixpkgs \
   --rev refs/heads/nixos-20.03' \
 > $dir/nixpkgs.json
 newrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
