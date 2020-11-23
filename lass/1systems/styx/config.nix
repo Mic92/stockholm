636d1eb762aae20e1da580d6f37460aa25391f45
lassulus <lassulus@lassul.us>
2020-11-23 11:32:23 +0100

    l: init styx.r

---
+++ lass/1systems/styx/config.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+with import <stockholm/lib>;
+{
+  imports = [
+    <stockholm/lass>
+
+    <stockholm/lass/2configs/mouse.nix>
+    <stockholm/lass/2configs/retiolum.nix>
+    <stockholm/lass/2configs/baseX.nix>
+    <stockholm/lass/2configs/exim-retiolum.nix>
+    <stockholm/lass/2configs/browsers.nix>
+    <stockholm/lass/2configs/programs.nix>
+    <stockholm/lass/2configs/nfs-dl.nix>
+    # <stockholm/lass/2configs/gg23.nix>
+    # <stockholm/lass/2configs/hass>
+    # <stockholm/lass/2configs/br.nix>
+    <stockholm/lass/2configs/fetchWallpaper.nix>
+    <stockholm/lass/2configs/home-media.nix>
+    # <stockholm/lass/2configs/syncthing.nix>
+    # <stockholm/lass/2configs/sync/sync.nix>
+    # <stockholm/lass/2configs/idc.nix>
+  ];
+
+  krebs.build.host = config.krebs.hosts.styx;
+
+  krebs.iptables.tables.filter.INPUT.rules = [
+    { predicate = "-p tcp --dport ${toString config.services.smokeping.port}"; target = "ACCEPT"; }
+  ];
+  services.smokeping = {
+    enable = true;
+    targetConfig = ''
+      probe = FPing
+      menu = top
+      title = top
+
+      + Local
+      menu = Local
+      title = Local Network
+      ++ LocalMachine
+      menu = Local Machine
+      title = This host
+      host = localhost
+
+      + Internet
+      menu = internet
+      title = internet
+
+      ++ CloudflareDNS
+      menu = Cloudflare DNS
+      title = Cloudflare DNS server
+      host = 1.1.1.1
+
+      ++ GoogleDNS
+      menu = Google DNS
+      title = Google DNS server
+      host = 8.8.8.8
+
+      + retiolum
+      menu = retiolum
+      title = retiolum
+
+      ++ gum
+      menu = gum.r
+      title = gum.r
+      host = gum.r
+
+      ++ ni
+      menu = ni.r
+      title = ni.r
+      host = ni.r
+
+      ++ prism
+      menu = prism.r
+      title = prism.r
+      host = prism.r
+    '';
+  };
+}
+
