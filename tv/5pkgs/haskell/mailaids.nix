5022ddc71b74c276974c07a4d1cb669ca0dfc35c
tv <tv@krebsco.de>
2020-11-24 20:19:05 +0100

    tv mailaids: init at 1.0.0

---
+++ tv/5pkgs/haskell/mailaids.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, aeson-pretty, base, bytestring
+, case-insensitive, fetchgit, lens, optparse-applicative
+, purebred-email, stdenv, text, vector, word8
+}:
+mkDerivation {
+  pname = "mailaids";
+  version = "1.0.0";
+  src = fetchgit {
+    url = "https://cgit.krebsco.de/mailaids";
+    sha256 = "15h0k82czm89gkwhp1rwdy77jz8dmb626qdz7c2narvz9j7169v5";
+    rev = "8f11927ea74d6adb332c884502ebd9c486837523";
+    fetchSubmodules = true;
+  };
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson aeson-pretty base bytestring case-insensitive lens
+    optparse-applicative purebred-email text vector word8
+  ];
+  license = stdenv.lib.licenses.mit;
+}
