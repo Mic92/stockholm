70bfe7e697c99b9f0f70d142a58aeea9143e9de3
lassulus <lassulus@lassul.us>
2020-11-26 20:42:20 +0100

    l green.r: use shallow clone

--- lass/1systems/green/source.nix
+++ lass/1systems/green/source.nix
@@ -1,14 +1,5 @@
 { lib, pkgs, ... }:
 {
-  nixpkgs = lib.mkForce {
-    file = {
-      path = toString (pkgs.fetchFromGitHub {
-        owner = "nixos";
-        repo = "nixpkgs";
-        rev = (lib.importJSON ../../../krebs/nixpkgs.json).rev;
-        sha256 = (lib.importJSON ../../../krebs/nixpkgs.json).sha256;
-      });
-      useChecksum = true;
-    };
-  };
+  nixpkgs-unstable = lib.mkForce { file = "/var/empty"; };
+  nixpkgs.git.shallow = true;
 }
