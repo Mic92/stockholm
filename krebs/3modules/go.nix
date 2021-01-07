f3f6778c035f11b361beefc23cbb4250a94be7fb
lassulus <lassulus@lassul.us>
2021-01-08 00:38:34 +0100

    go: implement with htgen

--- krebs/3modules/go.nix
+++ krebs/3modules/go.nix
@@ -13,52 +13,78 @@ let
   api = {
     enable = mkEnableOption "Enable go url shortener";
     port = mkOption {
-      type = types.str;
-      default = "1337";
+      type = types.int;
+      default = 1337;
       description = "on which port go should run on";
-    };
-    redisKeyPrefix = mkOption {
-      type = types.str;
-      default = "go:";
-      description = "change the Redis key prefix which defaults to `go:`";
     };
   };
 
   imp = {
-    services.redis = {
-      enable = mkDefault true;
-      bind = mkDefault "127.0.0.1";
-    };
+    krebs.htgen.go = {
+      port = cfg.port;
+      script = ''. ${pkgs.writeDash "go" ''
+        find_item() {
+          if test ''${#1} -ge 7; then
+            set -- "$(find "$STATEDIR/items" -mindepth 1 -maxdepth 1 \
+                -regex "$STATEDIR/items/$1[0-9A-Za-z]*$")"
+            if test -n "$1" && test $(echo "$1" | wc -l) = 1; then
+              echo "$1"
+              return 0
+            fi
+          fi
+          return 1
+        }
 
-    users.extraUsers.go = rec {
-      name = "go";
-      uid = genid name;
-      description = "go url shortener user";
-      home = "/var/lib/go";
-      createHome = true;
-    };
+        STATEDIR=$HOME
+        mkdir -p "$STATEDIR/items"
 
-    systemd.services.go = {
-      description = "go url shortener";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
+        case "$Method $Request_URI" in
+          "GET /"*)
+            if item=$(find_item "''${Request_URI#/}"); then
+              uri=$(cat "$item")
+              printf 'HTTP/1.1 302 Found\r\n'
+              printf 'Content-Type: text/plain\r\n'
+              printf 'Connection: closed\r\n'
+              printf 'Location: %s\r\n' "$uri"
+              printf '\r\n'
+              exit
+            fi
+          ;;
+          "POST /")
+            uri=$(mktemp -t htgen.$$.content.XXXXXXXX)
+            trap 'rm $uri >&2' EXIT
 
-      path = with pkgs; [
-        go-shortener
-      ];
+            head -c "$req_content_length" \
+              | sed 's/+/ /g;s/%\(..\)/\\x\1/g;' \
+              | xargs -0 echo -e \
+              | tee /tmp/tee.log \
+              | ${pkgs.urix}/bin/urix \
+              | head -1 \
+              > "$uri"
+            sha256=$(sha256sum -b "$uri" | cut -d\  -f1)
+            base32=$(${pkgs.nixStable}/bin/nix-hash --to-base32 --type sha256 "$sha256")
+            item="$STATEDIR/items/$base32"
+            ref="http://$req_host/$base32"
 
-      environment = {
-        PORT = cfg.port;
-        REDIS_KEY_PREFIX = cfg.redisKeyPrefix;
-      };
+            if ! test -e "$item"; then
+              mkdir -v -p "$STATEDIR/items" >&2
+              cp -v "$uri" "$item" >&2
+            fi
 
-      restartIfChanged = true;
+            base32short=$(echo "$base32" | cut -b-7)
+            if item=$(find_item "$base32short"); then
+              ref="http://$req_host/$base32short"
+            fi
 
-      serviceConfig = {
-        User = "go";
-        Restart = "always";
-        ExecStart = "${pkgs.go-shortener}/bin/go";
-      };
+            printf 'HTTP/1.1 200 OK\r\n'
+            printf 'Content-Type: text/plain; charset=UTF-8\r\n'
+            printf 'Connection: close\r\n'
+            printf '\r\n'
+            printf '%s\n' "$ref"
+            exit
+          ;;
+        esac
+      ''}'';
     };
   };
 
