238d9cd5006572b6f923ace5acbdcd7179239b15
lassulus <lassulus@lassul.us>
2021-01-12 22:38:52 +0100

    brockman: 2.2.0 -> 3.0.0

--- krebs/5pkgs/haskell/brockman.nix
+++ krebs/5pkgs/haskell/brockman.nix
@@ -1,24 +1,24 @@
 { mkDerivation, aeson, aeson-pretty, base, bloomfilter, bytestring
 , conduit, containers, directory, feed, filepath, hslogger
-, http-client, irc-conduit, lens, network, optparse-applicative
-, random, safe, stdenv, text, wreq
+, html-entity, http-client, irc-conduit, lens, network
+, optparse-applicative, random, safe, stdenv, text, wreq
 , fetchFromGitHub
 }:
 mkDerivation rec {
   pname = "brockman";
-  version = "2.2.0";
+  version = "3.0.0";
   src = fetchFromGitHub {
     owner = "kmein";
     repo = "brockman";
     rev = version;
-    sha256 = "003crqcqlgai7vwvhvfa7lr5ain8xzs7dm63ksm85mq58cwpsspx";
+    sha256 = "08yla9q2mjd7znpasfwsdqzc3dp2vcvg53x9p4vlx4g7jr3dw3yp";
   };
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson aeson-pretty base bloomfilter bytestring conduit containers
-    directory feed filepath hslogger http-client irc-conduit lens
-    network optparse-applicative random safe text wreq
+    directory feed filepath hslogger html-entity http-client
+    irc-conduit lens network optparse-applicative random safe text wreq
   ];
   license = stdenv.lib.licenses.mit;
 }
