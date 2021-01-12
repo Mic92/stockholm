b906bf0b02b727c583bead47d745d7ece96ea804
lassulus <lassulus@lassul.us>
2021-01-12 09:40:55 +0100

    brockman: 2.1.0 -> 2.2.0

--- krebs/5pkgs/haskell/brockman.nix
+++ krebs/5pkgs/haskell/brockman.nix
@@ -6,12 +6,12 @@
 }:
 mkDerivation rec {
   pname = "brockman";
-  version = "2.1.0";
+  version = "2.2.0";
   src = fetchFromGitHub {
     owner = "kmein";
     repo = "brockman";
     rev = version;
-    sha256 = "1wcv2rmmmnnz6gi3g9l2brqc46wm87byzyrixcnlnx3pj5g4d3zb";
+    sha256 = "003crqcqlgai7vwvhvfa7lr5ain8xzs7dm63ksm85mq58cwpsspx";
   };
   isLibrary = false;
   isExecutable = true;
