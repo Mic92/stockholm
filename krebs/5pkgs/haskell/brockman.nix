5c669397dac74d4c63281a7c785465569e93643e
lassulus <lassulus@lassul.us>
2021-01-23 14:11:16 +0100

    brockman: 3.2.0 -> 3.2.3

--- krebs/5pkgs/haskell/brockman.nix
+++ krebs/5pkgs/haskell/brockman.nix
@@ -1,26 +1,26 @@
 { mkDerivation, aeson, aeson-pretty, base, bloomfilter, bytestring
-, conduit, containers, directory, feed, filepath, hslogger
-, html-entity, http-client, irc-conduit, lens, network
+, case-insensitive, conduit, containers, directory, feed, filepath
+, hslogger, html-entity, http-client, irc-conduit, lens, network
 , optparse-applicative, random, safe, stdenv, text, time, timerep
 , wreq
 , fetchFromGitHub
 }:
 mkDerivation rec {
   pname = "brockman";
-  version = "3.2.0";
+  version = "3.2.3";
   src = fetchFromGitHub {
     owner = "kmein";
     repo = "brockman";
     rev = version;
-    sha256 = "0vvps5czl6qcpfyrm2a6vj00hdh941wj4zb2bd9jlgf9mfikqm77";
+    sha256 = "1qbjbf0l1ikfzmvky4cnvv7nlcwi2in4afliifh618j0a4f7j427";
   };
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    aeson aeson-pretty base bloomfilter bytestring conduit containers
-    directory feed filepath hslogger html-entity http-client
-    irc-conduit lens network optparse-applicative random safe text time
-    timerep wreq
+    aeson aeson-pretty base bloomfilter bytestring case-insensitive
+    conduit containers directory feed filepath hslogger html-entity
+    http-client irc-conduit lens network optparse-applicative random
+    safe text time timerep wreq
   ];
   license = stdenv.lib.licenses.mit;
 }
