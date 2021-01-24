8e5eb283f4f06a87ab228ad2a0d3a9a6e3ffd737
lassulus <lassulus@lassul.us>
2021-01-24 14:47:50 +0100

    l syncthing: just configure used peers

--- lass/2configs/syncthing.nix
+++ lass/2configs/syncthing.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }: with import <stockholm/lib>; let
-  all_peers = filterAttrs (n: v: v.syncthing.id != null) config.krebs.hosts;
   mk_peers = mapAttrs (n: v: { id = v.syncthing.id; });
+
+  all_peers = filterAttrs (n: v: v.syncthing.id != null) config.krebs.hosts;
+  used_peer_names = unique (flatten (mapAttrsToList (n: v: v.devices) config.services.syncthing.declarative.folders));
+  used_peers = filterAttrs (n: v: elem n used_peer_names) all_peers;
 in {
   services.syncthing = {
     enable = true;
@@ -9,7 +12,7 @@ in {
     declarative = {
       key = toString <secrets/syncthing.key>;
       cert = toString <secrets/syncthing.cert>;
-      devices = mk_peers all_peers;
+      devices = mk_peers used_peers;
     };
   };
   krebs.iptables.tables.filter.INPUT.rules = [
