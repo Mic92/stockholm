6b68a4f5d8b61e91585b422a7b42f98ac6ec007f
tv <tv@krebsco.de>
2021-01-24 01:24:19 +0100

    tv cr: use XDG_RUNTIME_DIR

--- tv/5pkgs/default.nix
+++ tv/5pkgs/default.nix
@@ -13,13 +13,17 @@ foldl' mergeAttrs {}
 //
 
 {
-  # TODO use XDG_RUNTIME_DIR?
   cr = self.writeDashBin "cr" ''
     set -efu
+    if test -n "''${XDG_RUNTIME_DIR-}"; then
+      cache_dir=$XDG_RUNTIME_DIR/chromium-disk-cache
+    else
+      cache_dir=/tmp/chromium-disk-cache_$LOGNAME
+    fi
     export LC_TIME=de_DE.utf8
     exec ${self.chromium}/bin/chromium \
         --ssl-version-min=tls1 \
-        --disk-cache-dir=/tmp/chromium-disk-cache_"$LOGNAME" \
+        --disk-cache-dir="$cache_dir" \
         --disk-cache-size=50000000 \
         "$@"
   '';
