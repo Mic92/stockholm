ce0393f43aab8928c783276ac6d14e92f754391b
lassulus <lassulus@lassul.us>
2021-02-10 15:58:59 +0100

    brockman: 3.2.4 -> 3.4.0

--- krebs/5pkgs/haskell/brockman/default.nix
+++ krebs/5pkgs/haskell/brockman/default.nix
@@ -1,26 +1,26 @@
 { mkDerivation, aeson, aeson-pretty, base, bytestring
 , case-insensitive, conduit, containers, directory, feed, filepath
-, hslogger, html-entity, http-client, irc-conduit, lens, lrucache
-, network, optparse-applicative, random, safe, stdenv, text, time
-, timerep, wreq
+, hashable, hslogger, html-entity, http-client, irc-conduit, lens
+, lrucache, lrucaching, network, optparse-applicative, random, safe
+, stdenv, text, time, timerep, wreq
 , fetchFromGitHub
 }:
 mkDerivation rec {
   pname = "brockman";
-  version = "3.2.4";
+  version = "3.4.0";
   src = fetchFromGitHub {
     owner = "kmein";
     repo = "brockman";
     rev = version;
-    sha256 = "1jh2i3rxbw8x0p5xs9ph95ixpsa6h6qm0msjb9xqnw9j8by2fkk2";
+    sha256 = "02nval6a9xcddj6znzxvcb8g6klzjydj1lb4ych64i9mr4a8jvic";
   };
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson aeson-pretty base bytestring case-insensitive conduit
-    containers directory feed filepath hslogger html-entity http-client
-    irc-conduit lens lrucache network optparse-applicative random safe
-    text time timerep wreq
+    containers directory feed filepath hashable hslogger html-entity
+    http-client irc-conduit lens lrucache lrucaching network
+    optparse-applicative random safe text time timerep wreq
   ];
   license = stdenv.lib.licenses.mit;
 }
