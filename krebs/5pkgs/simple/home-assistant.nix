2c59f04f5f15d36dbc826cfc7581f5cea1f5d0d1
lassulus <lassulus@lassul.us>
2021-02-11 17:13:04 +0100

    remove broken hass override

--- krebs/5pkgs/simple/home-assistant.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, lib, ... }: let
-  unstable = pkgs.fetchFromGitHub {
-    owner = "nixos";
-    repo = "nixpkgs";
-    rev = (lib.importJSON ../../nixpkgs-unstable.json).rev;
-    sha256 = (lib.importJSON ../../nixpkgs-unstable.json).sha256;
-  };
-in (import unstable {}).home-assistant
