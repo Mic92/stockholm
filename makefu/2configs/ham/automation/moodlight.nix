dc7dca887a082666b3e4c7592239131181bc42c1
makefu <github@syntax-fehler.de>
2021-02-12 13:08:45 +0100

    ma ham/moodlight: turn on cubes in blink mode

--- makefu/2configs/ham/automation/moodlight.nix
+++ makefu/2configs/ham/automation/moodlight.nix
@@ -5,14 +5,19 @@ let
   arbeitszimmer = "light.box_led_status";
   final_off = "01:00";
 
-  turn_on = entity_id: at:
+  turn_on = entity_id: at: extra:
   { alias = "Turn on ${entity_id} at ${at}";
     trigger = [
       { platform = "time"; inherit at; }
     ];
     action =
     [
-      { service = "light.turn_on"; inherit entity_id; }
+      ({ service = "light.turn_on"; 
+        data = {
+          inherit entity_id; 
+
+        } // extra;
+        })
     ];
   };
 in
@@ -21,8 +26,8 @@ in
   {
     automation =
     [
-      (turn_on wohnzimmer "17:30")
-      (turn_on arbeitszimmer "9:00")
+      # (turn_on wohnzimmer "17:30")
+      (turn_on arbeitszimmer "9:00" { effect = "Slow Random Twinkle";})
 
       { alias = "Always turn off the lights at ${final_off}";
         trigger = [
