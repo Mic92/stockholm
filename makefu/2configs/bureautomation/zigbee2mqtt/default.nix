1da12adf038e962af76228b3e53d2eb8c0d367b2
makefu <github@syntax-fehler.de>
2021-03-12 20:32:34 +0100

    ma bureautomation/zigbee2mqtt: init

---
+++ makefu/2configs/bureautomation/zigbee2mqtt/default.nix
@@ -0,0 +1,33 @@
+{config, pkgs, lib, ...}:
+
+let
+  dataDir = "/var/lib/zigbee2mqtt";
+in
+  {
+  # symlink the zigbee controller
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="0451", ATTRS{idProduct}=="16a8", SYMLINK+="cc2531", MODE="0660", GROUP="dialout"
+  '';
+
+  services.zigbee2mqtt = {
+    enable = true;
+    inherit dataDir;
+    config = {
+      permit_join = true;
+      serial.port = "/dev/cc2531";
+      homeassistant = true;
+    };
+  };
+
+  state = [ "${dataDir}/devices.yaml" "${dataDir}/state.json" ];
+
+  systemd.services.zigbee2mqtt = {
+    # override automatic configuration.yaml deployment
+    environment.ZIGBEE2MQTT_DATA = dataDir;
+    after = [
+      "home-assistant.service"
+      "mosquitto.service"
+      "network-online.target"
+    ];
+  };
+}
