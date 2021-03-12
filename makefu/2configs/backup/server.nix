ca37236a537b980066205a6d2f5309fa3eb8e0e0
makefu <github@syntax-fehler.de>
2021-03-12 20:13:23 +0100

    ma backup/server: init backup server repo option

--- makefu/2configs/backup/server.nix
+++ makefu/2configs/backup/server.nix
@@ -1,11 +1,19 @@
-{lib, ... }:
+{lib,config, ... }:
 let
   hosts = lib.mapAttrsToList (f: _: lib.removeSuffix ".pub" f) (builtins.readDir ./ssh );
 in {
   # TODO: for all enabled machines
-  services.borgbackup.repos = lib.genAttrs hosts (host: {
-    authorizedKeys = [ (builtins.readFile (./ssh + "/${host}.pub") ) ];
-    path = "/var/lib/borgbackup/${host}";
-    user = "borg-${host}";
-  }) ;
+  options = {
+    makefu.backup.server.repo = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/borgbackup";
+    };
+  };
+  config = {
+    services.borgbackup.repos = lib.genAttrs hosts (host: {
+      authorizedKeys = [ (builtins.readFile (./ssh + "/${host}.pub") ) ];
+      path = "${config.makefu.backup.server.repo}/${host}";
+      user = "borg-${host}";
+    }) ;
+  };
 }
