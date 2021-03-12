f20695084ed681adecb9fbfa459ffb46591812f2
makefu <github@syntax-fehler.de>
2021-03-12 20:12:04 +0100

    ma tsp.r: refactor

--- makefu/1systems/tsp/config.nix
+++ makefu/1systems/tsp/config.nix
@@ -6,21 +6,26 @@
 {
   imports =
     [ # Include the results of the hardware scan.
+      ./hardware.nix
       <stockholm/makefu>
+      <stockholm/makefu/2configs/nur.nix>
       <stockholm/makefu/2configs/home-manager>
       <stockholm/makefu/2configs/main-laptop.nix>
+      <stockholm/makefu/2configs/editor/neovim>
+      <stockholm/makefu/2configs/tools/core.nix>
       # <stockholm/makefu/2configs/tools/all.nix>
       <stockholm/makefu/2configs/fs/single-partition-ext4.nix>
       # hardware specifics are in here
-      # imports tp-x2x0.nix
-      <stockholm/makefu/2configs/hw/tp-x230.nix>
       <stockholm/makefu/2configs/hw/bluetooth.nix>
       <stockholm/makefu/2configs/hw/network-manager.nix>
+
 
       # <stockholm/makefu/2configs/rad1o.nix>
 
       <stockholm/makefu/2configs/zsh-user.nix>
-      <stockholm/makefu/2configs/exim-retiolum.nix>
+      <stockholm/makefu/2configs/home-manager>
+      <stockholm/makefu/2configs/home-manager/desktop.nix>
+      <stockholm/makefu/2configs/home-manager/cli.nix>
       <stockholm/makefu/2configs/tinc/retiolum.nix>
 
       <stockholm/makefu/2configs/sshd-totp.nix>
