fe393ec4e753f2b84439eb719613a9fa79ec0c15
makefu <github@syntax-fehler.de>
2021-03-12 20:34:01 +0100

    puyak.r: rename interface, use correct subnet

--- krebs/1systems/puyak/net.nix
+++ krebs/1systems/puyak/net.nix
@@ -1,11 +1,11 @@
 let
-  ext-if = "enp0s25";
+  ext-if = "et0";
   shack-ip = "10.42.22.184";
   shack-gw = "10.42.20.1";
 in {
   services.udev.extraRules = ''
     SUBSYSTEM=="net", ATTR{address}=="8c:70:5a:b2:84:58", NAME="wl0"
-    SUBSYSTEM=="net", ATTR{address}=="3c:97:0e:07:b9:14", NAME="et0"
+    SUBSYSTEM=="net", ATTR{address}=="3c:97:0e:07:b9:14", NAME="${ext-if}"
   '';
   networking = {
     firewall.enable = false;
@@ -13,7 +13,7 @@ in {
     interfaces."${ext-if}".ipv4.addresses = [
       {
         address = shack-ip;
-        prefixLength = 20;
+        prefixLength = 22;
       }
     ];
 
