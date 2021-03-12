72019707d4c09e0066dd0b07e589a1bb42c64e2e
makefu <github@syntax-fehler.de>
2021-03-12 20:30:35 +0100

    ma systemdultras/ircbot: disable notifications

--- makefu/2configs/systemdultras/ircbot.nix
+++ makefu/2configs/systemdultras/ircbot.nix
@@ -1,7 +1,9 @@
 {
+  systemd.services.brockman.environment."BROCKMAN_LOG_LEVEL" = "DEBUG";
   krebs.brockman = {
     enable = true;
     config = {
+      channel = "#systemdultras";
       irc = {
         host = "irc.freenode.net";
         port = 6667;
@@ -11,11 +13,13 @@
           feed = "https://www.reddit.com/r/systemdultras/.rss";
           delay = 136;
           channels = [ "#systemdultras" ];
+          notifyErrors = false;
         };
         r-systemd-rss = {
           feed = "https://www.reddit.com/r/systemd/.rss";
           delay = 172;
           channels = [ "#systemdultras" ];
+          notifyErrors = false;
         };
       };
     };
