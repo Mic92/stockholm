fd3dff19ed4952a6a92cb3ebf40e52c7ba81d563
lassulus <lassulus@lassul.us>
2021-05-02 17:48:27 +0200

    l fetchWallpaper: use stars version

--- lass/2configs/fetchWallpaper.nix
+++ lass/2configs/fetchWallpaper.nix
@@ -5,8 +5,7 @@ let
 in {
   krebs.fetchWallpaper = {
     enable = true;
-    unitConfig.ConditionPathExists = "!/var/run/ppp0.pid";
-    url = "prism/realwallpaper-krebs.png";
+    url = "prism/realwallpaper-krebs-stars.png";
   };
 }
 
