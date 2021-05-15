41e87ff78857eb209b9598ee9aee8c6fa73a1af0
makefu <github@syntax-fehler.de>
2021-05-15 13:17:09 +0200

    ma printer: update

--- makefu/2configs/printer.nix
+++ makefu/2configs/printer.nix
@@ -21,20 +21,26 @@ in {
   hardware.sane = {
     enable = true;
     extraBackends = [ ];
-    netConf =
-      # drucker.lan SCX-3205W
-      ''
-        192.168.111.16''
-      # uhrenkind.shack magicolor 1690mf
-    + ''
-        10.42.20.30'';
+    extraConfig.xerox_mfp = ''
+      usb 0x04e8 0x3441
+    '';
+    #netConf =
+    #  # drucker.lan SCX-3205W
+    #  ''
+    #    192.168.111.16''
+    #  # uhrenkind.shack magicolor 1690mf
+    #+ ''
+    #    10.42.20.30'';
 
-    # $ scanimage -p --format=jpg --mode=Gray --source="Automatic Document Feeder" -v --batch="lol%d.jpg" --resolution=150
+    ## $ scanimage -p --format=jpg --mode=Gray --source="Automatic Document Feeder" -v --batch="lol%d.jpg" --resolution=150
 
-    # requires 'sane-extra', scan via:
-    extraConfig."magicolor" = ''
-      net 10.42.20.30 0x2098
-    ''; # 10.42.20.30: uhrenkind.shack magicolor 1690mf
+    ## requires 'sane-extra', scan via:
+    #extraConfig."magicolor" = ''
+    #  net 10.42.20.30 0x2098
+    #''; # 10.42.20.30: uhrenkind.shack magicolor 1690mf
   };
   state = [ "/var/lib/cups" ];
+  services.udev.extraRules = ''
+    ATTRS{idVendor}=="04e8", ATTRS{idProduct}=="3441", ENV{libsane_matched}="yes"
+  '';
 }
