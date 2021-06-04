42f704a4d9dbe57fa3f5417a4c1d1b6f019b5480
tv <tv@krebsco.de>
2021-06-05 00:46:13 +0200

    tv utsushi-customized: use upstream package

--- tv/2configs/xp-332.nix
+++ tv/2configs/xp-332.nix
@@ -19,17 +19,14 @@ with import <stockholm/lib>;
     packageName pkg == "imagescan-plugin-networkscan";
 
   nixpkgs.overlays = singleton (self: super: {
-    utsushi-customized = self.utsushi.override {
-      guiSupport = false;
-      jpegSupport = false;
-      networkSupport = true;
-      ocrSupport = false;
-      saneSupport = true;
-      tiffSupport = true;
-
-      logCategory = "ALL";
-      logLevel = "BRIEF";
-    };
+    utsushi-customized = self.utsushi.overrideAttrs (old: {
+      postInstall = ''
+        ${old.postInstall or ""}
+        ln -s /etc/utsushi.conf $out/etc/utsushi/utsushi.conf
+        ln -s ${pkgs.imagescan-plugin-networkscan}/lib/utsushi/networkscan \
+          $out/libexec/utsushi/
+      '';
+    });
   });
 
   services = {
