1ac0608fc53214897703e4a163186a4586c73bb6
lassulus <lassulus@lassul.us>
2021-08-31 19:26:37 +0200

    cyberlocker-tools: pass fail

--- krebs/5pkgs/simple/cyberlocker-tools/default.nix
+++ krebs/5pkgs/simple/cyberlocker-tools/default.nix
@@ -6,14 +6,14 @@ pkgs.symlinkJoin {
       set -efu
       path=''${1:-$(hostname)}
 
-      ${pkgs.curl}/bin/curl -Ss --data-binary @- "http://c.r/$path"
+      ${pkgs.curl}/bin/curl -fSs --data-binary @- "http://c.r/$path"
       echo "http://c.r/$path"
     '')
     (pkgs.writers.writeDashBin "cdel" ''
       set -efu
       path=$1
 
-      ${pkgs.curl}/bin/curl -X DELETE "http://c.r/$path"
+      ${pkgs.curl}/bin/curl -f -X DELETE "http://c.r/$path"
     '')
   ];
 }
