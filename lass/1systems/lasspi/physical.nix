b8cd625a70fdd8811b8c5bfd0abf17a00c2e628c
lassulus <lassulus@lassul.us>
2021-09-10 09:23:02 +0200

    l: add lasspi & domsen-pixel hosts

---
+++ lass/1systems/lasspi/physical.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, ... }:
+{
+ # This configuration worked on 09-03-2021 nixos-unstable @ commit 102eb68ceec
+ # The image used https://hydra.nixos.org/build/134720986
+  imports = [
+    ./config.nix
+  ];
+
+  boot = {
+    # kernelPackages = pkgs.linuxPackages_rpi4;
+    tmpOnTmpfs = true;
+    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    # ttyAMA0 is the serial console broken out to the GPIO
+    kernelParams = [
+        "8250.nr_uarts=1"
+        "console=ttyAMA0,115200"
+        "console=tty1"
+        # Some gui programs need this
+        "cma=128M"
+    ];
+  };
+
+  boot.loader.raspberryPi = {
+    enable = true;
+    version = 4;
+  };
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  # Required for the Wireless firmware
+  hardware.enableRedistributableFirmware = true;
+
+  # Assuming this is installed on top of the disk image.
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  powerManagement.cpuFreqGovernor = "ondemand";
+}
