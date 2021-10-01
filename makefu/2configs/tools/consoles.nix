9e27690932169e895a00355398dd36f4241df37a
makefu <github@syntax-fehler.de>
2021-10-01 18:53:52 +0200

    ma tools/consoles: add opl-pc-tools

--- makefu/2configs/tools/consoles.nix
+++ makefu/2configs/tools/consoles.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 {
   users.users.makefu.packages = with pkgs; [
+    # PS2
     opl-utils
+    #opl-pc-tools
     hdl-dump
     bin2iso
     cue2pops
+
+    # switch
     nx_game_info
     hactool
     nsrenamer
