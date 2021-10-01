eacb221f243d48ccbdea3038f78ec60b3392ee9e
makefu <github@syntax-fehler.de>
2021-10-01 18:41:50 +0200

    ma rss.euer: remove workaround for tt-rss

--- makefu/2configs/deployment/rss.euer.krebsco.de.nix
+++ makefu/2configs/deployment/rss.euer.krebsco.de.nix
@@ -8,11 +8,6 @@ in {
     selfUrlPath = "https://${fqdn}";
   };
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "python2.7-Pillow-6.2.2"
-  ];
-
-  systemd.services.tt-rss.serviceConfig.ExecStart = lib.mkForce "${pkgs.php}/bin/php /var/lib/tt-rss/update_daemon2.php";
   services.postgresql.package = pkgs.postgresql_9_6;
   state = [ config.services.postgresqlBackup.location ];
 
