4d28255e28e5bfe6af25fb252a0bba9342730609
makefu <github@syntax-fehler.de>
2021-10-01 18:29:43 +0200

    ma gum.r: disable wa for now

--- makefu/1systems/gum/config.nix
+++ makefu/1systems/gum/config.nix
@@ -160,7 +160,7 @@ in {
       # <stockholm/makefu/2configs/deployment/systemdultras-rss.nix>
 
       <stockholm/makefu/2configs/shiori.nix>
-      <stockholm/makefu/2configs/workadventure>
+      # <stockholm/makefu/2configs/workadventure>
 
       <stockholm/makefu/2configs/bgt/download.binaergewitter.de.nix>
       <stockholm/makefu/2configs/bgt/hidden_service.nix>
