51eaeac0bc3ec8543dbb24fb3398a906fa8b22b3
makefu <github@syntax-fehler.de>
2021-10-01 18:53:26 +0200

    ma systemdultras/ircbot: for hackint, use tls

--- makefu/2configs/systemdultras/ircbot.nix
+++ makefu/2configs/systemdultras/ircbot.nix
@@ -1,18 +1,27 @@
 { pkgs, ... }: {
   systemd.services.brockman.environment."BROCKMAN_LOG_LEVEL" = "DEBUG";
+
+  services.rss-bridge = {
+    enable = true;
+    whitelist = [ "*" ];
+    virtualHost = "rss.makefu.r";
+  };
+
   systemd.services.restart-brockman = {
     after = [ "brockman.service" ];
     wantedBy = [ "multi-user.target" ];
     startAt = "daily";
     script = "${pkgs.systemd}/bin/systemctl try-restart brockman.service";
   };
+
   krebs.brockman = {
     enable = true;
     config = {
       channel = "#systemdultras";
       irc = {
         host = "irc.hackint.org";
-        port = 6667;
+        port = 6697;
+        tls = true;
       };
       notifyErrors = false;
       bots = {
@@ -23,6 +32,10 @@
         r-systemd-rss = {
           feed = "https://www.reddit.com/r/systemd/.rss";
           delay = 172;
+        };
+        r-pid_eins-twitter = {
+          feed = "http://rss.makefu.r/?action=display&bridge=Twitter&context=By+username&u=pid_eins&format=Atom";
+          delay = 621;
         };
       };
     };
