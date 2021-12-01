44542b6914a9cf6c1a3dc9326fbd1048a3ce0831
lassulus <lassulus@lassul.us>
2021-12-01 10:56:11 +0100

    nixpkgs: 21.05 -> 21.11

--- krebs/update-nixpkgs.sh
+++ krebs/update-nixpkgs.sh
@@ -3,7 +3,7 @@ dir=$(dirname $0)
 oldrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 nix-shell -p nix-prefetch-git --run 'nix-prefetch-git \
   --url https://github.com/NixOS/nixpkgs \
-  --rev refs/heads/nixos-21.05' \
+  --rev refs/heads/nixos-21.11' \
 > $dir/nixpkgs.json
 newrev=$(cat $dir/nixpkgs.json | jq -r .rev | sed 's/\(.\{7\}\).*/\1/')
 git commit $dir/nixpkgs.json -m "nixpkgs: $oldrev -> $newrev"
