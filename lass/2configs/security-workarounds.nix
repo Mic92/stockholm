b749315dc7db653d1f077e775eab28d206a029a4
lassulus <lassulus@lassul.us>
2022-01-26 12:17:04 +0100

    l: workaround for CVE-2021-4034

--- lass/2configs/security-workarounds.nix
+++ lass/2configs/security-workarounds.nix
@@ -1,8 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 with import <stockholm/lib>;
 {
   # http://seclists.org/oss-sec/2017/q1/471
   boot.extraModprobeConfig = ''
     install dccp /run/current-system/sw/bin/false
   '';
+
+  security.wrappers.pkexec.source = lib.mkForce (pkgs.writeText "pkexec" "");
 }
