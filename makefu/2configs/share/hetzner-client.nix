94b29daa137e704d9db1979169ba82f9150515c6
makefu <github@syntax-fehler.de>
2022-03-01 23:26:31 +0100

    ma share: set file-mode to 0770 for hetzner

--- makefu/2configs/share/hetzner-client.nix
+++ makefu/2configs/share/hetzner-client.nix
@@ -1,5 +1,6 @@
 { config, lib, pkgs, ... }:
 
+with <stockholm/lib>;
 let
   automount_opts =
   [ "x-systemd.automount"
@@ -10,14 +11,16 @@ let
   host = "u288834.your-storagebox.de";
 in {
   boot.kernel.sysctl."net.ipv6.route.max_size" = 2147483647;
+
   fileSystems."/media/cloud" = {
       device = "//${host}/backup";
       fsType = "cifs";
       options = automount_opts ++
-      [ "credentials=/var/src/secrets/hetzner.smb"
-        "file_mode=0775"
-        "dir_mode=0775"
-        "uid=9001"
+      [ "credentials=${toString <secrets/hetzner.smb>}"
+        "file_mode=0770"
+        "dir_mode=0770"
+        "uid=${toString config.users.users.download.uid}"
+        "gid=${toString config.users.groups.download.gid}"
         #"vers=3"
         "vers=2.1"
         "rsize=65536"
@@ -25,5 +28,4 @@ in {
         "iocharset=utf8"
       ];
   };
-
 }
