ad6e957a51d0e7f3e4090ed3eadc910e8ce75695
tv <tv@krebsco.de>
2022-03-05 09:04:34 +0100

    tv pkgs: move input-font to override

--- tv/5pkgs/override/default.nix
+++ tv/5pkgs/override/default.nix
@@ -1,17 +1,4 @@
 with import <stockholm/lib>;
-self: super: {
-  input-fonts = super.input-fonts.overrideAttrs (old: rec {
-    src = self.fetchzip {
-      url = "http://xu.r/~tv/mirrors/input-fonts/Input-Font-2.zip";
-      sha256 = "1q58x92nm7dk9ylp09pvgj74nxkywvqny3xmfighnsl30dv42fcr";
-      stripRoot = false;
-    };
-    sourceRoot = null;
-    outputHash = null;
-    outputHashAlgo = null;
-    outputHashMode = null;
-  });
+self: super:
 
-}
-//
 mapNixDir (path: import path self super) ./.
