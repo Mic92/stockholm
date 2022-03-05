b7f21925de63a03eec47aa42e206d0a5cdb82f2d
tv <tv@krebsco.de>
2022-03-05 09:00:16 +0100

    tv pkgs: move fzf to override

--- tv/5pkgs/override/default.nix
+++ tv/5pkgs/override/default.nix
@@ -1,12 +1,5 @@
 with import <stockholm/lib>;
 self: super: {
-  fzf = super.fzf.overrideAttrs (old: {
-    # XXX cannot use `patches` because fzf has a custom patchPhase
-    patchPhase = ''
-      patch -Np1 < ${./fzf.complete1.patch}
-      ${old.patchPhase or ""}
-    '';
-  });
   input-fonts = super.input-fonts.overrideAttrs (old: rec {
     src = self.fetchzip {
       url = "http://xu.r/~tv/mirrors/input-fonts/Input-Font-2.zip";
