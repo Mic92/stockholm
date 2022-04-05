1407ad3502a7dd46d6e432b7ccc013bc80341bb1
tv <tv@krebsco.de>
2022-04-05 22:16:29 +0200

    pager: init at 1.0.0

---
+++ krebs/5pkgs/simple/pager.nix
@@ -0,0 +1,34 @@
+{ pkgs }:
+
+pkgs.writeDashBin "pager" ''
+  # usage: pager {view,shift,shiftview}
+  #
+  # Environment variables
+  #
+  #   PAGER_NAME (default: Pager)
+  #     The environment variables specifies the application name under which
+  #     resources are to be obtained.  PAGER_NAME should not contain “.” or “*”
+  #     characters.
+  #
+  set -efu
+
+  pidfile=$XDG_RUNTIME_DIR/pager.lock
+  name=''${PAGER_NAME-Pager}
+
+  if test -e "$pidfile" &&
+     ${pkgs.procps}/bin/pgrep --pidfile="$pidfile" >/dev/null
+  then
+    ${pkgs.procps}/bin/pkill --pidfile="$pidfile"
+    ${pkgs.coreutils}/bin/rm "$pidfile"
+    exit
+  fi
+
+  echo $$ > "$pidfile"
+
+  exec ${pkgs.xterm}/bin/xterm \
+      -name "$name" \
+      -ti vt340 \
+      -xrm 'Pager*geometry: 32x10' \
+      -xrm 'Pager*internalBorder: 2' \
+      -e ${pkgs.haskellPackages.pager}/bin/pager "$@"
+''
