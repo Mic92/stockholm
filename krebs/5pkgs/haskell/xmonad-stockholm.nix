59056a2e404544b418bf7be0cfb087ac292761fc
tv <tv@krebsco.de>
2022-04-06 15:32:06 +0200

    xmonad-stockholm: 1.3.0 -> 1.3.1

--- krebs/5pkgs/haskell/xmonad-stockholm.nix
+++ krebs/5pkgs/haskell/xmonad-stockholm.nix
@@ -1,15 +1,18 @@
-{ mkDerivation, base, containers, fetchgit, filepath, lib, unix, X11, X11-xft , X11-xshape, xmonad, xmonad-contrib
+{ mkDerivation, base, containers, directory, fetchgit, filepath
+, lib, unix, X11, X11-xft, X11-xshape, xmonad, xmonad-contrib
 }:
-mkDerivation rec {
+mkDerivation {
   pname = "xmonad-stockholm";
-  version = "1.3.0";
+  version = "1.3.1";
   src = fetchgit {
-    url = http://cgit.ni.krebsco.de/xmonad-stockholm;
-    rev = "refs/tags/v1.3.0";
-    sha256 = "1np5126wn67y0a1r60rnkq828s0w9zjnvai4b8zy3yc02xlkrjm9";
+    url = "https://cgit.krebsco.de/xmonad-stockholm";
+    sha256 = "1m4kkppy143jvjzhy5aawh8q6sglpnqhiajxbdcr42j02ibf3vvq";
+    rev = "89bae8aad73db8fe9e11da7d515f0b236e7fea51";
+    fetchSubmodules = true;
   };
   libraryHaskellDepends = [
-    base containers filepath unix X11 X11-xft X11-xshape xmonad xmonad-contrib
+    base containers directory filepath unix X11 X11-xft X11-xshape
+    xmonad xmonad-contrib
   ];
   license = lib.licenses.mit;
 }
