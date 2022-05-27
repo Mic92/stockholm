08887763dcf8a3e4d2a8152f051d4fa00d5b216e
lassulus <lassulus@lassul.us>
2022-05-27 19:59:06 +0200

    l security-workarounds: remove pkexec fix

--- krebs/2configs/security-workarounds.nix
+++ krebs/2configs/security-workarounds.nix
@@ -1,6 +1,4 @@
 { config, lib, pkgs, ... }:
 with import <stockholm/lib>;
 {
-  # https://github.com/Lassulus/CVE-2021-4034
-  security.wrappers.pkexec.source = lib.mkForce (pkgs.writeText "pkexec" "");
 }
