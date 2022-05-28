9e36a59fb16d7c4eb5dacae77069403790302aa1
lassulus <lassulus@lassul.us>
2022-05-28 12:11:37 +0200

    container-networking: NAT to con*

--- krebs/2configs/container-networking.nix
+++ krebs/2configs/container-networking.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 {
   networking.nat.enable = true;
-  networking.nat.internalInterfaces = ["ve-+"];
+  networking.nat.internalInterfaces = ["ve-+" "ctr+" ];
   networking.nat.externalInterface = lib.mkDefault "et0";
   networking.networkmanager.unmanaged = [ "interface-name:ve-*" ];
 }
