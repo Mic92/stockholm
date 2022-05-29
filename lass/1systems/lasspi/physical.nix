560b925e47faa283a13f63b096b66f6f8beeb754
lassulus <lassulus@lassul.us>
2022-05-29 21:06:43 +0200

    l lasspi.r: remove extlinux-compat

--- lass/1systems/lasspi/physical.nix
+++ lass/1systems/lasspi/physical.nix
@@ -25,7 +25,6 @@
     version = 4;
   };
   boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
 
   # Required for the Wireless firmware
   hardware.enableRedistributableFirmware = true;
