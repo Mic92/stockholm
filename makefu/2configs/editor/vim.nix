5e4e92b5fa8b293b3e2625c200f261b11d85351e
makefu <github@syntax-fehler.de>
2022-06-06 21:21:00 +0200

    ma vim: use vimPlugins, not vam

--- makefu/2configs/editor/vim.nix
+++ makefu/2configs/editor/vim.nix
@@ -6,16 +6,15 @@
     ((pkgs.vim_configurable.override { python = pkgs.python3; }).customize {
       name = "vim";
       vimrcConfig.customRC = builtins.readFile ./vimrc;
-      vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
-      vimrcConfig.vam.pluginDictionaries = [
-        { names = [ "undotree"
+      vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; { start = [
+       "undotree"
         "YouCompleteMe"
         #"UltiSnips"
-          "vim-better-whitespace" ]; }
         # vim-nix handles indentation better but does not perform sanity
-        { names = [ "vim-addon-nix" ]; ft_regex = "^nix\$"; }
+          "vim-addon-nix"
+          "vim-better-whitespace"
       ];
-
+      };
     })
   ];
 }
