cd1e86e682e06c9845de563ab3e8bbf79ec27742
makefu <github@syntax-fehler.de>
2022-06-06 21:26:07 +0200

    ma pkgs.tw-upload-plugin: bump

--- makefu/5pkgs/tw-upload-plugin/default.nix
+++ makefu/5pkgs/tw-upload-plugin/default.nix
@@ -3,6 +3,6 @@
 pkgs.fetchFromGitHub {
   owner = "makefu";
   repo = "tw-upload-plugin";
-  rev = "621e5fbe";
-  sha256 = "1wjy76s5mr6z772w6sr0dwqxaxn45c7ys4a5y2v55hxclr096dqg";
+  rev = "aa95fc1";
+  sha256 = "1nyx5jl4gd5nrrksjl4sz7f4ksg89p29znvd2fahd6dizs1vcw87";
 }
