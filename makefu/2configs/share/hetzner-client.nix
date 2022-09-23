f66a4cb237d29ae152ebb601b62bb6c9e29e4031
makefu <github@syntax-fehler.de>
2022-09-23 23:01:58 +0200

    ma share: proxy hetzner via gum

--- makefu/2configs/share/hetzner-client.nix
+++ makefu/2configs/share/hetzner-client.nix
@@ -21,11 +21,12 @@ in {
         "dir_mode=0770"
         "uid=${toString config.users.users.download.uid}"
         "gid=${toString config.users.groups.download.gid}"
-        #"vers=3"
-        "vers=2.1"
+        "vers=3"
+        #"vers=2.1"
         "rsize=65536"
         "wsize=130048"
         "iocharset=utf8"
+        "cache=loose"
       ];
   };
 }
