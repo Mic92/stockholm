092e42f7370b604ba05eda9b3ab1e07c91541d9a
makefu <github@syntax-fehler.de>
2022-09-23 22:19:00 +0200

    ma bitlbee: disable purple

--- makefu/2configs/bitlbee.nix
+++ makefu/2configs/bitlbee.nix
@@ -2,7 +2,7 @@
 {
    services.bitlbee = {
     enable = true;
-    libpurple_plugins = [ pkgs.telegram-purple pkgs.pidgin-skypeweb];
+    # libpurple_plugins = [ pkgs.telegram-purple pkgs.pidgin-skypeweb];
   };
   users.users.makefu.packages = with pkgs; [ weechat tmux ];
   state = [ "/var/lib/bitlbee" ];
