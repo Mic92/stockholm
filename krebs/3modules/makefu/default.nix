0fc70d222f4a7f10fac70936475cbbc0875c02ce
makefu <github@syntax-fehler.de>
2022-09-23 22:25:51 +0200

    ma music.euer.krebsco.de: init

--- krebs/3modules/makefu/default.nix
+++ krebs/3modules/makefu/default.nix
@@ -235,6 +235,7 @@ in {
           maps.work.euer    IN A      ${nets.internet.ip4.addr}
           play.work.euer    IN A      ${nets.internet.ip4.addr}
           ul.work.euer      IN A      ${nets.internet.ip4.addr}
+          music.euer        IN A      ${nets.internet.ip4.addr}
         '';
       };
       cores = 8;
