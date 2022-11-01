a5c08e96997d5fbbee41341b54c8b89ea48d423c
tv <tv@krebsco.de>
2022-11-01 11:09:48 +0100

    security-workarounds: let exim run with gnutls

--- krebs/2configs/security-workarounds.nix
+++ krebs/2configs/security-workarounds.nix
@@ -1,4 +1,20 @@
 { config, lib, pkgs, ... }:
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      exim =
+        super.exim.overrideAttrs (old: {
+          buildInputs = old.buildInputs ++ [ self.gnutls ];
+          preBuild = /* sh */ ''
+            ${old.preBuild}
+            sed -Ei '
+              s:^USE_OPENSSL=.*:# &:
+              s:^# (USE_GNUTLS)=.*:\1=yes:
+              s:^# (USE_GNUTLS_PC=.*):\1:
+            ' Local/Makefile
+          '';
+        });
+    })
+  ];
   services.nginx.package = lib.mkDefault (pkgs.nginxStable.override { openssl = pkgs.libressl; });
 }
