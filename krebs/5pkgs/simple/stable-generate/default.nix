6db1863a27857f92303322c3502505d541b6241f
lassulus <lassulus@lassul.us>
2022-11-21 14:28:25 +0100

    init pkgs.stable-generate

---
+++ krebs/5pkgs/simple/stable-generate/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+pkgs.writers.writeDashBin "stable-generate" ''
+  set -efu
+
+  STABLE_URL=''${STABLE_URL:-http://stable-confusion.r}
+
+  PAYLOAD=$(jq -cn --arg query "$*" '{fn_index: 51, data: [$query,"","None","None",20,"Euler a",false,false,1,1,7,-1,-1,0,0,0,false,512,512,false,0.7,0,0,"None","",false,false,false,"","Seed","","Nothing","",true,false,false,null,"",""], session_hash: "hello_this_is_dog"}')
+
+  data=$(curl -Ssf "$STABLE_URL/run/predict/" \
+    -X POST \
+    --Header 'Content-Type: application/json' \
+    --data "$PAYLOAD"
+  )
+  export data
+
+  filename=$(jq -rn 'env.data | fromjson.data[0][0].name')
+
+  echo "$STABLE_URL/file=$filename"
+''
