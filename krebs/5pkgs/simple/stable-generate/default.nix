618185c6f8936a6f70509be554704418e8471477
lassulus <lassulus@lassul.us>
2022-11-22 10:44:06 +0100

    stable-generate: activate repair faces

--- krebs/5pkgs/simple/stable-generate/default.nix
+++ krebs/5pkgs/simple/stable-generate/default.nix
@@ -10,7 +10,46 @@ pkgs.writers.writeDashBin "stable-generate" ''
 
   STABLE_URL=''${STABLE_URL:-http://stable-confusion.r}
 
-  PAYLOAD=$(jq -cn --arg query "$*" '{fn_index: 51, data: [$query,"","None","None",20,"Euler a",false,false,1,1,7,-1,-1,0,0,0,false,512,512,false,0.7,0,0,"None","",false,false,false,"","Seed","","Nothing","",true,false,false,null,"",""], session_hash: "hello_this_is_dog"}')
+  PAYLOAD=$(jq -cn --arg query "$*" '{fn_index: 51, data: [
+    $query,
+    "",
+    "None",
+    "None",
+    20,
+    "Euler a",
+    false, # restore faces
+    false,
+    1,
+    1,
+    7,
+    -1,
+    -1,
+    0,
+    0,
+    0,
+    false,
+    512, #probably resolution
+    512, #probably resolution
+    false,
+    0.7,
+    0,
+    0,
+    "None",
+    "",
+    false,
+    false,
+    false,
+    "",
+    "Seed",
+    "",
+    "Nothing",
+    "",
+    true,
+    false,
+    false,
+    null,
+    "",
+  ""], session_hash: "hello_this_is_dog"}')
 
   data=$(curl -Ssf "$STABLE_URL/run/predict/" \
     -X POST \
