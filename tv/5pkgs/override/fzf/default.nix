7721142f255cd00d4dace91b0b26725e7b70e696
tv <tv@krebsco.de>
2022-12-05 16:02:32 +0100

    tv fzf: bump complete-1

--- tv/5pkgs/override/fzf/default.nix
+++ tv/5pkgs/override/fzf/default.nix
@@ -1,9 +1,7 @@
 self: super:
 
 super.fzf.overrideAttrs (old: {
-  # XXX cannot use `patches` because fzf has a custom patchPhase
-  patchPhase = ''
-    patch -Np1 < ${./complete1.patch}
-    ${old.patchPhase or ""}
-  '';
+  patches = old.patches or [] ++ [
+    ./complete1.patch
+  ];
 })
