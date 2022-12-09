54300dfe750340d1e61947400ea86f71dad877af
tv <tv@krebsco.de>
2022-12-09 17:00:03 +0100

    ponte: enable krebs.pages

--- krebs/1systems/ponte/config.nix
+++ krebs/1systems/ponte/config.nix
@@ -8,4 +8,11 @@
   ];
 
   krebs.build.host = config.krebs.hosts.ponte;
+
+  krebs.pages.enable = true;
+  krebs.pages.nginx.addSSL = true;
+  krebs.pages.nginx.enableACME = true;
+
+  security.acme.acceptTerms = true;
+  security.acme.certs.${config.krebs.pages.domain}.email = "spam@krebsco.de";
 }
