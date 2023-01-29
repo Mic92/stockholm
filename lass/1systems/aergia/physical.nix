2adf9ebfcc9ba12c75eb812843c4daa32d5f07d4
lassulus <git@lassul.us>
2023-01-30 00:27:34 +0100

    l aergia.r: finetune hardware

--- lass/1systems/aergia/physical.nix
+++ lass/1systems/aergia/physical.nix
@@ -15,10 +15,20 @@
     efiInstallAsRemovable = true;
   };
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelParams = [
+    # Enable energy savings during sleep
+    "mem_sleep_default=deep"
+    "initcall_blacklist=acpi_cpufreq_init"
+
+    # for ryzenadj -i
+    "iomem=relaxed"
+  ];
 
   # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
   # On recent AMD CPUs this can be more energy efficient.
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "amd-pstate" "kvm-amd" ];
 
   # hardware.cpu.amd.updateMicrocode = true;
 
@@ -28,13 +38,35 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
 
-  services.logind.lidSwitch = "ignore";
-  services.logind.lidSwitchDocked = "ignore";
-
   environment.systemPackages = [
+    pkgs.vulkan-tools
     pkgs.ryzenadj
+    (pkgs.writers.writeDashBin "set_tdp" ''
+      set -efux
+      watt=$1
+      value=$(( $watt * 1000 ))
+      ${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit="$value" --fast-limit="$value" --slow-limit="$value"
+    '')
   ];
 
   # textsize
   services.xserver.dpi = 200;
+  hardware.video.hidpi.enable = lib.mkDefault true;
+
+  # corectrl
+  programs.corectrl.enable = true;
+  users.users.mainUser.extraGroups = [ "corectrl" ];
+
+  # use newer ryzenadj
+  nixpkgs.config.packageOverrides = super: {
+    ryzenadj = super.ryzenadj.overrideAttrs (old: {
+      version = "unstable-2023-01-15";
+      src = pkgs.fetchFromGitHub {
+        owner = "FlyGoat";
+        repo = "RyzenAdj";
+        rev = "1052fb52b2c0e23ac4cd868c4e74d4a9510be57c"; # unstable on 2023-01-15
+        sha256 = "sha256-/IxkbQ1XrBrBVrsR4EdV6cbrFr1m+lGwz+rYBqxYG1k=";
+      };
+    });
+  };
 }
