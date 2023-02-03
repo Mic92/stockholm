af9e845e861b51d691102c1bc177dfe5e26fe63f
tv <tv@krebsco.de>
2023-02-03 13:15:23 +0100

    flameshot-once: move to tv

--- lass/2configs/baseX.nix
+++ lass/2configs/baseX.nix
@@ -91,11 +91,11 @@ in {
     xorg.xhost
     xsel
     zathura
-    flameshot-once
+    flameshot
     (pkgs.writeDashBin "screenshot" ''
       set -efu
 
-      ${pkgs.flameshot-once}/bin/flameshot-once
+      ${pkgs.flameshot}/bin/flameshot
       ${pkgs.klem}/bin/klem
     '')
   ];
