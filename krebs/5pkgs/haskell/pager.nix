1ce5e46e7b56d7da09802b909d74b6b3439710a8
tv <tv@krebsco.de>
2023-02-07 14:28:16 +0100

    pager: fc6105a -> dfa3ff3

--- krebs/5pkgs/haskell/pager.nix
+++ krebs/5pkgs/haskell/pager.nix
@@ -1,21 +1,22 @@
-{ mkDerivation, base, blessings, bytestring, containers
-, data-default, hack, lib, optparse-applicative, probability
-, scanner, speculate, split, terminal-size, text, unix, X11
-, fetchgit
+{ mkDerivation, aeson, base, blessings, bytestring, containers
+, data-default, extra, fetchgit, hack, lib, optparse-applicative
+, probability, scanner, speculate, split, terminal-size, text, unix
+, utf8-string, X11
 }:
 mkDerivation {
   pname = "pager";
   version = "1.0.0";
   src = fetchgit {
     url = "https://cgit.krebsco.de/pager";
-    sha256 = "1qlkhqidaa6w02ix9ambfdsm7lfyx30ap481b9ic1ppyfkhqzfp6";
-    rev = "fc6105a5e7d1e3a07bf07ea85e7902dd8e9fc849";
+    sha256 = "07wjlhnb27vfhkqq5vhi768mlrcpwl4b2yfk04v3lw047q6pmby0";
+    rev = "dfa3ff346d22d332ffbadd46963f1cc5cb2a4939";
     fetchSubmodules = true;
   };
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
+  libraryHaskellDepends = [ base extra utf8-string X11 ];
   executableHaskellDepends = [
-    base blessings bytestring containers data-default hack
+    aeson base blessings bytestring containers data-default hack
     optparse-applicative probability scanner speculate split
     terminal-size text unix X11
   ];
