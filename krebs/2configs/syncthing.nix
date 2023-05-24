9fe6e5bb4f320876bff02bc75c51050d16593ad0
lassulus <git@lassul.us>
2023-05-24 17:39:54 +0200

    syncthing: add workaround for infinite recursion on unstable

--- krebs/2configs/syncthing.nix
+++ krebs/2configs/syncthing.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, ... }: with import <stockholm/lib>; let
+{ options, config, pkgs, ... }: with import <stockholm/lib>; let
   mk_peers = mapAttrs (n: v: { id = v.syncthing.id; });
 
   all_peers = filterAttrs (n: v: v.syncthing.id != null) config.krebs.hosts;
-  used_peer_names = unique (flatten (mapAttrsToList (n: v: v.devices) config.services.syncthing.declarative.folders));
+  used_peer_names = unique (filter isString (flatten (mapAttrsToList (n: v: v.devices) config.services.syncthing.folders)));
   used_peers = filterAttrs (n: v: elem n used_peer_names) all_peers;
 in {
   services.syncthing = {
     enable = true;
     configDir = "/var/lib/syncthing";
-    devices = mk_peers used_peers;
     key = toString <secrets/syncthing.key>;
     cert = toString <secrets/syncthing.cert>;
-  };
+    # workaround for infinite recursion on unstable, remove in 23.11
+  } // (if builtins.hasAttr "settings" options.services.syncthing then
+    { settings.devices = mk_peers used_peers; }
+  else
+    { devices = mk_peers used_peers; }
+  );
 
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
 }
