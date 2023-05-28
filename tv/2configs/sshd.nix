1a678e3093148d9f376d32723430444305ffb055
tv <tv@krebsco.de>
2023-05-28 12:51:19 +0200

    tv sshd: accept connections via wiregrill

--- tv/2configs/sshd.nix
+++ tv/2configs/sshd.nix
@@ -11,9 +11,11 @@ in {
   ];
   tv.iptables.extra4.nat.PREROUTING = [
     "-d ${cfg.host.nets.retiolum.ip4.addr} -p tcp --dport 22 -j ACCEPT"
+    "-d ${cfg.host.nets.wiregrill.ip4.addr} -p tcp --dport 22 -j ACCEPT"
   ];
   tv.iptables.extra6.nat.PREROUTING = [
     "-d ${cfg.host.nets.retiolum.ip6.addr} -p tcp --dport 22 -j ACCEPT"
+    "-d ${cfg.host.nets.wiregrill.ip6.addr} -p tcp --dport 22 -j ACCEPT"
   ];
   tv.iptables.extra.nat.PREROUTING = [
     "-p tcp --dport 22 -j REDIRECT --to-ports 0"
