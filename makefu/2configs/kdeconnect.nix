e55cc0644eda087ce49a854536b69cd3fd74f9af
makefu <github@syntax-fehler.de>
2023-06-03 15:34:18 +0200

    ma kdeconnect: reformat

--- makefu/2configs/kdeconnect.nix
+++ makefu/2configs/kdeconnect.nix
@@ -1,6 +1,6 @@
 {pkgs, ... }:
 {
-    environment.systemPackages = with pkgs; [ kdeconnect ];
-    networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
-    networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+  environment.systemPackages = with pkgs; [ kdeconnect ];
+  networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+  networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
 }
