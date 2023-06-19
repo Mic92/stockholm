0ef3c1bd01394b5278d94e813c00066ff18d697b
lassulus <git@lassul.us>
2023-06-19 02:16:17 +0200

    l: use unstable channel by default

--- lass/krops.nix
+++ lass/krops.nix
@@ -5,16 +5,33 @@
     pkgs
   ;
 
-
   source = { test }: lib.evalSource ([
     (krebs-source { test = test; })
     {
       nixos-config.symlink = "stockholm/lass/1systems/${name}/physical.nix";
-      nixpkgs-unstable.git = {
-        url = "https://github.com/nixos/nixpkgs";
-        ref = (lib.importJSON ../krebs/nixpkgs-unstable.json).rev;
-        shallow = true;
-      };
+      nixpkgs = lib.mkForce (if test then {
+        derivation = let
+          rev = (lib.importJSON ../krebs/nixpkgs-unstable.json).rev;
+          sha256 = (lib.importJSON ../krebs/nixpkgs-unstable.json).sha256;
+        in ''
+          with import (builtins.fetchTarball {
+            url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
+            sha256 = "${sha256}";
+          }) {};
+          pkgs.fetchFromGitHub {
+            owner = "nixos";
+            repo = "nixpkgs";
+            rev = "${rev}";
+            sha256 = "${sha256}";
+          }
+        '';
+      } else {
+        git = {
+          ref = (lib.importJSON ./nixpkgs.json).rev;
+          url = https://github.com/NixOS/nixpkgs;
+          shallow = true;
+        };
+      });
       secrets = if test then {
         file = toString ./2configs/tests/dummy-secrets;
       } else {
