accd095c5b86350a78315e2d3a1fe237fe4d449f
lassulus <git@lassul.us>
2023-07-03 11:16:57 +0200

    l themes: ignore gsettings errors

--- lass/2configs/themes.nix
+++ lass/2configs/themes.nix
@@ -15,7 +15,7 @@
         ${pkgs.coreutils}/bin/chown lass:users /var/theme/current_theme
         ${pkgs.xorg.xrdb}/bin/xrdb -merge /var/theme/config/xresources
         ${pkgs.procps}/bin/pkill -HUP xsettingsd
-        ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface gtk-theme "$(cat /var/theme/config/gtk-theme)"
+        ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface gtk-theme "$(cat /var/theme/config/gtk-theme)" || :
       else
         echo "theme $1 not found"
       fi
