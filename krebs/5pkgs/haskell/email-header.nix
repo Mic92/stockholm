2bdd7ad3349b2012d8c9659286c5f34843271768
tv <tv@krebsco.de>
2023-07-04 14:02:48 +0200

    email-header: 0.4.1-tv2 -> 0.4.2-tv1

--- krebs/5pkgs/haskell/email-header.nix
+++ krebs/5pkgs/haskell/email-header.nix
@@ -18,9 +18,9 @@ let
       sha256 = "11xjivpj495r2ss9aqljnpzzycb57cm4sr7yzmf939rzwsd3ib0x";
     };
   }.${versions.majorMinor version} or {
-    version = "0.4.1-tv2";
+    version = "0.4.2-tv1";
     rev = "refs/tags/v${cfg.version}";
-    sha256 = "1yg4b5318lpviwgjs4kdcqg8cwfnxxfcdckcjq12r2nnj2k4ms2d";
+    sha256 = "JZfqvkbb/1t0q1iWmZHmmCN2Vr+QKTiq4LVncrG+xMU=";
   };
 
 in mkDerivation {
