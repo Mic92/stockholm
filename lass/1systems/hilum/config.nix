76848d24c224b3a88b1e2ead3e9b895ed9319006
lassulus <git@lassul.us>
2023-07-08 15:05:48 +0200

    l hilum.r: make config minimal

--- lass/1systems/hilum/config.nix
+++ lass/1systems/hilum/config.nix
@@ -4,13 +4,8 @@
     <stockholm/lass>
 
     <stockholm/lass/2configs/retiolum.nix>
-    <stockholm/lass/2configs/baseX.nix>
-    <stockholm/lass/2configs/browsers.nix>
-    <stockholm/lass/2configs/programs.nix>
     <stockholm/lass/2configs/network-manager.nix>
-    <stockholm/lass/2configs/mail.nix>
     <stockholm/lass/2configs/syncthing.nix>
-    <stockholm/lass/2configs/nfs-dl.nix>
   ];
 
   krebs.build.host = config.krebs.hosts.hilum;
