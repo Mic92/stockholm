dd4c71d1429bba2251165ba4ac4ecad132b8947c
lassulus <git@lassul.us>
2023-07-15 19:44:41 +0200

    l nm-dmenu: add props to PATH

--- lass/5pkgs/nm-dmenu/default.nix
+++ lass/5pkgs/nm-dmenu/default.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 pkgs.writeDashBin "nm-dmenu" ''
-  export PATH=$PATH:${pkgs.dmenu}/bin:${pkgs.networkmanagerapplet}/bin
+  export PATH=$PATH:${lib.makeBinPath [
+    pkgs.dmenu
+    pkgs.networkmanagerapplet
+    pkgs.procps
+  ]}
   exec ${pkgs.networkmanager_dmenu}/bin/networkmanager_dmenu "$@"
 ''
