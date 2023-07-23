f59af7891eae767805a5ccf04799fe2f5929fb4d
lassulus <git@lassul.us>
2023-07-23 23:06:52 +0200

    l antimicrox: fix with newer versions

--- lass/2configs/antimicrox/default.nix
+++ lass/2configs/antimicrox/default.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   systemd.services.antimicrox = {
+    after = [ "display-manager.service" ];
     wantedBy = [ "multi-user.target" ];
     environment = {
       DISPLAY = ":0";
@@ -8,18 +9,23 @@
     serviceConfig = {
       User = config.users.users.mainUser.name;
       ExecStartPre = lib.singleton (pkgs.writeDash "init_state" "echo 0 > /tmp/gamepad.state");
-      ExecStart = "${pkgs.antimicrox}/bin/antimicrox --no-tray --hidden --profile ${./mouse.amgp}";
+      ExecStart = "${pkgs.antimicrox}/bin/antimicrox --hidden --profile ${./mouse.gamecontroller.amgp}";
     };
   };
 
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="input", OPTIONS+="static_node=uinput"
+  '';
+
   environment.systemPackages = [
+    pkgs.antimicrox
     (pkgs.writers.writeDashBin "gamepad_mouse_disable" ''
       echo 1 > /tmp/gamepad.state
-      ${pkgs.antimicrox}/bin/antimicrox --profile ${./empty.amgp}
+      ${pkgs.antimicrox}/bin/antimicrox --profile ${./empty.gamecontroller.amgp}
     '')
     (pkgs.writers.writeDashBin "gamepad_mouse_enable" ''
       echo 0 > /tmp/gamepad.state
-      ${pkgs.antimicrox}/bin/antimicrox --profile ${./mouse.amgp}
+      ${pkgs.antimicrox}/bin/antimicrox --profile ${./mouse.gamecontroller.amgp}
     '')
     (pkgs.writers.writeDashBin "gamepad_mouse_toggle" ''
       state=$(${pkgs.coreutils}/bin/cat /tmp/gamepad.state)
