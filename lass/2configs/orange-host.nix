89328a48aaa7ccdd411786c60831d43d9feba2ad
lassulus <git@lassul.us>
2023-09-03 12:09:18 +0200

    l orange: add bindmount for /var/lib

--- lass/2configs/orange-host.nix
+++ lass/2configs/orange-host.nix
@@ -3,6 +3,10 @@
   krebs.sync-containers3.containers.orange = {
     sshKey = "${toString <secrets>}/orange.sync.key";
   };
+  containers.orange.bindMounts."/var/lib" = {
+    hostPath = "/var/lib/sync-containers3/orange/state";
+    isReadOnly = false;
+  };
   services.nginx.virtualHosts."lassul.us" = {
     # enableACME = config.security;
     # forceSSL = true;
