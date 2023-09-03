63fc24339c838c8596ee535590a4c756bc5573ce
lassulus <git@lassul.us>
2023-09-03 12:12:33 +0200

    l snapserver: add radio stream

--- lass/2configs/snapserver.nix
+++ lass/2configs/snapserver.nix
@@ -2,12 +2,29 @@
 {
   services.snapserver = {
     enable = true;
-    openFirewall = true;
-      streams = {
-      pipewire  = {
+    # openFirewall = true;
+    streams = {
+      radio = {
+        type = "process";
+        location = pkgs.writers.writeDash "radio" ''
+          exec ${pkgs.mpv}/bin/mpv http://radio.lassul.us/radio.ogg \
+            --no-terminal \
+            --audio-display=no \
+            --audio-channels=stereo \
+            --audio-samplerate=48000 \
+            --audio-format=s16 \
+            --ao=pcm \
+            --ao-pcm-file=/dev/stdout
+        '';
+      };
+      styx = {
         type = "pipe";
         location = "/run/snapserver/snapfifo";
       };
     };
+    http.enable = true;
   };
+
+  networking.firewall.interfaces.int0.allowedTCPPorts = [ 1704 1705 1780 ];
+  networking.firewall.interfaces.retiolum.allowedTCPPorts = [ 1780 ];
 }
