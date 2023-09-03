f0ab828e1ab90826f24aa4b4b2ef02650f824139
lassulus <git@lassul.us>
2023-09-03 12:05:08 +0200

    l gc: disable on aergia

--- lass/2configs/gc.nix
+++ lass/2configs/gc.nix
@@ -3,7 +3,7 @@
 with import <stockholm/lib>;
 {
   nix.gc = {
-    automatic = ! (elem config.krebs.build.host.name [ "mors" "xerxes" "coaxmetal" ] || config.boot.isContainer);
+    automatic = ! (elem config.krebs.build.host.name [ "aergia" "mors" "xerxes" "coaxmetal" ] || config.boot.isContainer);
     options = "--delete-older-than 15d";
   };
 }
