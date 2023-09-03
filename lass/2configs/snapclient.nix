bb29da920769395db909ee3e1fa373a3e122a98e
lassulus <git@lassul.us>
2023-09-03 12:12:23 +0200

    l snapclient: use hostname as id

--- lass/2configs/snapclient.nix
+++ lass/2configs/snapclient.nix
@@ -3,7 +3,7 @@
   systemd.services.snapclient = {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.snapcast ];
-    script = "snapclient -h 10.42.0.1";
+    script = "snapclient -h 10.42.0.1 --hostID ${config.networking.hostName}";
     serviceConfig = {
       DynamicUser = true;
       Group = "pipewire";
