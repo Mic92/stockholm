62630ff3def88d169159f9228f07e39d8969f486
lassulus <git@lassul.us>
2023-09-03 12:16:34 +0200

    l deploy: add debug

--- lass/5pkgs/deploy/default.nix
+++ lass/5pkgs/deploy/default.nix
@@ -1,6 +1,6 @@
 { writers }:
 writers.writeDashBin "deploy" ''
-  set -eu
+  set -xeu
   export SYSTEM="$1"
   $(nix-build $HOME/sync/stockholm/lass/krops.nix --no-out-link --argstr name "$SYSTEM" -A deploy)
 ''
