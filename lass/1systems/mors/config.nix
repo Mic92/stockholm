ced758208aba91337d35831c69c47dbf7058f8df
lassulus <git@lassul.us>
2023-09-04 12:32:33 +0200

    l mors.r: add fast binfmt

--- lass/1systems/mors/config.nix
+++ lass/1systems/mors/config.nix
@@ -141,6 +141,24 @@ with import <stockholm/lib>;
   };
 
 
+  # It may leak your data, but look how FAST it is!1!!
+  # https://make-linux-fast-again.com/
+  boot.kernelParams = [
+    "noibrs"
+    "noibpb"
+    "nopti"
+    "nospectre_v2"
+    "nospectre_v1"
+    "l1tf=off"
+    "nospec_store_bypass_disable"
+    "no_stf_barrier"
+    "mds=off"
+    "mitigations=off"
+  ];
+
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+  ];
 
   nix.trustedUsers = [ "root" "lass" ];
 
