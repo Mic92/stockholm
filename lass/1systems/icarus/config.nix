754faee1a004573daaa0cc8e81019075b5986a94
lassulus <git@lassul.us>
2023-09-04 12:29:59 +0200

    l icarus.r: remove some services, add consul

--- lass/1systems/icarus/config.nix
+++ lass/1systems/icarus/config.nix
@@ -17,20 +17,14 @@ with import <stockholm/lib>;
     <stockholm/lass/2configs/games.nix>
     <stockholm/lass/2configs/bitcoin.nix>
     <stockholm/lass/2configs/wine.nix>
-    <stockholm/lass/2configs/syncthing.nix>
-    <stockholm/lass/2configs/nfs-dl.nix>
     #<stockholm/lass/2configs/prism-share.nix>
     <stockholm/lass/2configs/network-manager.nix>
-    <stockholm/lass/2configs/home-media.nix>
     <stockholm/lass/2configs/snapclient.nix>
+    <stockholm/lass/2configs/consul.nix>
   ];
 
   krebs.build.host = config.krebs.hosts.icarus;
 
-  services.xserver.displayManager.lightdm.autoLogin = {
-    enable = true;
-    user = "media";
-  };
 
   environment.systemPackages = [ pkgs.chromium ];
 }
