f9adf3af97b7f4be5dcd314fa45138fc59cc1b7f
tv <tv@krebsco.de>
2023-09-05 11:30:39 +0200

    tv initrd/sshd: init

---
+++ tv/2configs/initrd/sshd.nix
@@ -0,0 +1,17 @@
+{ config, ... }: {
+  boot.initrd.availableKernelModules = [
+    "e1000e"
+  ];
+  boot.initrd.network.enable = true;
+  boot.initrd.network.ssh = {
+    enable = true;
+    port = 11423;
+    authorizedKeys = [
+      config.krebs.users.tv.pubkey
+    ];
+    ignoreEmptyHostKeys = true;
+  };
+  boot.initrd.secrets = {
+    "/etc/ssh/ssh_host_rsa_key" = <secrets/initrd/ssh_host_rsa_key>;
+  };
+}
