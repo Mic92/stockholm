0ea93f6de78169f73cfea8935fb5173904ac8c8e
lassulus <git@lassul.us>
2023-09-20 13:36:29 +0200

    l prism.r: add {flex,flux}.lassul.us

--- kartei/lass/prism.nix
+++ kartei/lass/prism.nix
@@ -37,6 +37,8 @@ rec {
       mail                60 IN A      ${nets.internet.ip4.addr}
       mail                60 IN AAAA   ${nets.internet.ip6.addr}
       flix                60 IN A      ${nets.internet.ip4.addr}
+      flex                60 IN A      ${nets.internet.ip4.addr}
+      flux                60 IN A      ${nets.internet.ip4.addr}
       testing             60 IN A      ${nets.internet.ip4.addr}
       schrott             60 IN A      ${nets.internet.ip4.addr}
     '';
