c0090dd2b1919f3aa3c7fe0e930ed5685d352e68
tv <tv@krebsco.de>
2023-09-27 11:30:49 +0200

    kartei Ra33it0: normalize retiolum RSA key

--- kartei/Ra33it0/default.nix
+++ kartei/Ra33it0/default.nix
@@ -10,7 +10,7 @@ in {
       aliases = [ "unispore.Ra33it0.r" ];
       ip6.addr = (slib.krebs.genipv6 "retiolum" "Ra33it0" { hostName = "unispore"; }).address;
       tinc.pubkey = ''
-        -----BEGIN RSA PUBLIC KEY-----
+-----BEGIN RSA PUBLIC KEY-----
 MIICCgKCAgEA6Cb+b+snYpsQv1J0yMPSL4P0iKs2EkDtqtt6kBOvqFTr2lRB2thp
 mu9fRbz/CFmcvFXoEMWQEEkKcyhgJEola2+7Ra49iMNX55o/I0iZ499ZI5rIK/JG
 +A60ijPCh5TSGYIMiD7VWRsxoAtzB1DZ6n4z94KN0wQB5dXKuLPjk/TDfJPuzMrS
@@ -22,7 +22,7 @@ CWYM/HzSffzztK8bF0DwhdWzjtNcwZ05XfA2krGZyMj9UxpwN84o1syCnnYC1Xzg
 4r48fUhubXXE4SbdnN68pCNCct9DT8exPeYeJL2FHi6s+EsfBY+NGEAaQGJTeQEW
 zUSnX/txoZV6xGUKZ4iOgfQ4MBCVVdtPAaurNP/esVwOr0WF0DTuBDPGBaOqo+Us
 Ef5cREwrCE8nEY8tu3xl4M9iuCTwBuT79YFhfNI3jr1lcg6f8wGaTYsCAwEAAQ==
-        -----END RSA PUBLIC KEY-----
+-----END RSA PUBLIC KEY-----
       '';
       tinc.pubkey_ed25519 = "cFCAfLbDYv/Ty3m34aHgHr1dXGp2DSwfP0K7GG1TA7D";
     };
