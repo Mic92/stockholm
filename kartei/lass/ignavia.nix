353875a4168790ceb1f96449ffa3cc62d01d1d35
lassulus <git@lassul.us>
2023-10-05 17:53:01 +0200

    kartei lass: init ignavia

---
+++ kartei/lass/ignavia.nix
@@ -0,0 +1,19 @@
+{ r6, w6, ... }:
+{
+  ci = false;
+  nets = {
+    retiolum = {
+      ip4.addr = "10.243.0.25";
+      ip6.addr = r6 "16a2";
+      aliases = [
+        "ignavia.r"
+      ];
+      tinc = {
+        pubkey = builtins.readFile ./ignavia/retiolum.rsa_key.pub;
+        pubkey_ed25519 = builtins.replaceStrings [ "Ed25519PublicKey = " ] [ "" ] (builtins.readFile ./ignavia/retiolum.ed25519_key.pub);
+      };
+    };
+  };
+  ssh.pubkey = builtins.readFile ./ignavia/ssh.id_ed25519.pub;
+  syncthing.id = builtins.readFile ./ignavia/syncthing.pub;
+}
