17c0846711a8d4eac17f2483eeffa7ffc2785ce6
makefu <github@syntax-fehler.de>
2024-09-06 22:44:11 +0200

    puyak.r: use relative import

--- krebs/2configs/buildbot/worker.nix
+++ krebs/2configs/buildbot/worker.nix
@@ -1,4 +1,4 @@
-{ buildbot-nix, ... }:
+{ config, buildbot-nix, ... }:
 {
   imports = [
     buildbot-nix.nixosModules.buildbot-worker
@@ -6,6 +6,8 @@
 
   services.buildbot-nix.worker = {
     enable = true;
+    name = config.krebs.build.host.name;
     workerPasswordFile = "/var/src/secrets/nix-worker-file";
+    masterUrl = "tcp:host=gum:port=9989";
   };
 }
