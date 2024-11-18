dc18d91c3f2552692ad96aee939a35590d65dd5e
lassulus <git@lassul.us>
2024-11-18 22:28:23 +0100

    kartei l daedalus: rotate

--- kartei/lass/daedalus.nix
+++ kartei/lass/daedalus.nix
@@ -10,15 +10,20 @@
       tinc = {
         pubkey = ''
           -----BEGIN RSA PUBLIC KEY-----
-          MIIBCgKCAQEAzlIJfYIoQGXishIQGFNOcaVoeelqy7a731FJ+VfrqeR8WURQ6D+8
-          5hz7go+l3Z7IhTc/HbpGFJ5QJJNFSuSpLfZVyi+cKAUVheTivIniHFIRw37JbJ4+
-          qWTlVe3uvOiZ0cA9S6LrbzqAUTLbH0JlWj36mvGIPICDr9YSEkIUKbenxjJlIpX8
-          ECEBm8RU1aq3PUo/cVjmpqircynVJBbRCXZiHoxyLXNmh23d0fCPCabEYWhJhgaR
-          arkYRls5A14HGMI52F3ehnhED3k0mU8/lb4OzYgk34FjuZGmyRWIfrEKnqL4Uu2w
-          3pmEvswG1WYG/3+YE80C5OpCE4BUKAzYSwIDAQAB
+          MIICCgKCAgEA5SYx0hfmZ25mFzlnzdeoB46nMfJcyEuiJvMqNjRaeTRGe1UYFMOV
+          KdLYRqioMBbvIwU+7+1jslI3Tjfs9kWtt57p4ZqTUmfKZYkhA6onvKZUtQrv8M9c
+          sMOUhrY4p/QRvON4b9o9bZGdzDAX43EsAfDMW8YZvS6P+SQNeGORX3pimQ5ODY5N
+          P0rnFuYjGt3wAWcwyzmfKFedxcB/eDyYSjmhnkIwrODdS5rQyaCDKALSGBQ6bF4S
+          rvgy7PbwP71o8jVlhndOGyvZJPyx4yjwENrRWh6Wgwy/i2GYXWuc0+/Lvjq/X2k5
+          C2LPGTfZ4rJaRdaS8EvOAy7uADemSymIGn3EM7qgLb2ogcQOFVtBHwWXYepLmbQi
+          lnGiE83eDcXKUs8lmnZQTP8C+Ho/SQCVXpgOg29BdwIZJ26Gv1ueVlhOKIvF7Pay
+          huRbQywgo7jnvNKk5TEGAX3bhUctT+UBJ+7JDHUJdqgJYKilYWwrK0jBVLSRqGTa
+          UHPCyM5zLdX6G8CXK7v+CbH66GsZxSkfxYjv049CWdbQ/BAW5hWUW77xTnP81/yi
+          3a2XxtxxmCiGxl3+eqtXh5q2bOB/JKffGYfblHXJ3NK6HH5qVpcjT7CHKO21gqsb
+          hdPbKKD5aB2EJm+DYOnxS1UYsj7R/SmKGmNuQVfBm6jsBmjc4XmZQWECAwEAAQ==
           -----END RSA PUBLIC KEY-----
         '';
-        pubkey_ed25519 = "ybmNcRLtZ0NxlxIRE3bdc2G4lLXtTGXu+iRaXMTKCNG";
+        pubkey_ed25519 = "+xM97iA8eIB58bfsEjMfK7aqv+Emzajx5CYqSGjRR/K";
       };
     };
     wiregrill = {
@@ -26,8 +31,8 @@
       aliases = [
         "daedalus.w"
       ];
-      wireguard.pubkey = "ZVTTWbJfe8Oq6E6QW1qgXU91FnkuKDGJO3MF3I3gDFI=";
+      wireguard.pubkey = "1/ZN/YvIBpWJGIwf0EE5NpxnQVCsde8f5WR3LExRW0M=";
     };
   };
-  ssh.pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq5Ovdcsljr5dOl7+2sQNKpGpdX0SlOIuCZKEiWEp8g";
+  ssh.pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjmN3oUAj/3AFH0t4JdCjWn+AzyYyr8Dhp0oqq9Nzbu";
 }
